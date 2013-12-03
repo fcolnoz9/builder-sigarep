612993bba0001acc3e37fc0aa1d04ba20e0a9c3f
jorgechaviel@gmail.com <jorgechaviel@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2013-12-03 17:26:26 +0000

    se agrego el archivo login.zul , la clase usuario.java y controlador login para manejar el acceso con spring security.

---
+++ trunk/Sigarep.webapp/src/sigarep/modelos/seguridad/ControladorLogin.java
@@ -0,0 +1,41 @@
+package sigarep.modelos.seguridad;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+ 
+@Controller
+public class ControladorLogin {
+ 
+	@RequestMapping(value="/Portal", method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+ 
+		User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String name = user.getUsername();
+	
+		model.addAttribute("username", name);
+		model.addAttribute("message", "Spring Security login + database example");
+		return "Portal.zul";
+	}
+ 
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	public String login(ModelMap model) {
+		return "accesoDenegado";
+	}
+	
+	@RequestMapping(value="/loginfailed", method = RequestMethod.GET)
+	public String loginerror(ModelMap model) {
+ 
+		model.addAttribute("error", "true");
+		return "timeout";
+	}
+	
+	@RequestMapping(value="/logout", method = RequestMethod.GET)
+	public String logout(ModelMap model) {
+		return "login";
+	}
+	
+}
