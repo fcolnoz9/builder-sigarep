297625fc302c438c7348d7030fb5c6692a867db6
lseolivera@gmail.com <lseolivera@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2013-12-09 20:46:00 +0000

    Marca de agua en los textbox

---
+++ trunk/Sigarep.webapp/WebContent/widgets/js/watermark.js
@@ -0,0 +1,15 @@
+        zk.afterMount(function() {
+            jq("$txtInstancia").Watermark("Nombre de la Instancia","gray");
+            jq("$txtRecurso").Watermark("Nombre del Recurso","gray");
+            jq("$txtDescripcion").Watermark("Ingrese alguna descripcion","gray");
+             
+            $.mask.definitions['A']='[A-Z]';
+            $.mask.definitions['m']='[01]';
+            $.mask.definitions['d']='[0123]';
+            $.mask.definitions['y']='[12]';
+             
+            jq("$phone").mask("(999) 999-9999");
+            jq("$date").mask("m9/d9/y999");
+            jq("$country").mask("AA");
+            jq("$cc").mask("9999-9999-9999-9999");
+        });
