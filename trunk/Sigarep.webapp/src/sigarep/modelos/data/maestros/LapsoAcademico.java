3542fcfa9bb18a701dc03cd943cbee78d6839ace
blumxp@gmail.com <blumxp@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2013-12-09 15:23:30 +0000

    version original

--- trunk/Sigarep.webapp/src/sigarep/modelos/data/maestros/LapsoAcademico.java
+++ trunk/Sigarep.webapp/src/sigarep/modelos/data/maestros/LapsoAcademico.java
@@ -9,128 +9,117 @@ import sigarep.modelos.data.transacciones.EstudianteSancionado;
 import java.util.Date;
 import java.util.List;
 
-/**as
+
+/**
  * The persistent class for the lapso_academico database table.
  * 
  */
 @Entity
-@Table(name = "lapso_academico")
+@Table(name="lapso_academico")
 public class LapsoAcademico implements Serializable {
-	private static final long serialVersionUID = 1L;
+        private static final long serialVersionUID = 1L;
 
-	private String codigoLapso;
-	private Date fechaInicio;
-	private Date fechaCierre;
-	private Boolean estatus;
+        @Id
+        @GeneratedValue(strategy=GenerationType.IDENTITY)
+        @Column(name="codigo_lapso", unique=true, nullable=false, length=6)
+        private String codigoLapso;
 
-	public LapsoAcademico(String codigoLapso, Date fechaCierre, Date fechaInicio,Boolean estatus) {
-		super();
-		this.codigoLapso = codigoLapso;
-		this.fechaInicio = fechaInicio;
-		this.fechaCierre = fechaCierre;
-		this.estatus = estatus;
+        @Column(nullable=false)
+        private Boolean estatus;
 
-	}
+        @Temporal(TemporalType.DATE)
+        @Column(name="fecha_cierre", nullable=false)
+        private Date fechaCierre;
 
-	public LapsoAcademico() {
-	}
+        @Temporal(TemporalType.DATE)
+        @Column(name="fecha_inicio", nullable=false)
+        private Date fechaInicio;
 
-	@Id
-	// @GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name = "codigolapso", unique = true, nullable = false, length = 6)
-	public String getCodigoLapso() {
-		return codigoLapso;
-	}
+        //bi-directional many-to-one association to Cronograma
+        @OneToMany(mappedBy="lapsoAcademico")
+        private List<Cronograma> cronogramas;
 
-	public void setCodigoLapso(String codigoLapso) {
-		this.codigoLapso = codigoLapso;
-	}
+        //bi-directional many-to-one association to EstudianteSancionado
+        @OneToMany(mappedBy="lapsoAcademico")
+        private List<EstudianteSancionado> estudianteSancionados;
 
-		@Temporal(TemporalType.DATE)
-	@Column(name = "fecha_inicio", nullable = false)
-	public Date getFechaInicio() {
-		return this.fechaInicio;
-	}
+        public LapsoAcademico() {
+        }
 
-	public void setFechaInicio(Date fechaInicio) {
-		this.fechaInicio = fechaInicio;
-	}
-	
-	@Temporal(TemporalType.DATE)
-	@Column(name = "fecha_cierre", nullable = false)
-	public Date getFechaCierre() {
-		return this.fechaCierre;
-	}
+        public String getCodigoLapso() {
+                return this.codigoLapso;
+        }
 
-	public void setFechaCierre(Date fechaCierre) {
-		this.fechaCierre = fechaCierre;
-	}
+        public void setCodigoLapso(String codigoLapso) {
+                this.codigoLapso = codigoLapso;
+        }
 
-	@Column(nullable = false)
-	public Boolean getEstatus() {
-		return this.estatus;
-	}
+        public Boolean getEstatus() {
+                return this.estatus;
+        }
 
-	public void setEstatus(Boolean estatus) {
-		this.estatus = estatus;
-	}
+        public void setEstatus(Boolean estatus) {
+                this.estatus = estatus;
+        }
 
-	
-	// bi-directional many-to-one association to Cronograma
-	@OneToMany(mappedBy = "lapsoAcademico")
-	private List<Cronograma> cronogramas;
+        public Date getFechaCierre() {
+                return this.fechaCierre;
+        }
 
-	// bi-directional many-to-one association to EstudianteSancionado
-	@OneToMany(mappedBy = "lapsoAcademico")
-	private List<EstudianteSancionado> estudianteSancionados;
+        public void setFechaCierre(Date fechaCierre) {
+                this.fechaCierre = fechaCierre;
+        }
 
-		
+        public Date getFechaInicio() {
+                return this.fechaInicio;
+        }
 
-	public List<Cronograma> getCronogramas() {
-		return this.cronogramas;
-	}
+        public void setFechaInicio(Date fechaInicio) {
+                this.fechaInicio = fechaInicio;
+        }
 
-	public void setCronogramas(List<Cronograma> cronogramas) {
-		this.cronogramas = cronogramas;
-	}
+        public List<Cronograma> getCronogramas() {
+                return this.cronogramas;
+        }
 
-	public Cronograma addCronograma(Cronograma cronograma) {
-		getCronogramas().add(cronograma);
-		cronograma.setLapsoAcademico(this);
+        public void setCronogramas(List<Cronograma> cronogramas) {
+                this.cronogramas = cronogramas;
+        }
 
-		return cronograma;
-	}
+        public Cronograma addCronograma(Cronograma cronograma) {
+                getCronogramas().add(cronograma);
+                cronograma.setLapsoAcademico(this);
 
-	public Cronograma removeCronograma(Cronograma cronograma) {
-		getCronogramas().remove(cronograma);
-		cronograma.setLapsoAcademico(null);
+                return cronograma;
+        }
 
-		return cronograma;
-	}
+        public Cronograma removeCronograma(Cronograma cronograma) {
+                getCronogramas().remove(cronograma);
+                cronograma.setLapsoAcademico(null);
 
-	public List<EstudianteSancionado> getEstudianteSancionados() {
-		return this.estudianteSancionados;
-	}
+                return cronograma;
+        }
 
-	public void setEstudianteSancionados(
-			List<EstudianteSancionado> estudianteSancionados) {
-		this.estudianteSancionados = estudianteSancionados;
-	}
+        public List<EstudianteSancionado> getEstudianteSancionados() {
+                return this.estudianteSancionados;
+        }
 
-	public EstudianteSancionado addEstudianteSancionado(
-			EstudianteSancionado estudianteSancionado) {
-		getEstudianteSancionados().add(estudianteSancionado);
-		estudianteSancionado.setLapsoAcademico(this);
+        public void setEstudianteSancionados(List<EstudianteSancionado> estudianteSancionados) {
+                this.estudianteSancionados = estudianteSancionados;
+        }
 
-		return estudianteSancionado;
-	}
+        public EstudianteSancionado addEstudianteSancionado(EstudianteSancionado estudianteSancionado) {
+                getEstudianteSancionados().add(estudianteSancionado);
+                estudianteSancionado.setLapsoAcademico(this);
 
-	public EstudianteSancionado removeEstudianteSancionado(
-			EstudianteSancionado estudianteSancionado) {
-		getEstudianteSancionados().remove(estudianteSancionado);
-		estudianteSancionado.setLapsoAcademico(null);
+                return estudianteSancionado;
+        }
 
-		return estudianteSancionado;
-	}
+        public EstudianteSancionado removeEstudianteSancionado(EstudianteSancionado estudianteSancionado) {
+                getEstudianteSancionados().remove(estudianteSancionado);
+                estudianteSancionado.setLapsoAcademico(null);
+
+                return estudianteSancionado;
+        }
 
 }
