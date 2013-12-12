edd222e5206c2e9115b6ba3f809701917059dd8d
vanedunob@gmail.com <vanedunob@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2013-12-12 21:17:41 +0000

    VM pregunta basica

---
+++ trunk/Sigarep.webapp/src/sigarep/viewmodels/maestros/VMpreguntaBasica.java
@@ -0,0 +1,121 @@
+package sigarep.viewmodels.maestros;
+import org.zkoss.zul.Button;
+import java.util.List;
+import javax.persistence.Column;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zk.ui.select.annotation.VariableResolver;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zk.ui.select.annotation.WireVariable;
+import org.zkoss.zul.Messagebox;
+import org.zkoss.zul.Textbox;
+import org.zkoss.zul.Window;
+import sigarep.modelos.data.maestros.PreguntaBasica;
+import sigarep.modelos.servicio.maestros.ServicioPreguntaBasica;
+
+
+@SuppressWarnings("serial")
+@VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
+public class VMpreguntaBasica {
+	@WireVariable ServicioPreguntaBasica serviciopreguntabasica;
+	private Integer idPreguntaBasica;private String pregunta; private String respuesta;private Boolean estatus;
+    @Wire Textbox txtpregunta;
+    @Wire Textbox txtrespuesta;
+    @Wire Button btnGuardarPregunta;
+    @Wire Window winPreguntaBasica;
+    private List<PreguntaBasica> listaPregunta;
+	private PreguntaBasica preguntaseleccionada;
+    
+    public Integer getIdPreguntaBasica() {
+		return idPreguntaBasica;
+	}
+
+	public void setIdPreguntaBasica(Integer idPreguntaBasica) {
+		this.idPreguntaBasica = idPreguntaBasica;
+	}
+	
+	public String getPregunta() {
+		return pregunta;
+	}
+
+	public void setPregunta(String pregunta) {
+		this.pregunta = pregunta;
+	}
+	
+	public String getRespuesta() {
+		return respuesta;
+	}
+
+	public void setRespuesta(String respuesta) {
+		this.respuesta = respuesta;
+	}
+	
+	public Boolean getEstatus() {
+		return estatus;
+	}
+	public void setEstatus(Boolean estatus){
+		this.estatus = estatus;
+	}
+	
+	public List<PreguntaBasica> getListaPregunta() {
+		return listaPregunta;
+	}
+
+	public void setListaPregunta(List<PreguntaBasica> listaPregunta) {
+		this.listaPregunta = listaPregunta;
+	}
+	public PreguntaBasica getPreguntaseleccionada() {
+		return preguntaseleccionada;
+	}
+	public void setPreguntaseleccionada(PreguntaBasica preguntaseleccionada) {
+		this.preguntaseleccionada = preguntaseleccionada;
+	}
+	@Init
+	public void init(){
+		
+		buscarPreguntab ();
+    }
+	@Command
+	@NotifyChange({"id_pregunta_basica", "pregunta", "respuesta","estatus","listaPregunta"})//el notifychange le  avisa a que parametros en la pantalla se van a cambiar, en este caso es nombre,apellido,email,sexo se va a colocar en blanco al guardar!!
+	public void guardarPregunta(){
+		if (pregunta.equals("")||respuesta.equals(""))
+			Messagebox.show("Debes Llenar todos los Campos", "Advertencia", Messagebox.OK, Messagebox.EXCLAMATION);
+		else{
+		PreguntaBasica preb = new PreguntaBasica (idPreguntaBasica,pregunta,respuesta,true);
+		serviciopreguntabasica.guardarPregunta(preb);
+		Messagebox.show("Se ha Registrado Correctamente", "Informacion", Messagebox.OK, Messagebox.INFORMATION);
+		limpiar();
+		}
+	}
+	@Command
+	@NotifyChange({"id_pregunta_basica", "pregunta", "respuesta","estatus"})
+	public void limpiar(){
+		 pregunta = "";respuesta="";
+		 buscarPreguntab ();
+		
+	}
+	
+	@Command
+	@NotifyChange({"listaPregunta"})
+	public void buscarPreguntab(){
+		listaPregunta =serviciopreguntabasica.buscarPr(pregunta);
+	}
+	@Command
+	@NotifyChange({"listaPregunta"})
+	public void eliminarPreguntaBasica(){
+		serviciopreguntabasica.eliminarPregunta(getPreguntaseleccionada().getIdPreguntaBasica());
+		limpiar();
+		Messagebox.show("Se ha Eliminado Correctamente", "Informacion", Messagebox.OK, Messagebox.INFORMATION);
+	}
+	@Command
+	@NotifyChange({"pregunta","respuesta"})
+	public void mostrarSeleccionada(){
+		pregunta=getPreguntaseleccionada().getPregunta();
+		respuesta=getPreguntaseleccionada().getRespuesta();
+	
+	
+	}
+	
+	
+}
