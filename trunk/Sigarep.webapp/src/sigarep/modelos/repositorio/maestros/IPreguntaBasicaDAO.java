218dbd2ebe179e069299e99560aabe109918bbf8
vanedunob@gmail.com <vanedunob@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2013-12-12 21:11:31 +0000

    pregunta basica

--- trunk/Sigarep.webapp/src/sigarep/modelos/repositorio/maestros/IPreguntaBasicaDAO.java
+++ trunk/Sigarep.webapp/src/sigarep/modelos/repositorio/maestros/IPreguntaBasicaDAO.java
@@ -1,10 +1,19 @@
 package sigarep.modelos.repositorio.maestros;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import sigarep.modelos.data.maestros.PreguntaBasica;
 
 public interface IPreguntaBasicaDAO extends
 		JpaRepository<PreguntaBasica, Integer> {
+	@Query("select  pre from PreguntaBasica pre where pre.pregunta= :pregunta")
+	public List<PreguntaBasica> buscarPregunta();
+	@Query("select pr from PreguntaBasica pr where pr.pregunta= :pregunta")
+    public PreguntaBasica findBynombre(Integer idPreguntaBasica);
+	@Query("select pr from PreguntaBasica pr where pr.idPreguntaBasica= :idPreguntaBasica")
+	public List<PreguntaBasica> buscarPreguntab(Integer idPreguntaBasica);
 
 }
