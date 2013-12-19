3d26a6caa768200d76e259b0359cc690e8392aed
saezgervazzikarol@gmail.com <saezgervazzikarol@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2013-12-19 20:21:56 +0000

    Actualizando enlces de interes

--- trunk/Sigarep.webapp/src/sigarep/modelos/data/maestros/EnlaceInteres.java
+++ trunk/Sigarep.webapp/src/sigarep/modelos/data/maestros/EnlaceInteres.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
 import sigarep.herramientas.Archivo;
 
 
+
 /**
  * The persistent class for the enlace_interes database table.
  * 
@@ -16,6 +17,7 @@ public class EnlaceInteres implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="id_enlace", unique=true, nullable=false)
 	private Integer idEnlace;
 
