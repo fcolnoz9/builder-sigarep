193e9ae7ea9fa315cb89d7e87f11ae4505b24e58
jorgechaviel@gmail.com <jorgechaviel@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2013-12-31 16:24:09 +0000

    Actualización del menú tree.zul para poder registrar usuarios y grupos de usuario, así como registrar sancionado, se modificarón los archivo repositorio de usuario y grupo, así como sus servicios y se agregaron sus viewmodel y la la modificación de la vista de usuario .zul

--- trunk/Sigarep.webapp/src/sigarep/modelos/repositorio/maestros/IUsuarioDAO.java
+++ trunk/Sigarep.webapp/src/sigarep/modelos/repositorio/maestros/IUsuarioDAO.java
@@ -1,9 +1,15 @@
 package sigarep.modelos.repositorio.maestros;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import sigarep.modelos.data.maestros.Usuario;
 
 public interface IUsuarioDAO extends JpaRepository<Usuario, String> {
 
+	@Query("Select usu FROM Usuario AS usu where estatus = TRUE")		
+	public List<Usuario> buscarGruposActivos();
+	
 }
