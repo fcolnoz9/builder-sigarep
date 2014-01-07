40a78e6f4ce3b4278d97f51d116099b01466ae06
palencia77@gmail.com <palencia77@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-01-07 22:24:51 +0000

    Actualizada la clase soporte para trabajar con la clase documento

--- Sigarep.webapp/src/sigarep/modelos/data/transacciones/Soporte.java
+++ Sigarep.webapp/src/sigarep/modelos/data/transacciones/Soporte.java
@@ -3,6 +3,8 @@ package sigarep.modelos.data.transacciones;
 import java.io.Serializable;
 import javax.persistence.*;
 
+import sigarep.herramientas.Documento;
+
 import java.math.BigDecimal;
 import java.util.Date;
 
@@ -20,24 +22,15 @@ public class Soporte implements Serializable {
 	@EmbeddedId
 	private SoportePK id;
 
-	@Column(name="archivo_soporte", nullable=false)
-	private byte[] archivoSoporte;
-
 	@Column(nullable=false)
 	private Boolean estatus;
-
-	@Column(name="extension_archivo", nullable=false, length=4)
-	private String extensionArchivo;
 
 	@Temporal(TemporalType.DATE)
 	@Column(name="fecha_subida", nullable=false)
 	private Date fechaSubida;
-
-	@Column(name="nombre_archivo", nullable=false, length=60)
-	private String nombreArchivo;
-
-	@Column(name="tamanio_archivo_mb", nullable=false, precision=19)
-	private BigDecimal tamanioArchivoMb;
+	
+	@Embedded
+	private Documento documento;
 
 	//bi-directional many-to-one association to RecaudoEntregado
 	@ManyToOne
@@ -61,28 +54,12 @@ public class Soporte implements Serializable {
 		this.id = id;
 	}
 
-	public byte[] getArchivoSoporte() {
-		return this.archivoSoporte;
-	}
-
-	public void setArchivoSoporte(byte[] archivoSoporte) {
-		this.archivoSoporte = archivoSoporte;
-	}
-
 	public Boolean getEstatus() {
 		return this.estatus;
 	}
 
 	public void setEstatus(Boolean estatus) {
 		this.estatus = estatus;
-	}
-
-	public String getExtensionArchivo() {
-		return this.extensionArchivo;
-	}
-
-	public void setExtensionArchivo(String extensionArchivo) {
-		this.extensionArchivo = extensionArchivo;
 	}
 
 	public Date getFechaSubida() {
@@ -93,28 +70,20 @@ public class Soporte implements Serializable {
 		this.fechaSubida = fechaSubida;
 	}
 
-	public String getNombreArchivo() {
-		return this.nombreArchivo;
-	}
-
-	public void setNombreArchivo(String nombreArchivo) {
-		this.nombreArchivo = nombreArchivo;
-	}
-
-	public BigDecimal getTamanioArchivoMb() {
-		return this.tamanioArchivoMb;
-	}
-
-	public void setTamanioArchivoMb(BigDecimal tamanioArchivoMb) {
-		this.tamanioArchivoMb = tamanioArchivoMb;
-	}
-
 	public RecaudoEntregado getRecaudoEntregado() {
 		return this.recaudoEntregado;
 	}
 
 	public void setRecaudoEntregado(RecaudoEntregado recaudoEntregado) {
 		this.recaudoEntregado = recaudoEntregado;
+	}
+
+	public Documento getDocumento() {
+		return documento;
+	}
+
+	public void setDocumento(Documento documento) {
+		this.documento = documento;
 	}
 
 }
