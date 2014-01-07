68170216b297421b7f7ab88ac8e3762c2d9ee33c
mima350@gmail.com <mima350@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-01-07 05:18:03 +0000

    Registrar recurso jerarquico

---
+++ Sigarep.webapp/src/sigarep/modelos/repositorio/transacciones/IApelacionEstudianteMomentoDAO.java
@@ -0,0 +1,29 @@
+package sigarep.modelos.repositorio.transacciones;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import sigarep.modelos.data.maestros.PreguntaBasica;
+import sigarep.modelos.data.transacciones.*;
+import sigarep.modelos.data.maestros.*;
+import sigarep.modelos.data.transacciones.ApelacionMomento;
+import sigarep.modelos.data.transacciones.ApelacionMomentoPK;
+import sigarep.modelos.data.transacciones.EstudianteSancionadoPK;
+import sigarep.modelos.data.transacciones.EstudianteSancionado;
+import sigarep.modelos.servicio.transacciones.ListaEstudianteApelacion;
+
+//REVISAR.
+public interface IApelacionEstudianteMomentoDAO extends
+JpaRepository<ApelacionMomento, ApelacionMomentoPK> {
+	@Query("select es.cedulaEstudiante, es.primerNombre, es.primerApellido, " +
+			"sm.nombreSancion, m.idMomento, ia.idInstanciaApelada from Estudiante es, EstudianteSancionado esa, SolicitudApelacion sa, " +
+			"ApelacionMomento ap, Momento m, SancionMaestro sm, InstanciaApelada ia  where  es.cedulaEstudiante = " +
+			"cedula_estudiante and cedula_estudiante = " +
+			"sa.estudianteSancionado and sa.estudianteSancionado = " +
+			"cedula_estudiante and id_momento = m.idMomento and " +
+			"m.nombreMomento = 'veredicto' and id_sancion = sm.idSancion and " +
+			"id_instancia_apelada = ia.idInstanciaApelada and codigo_lapso = codigo_lapso")
+	public List<ListaEstudianteApelacion> buscarApelaciones();
+}
---
+++ Sigarep.webapp/src/sigarep/modelos/servicio/transacciones/ServicioApelacionMomentoRecurso.java
@@ -0,0 +1,35 @@
+package sigarep.modelos.servicio.transacciones;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import sigarep.modelos.data.transacciones.ApelacionMomento;
+import sigarep.modelos.repositorio.transacciones.IApelacionMomentoDAO;
+
+@Service("servicioapelacionmomentorecurso")
+public class ServicioApelacionMomentoRecurso {
+private @Autowired IApelacionMomentoDAO iApelacionMomentoDAO;
+	
+	public ApelacionMomento guardar(ApelacionMomento apelacionmomento) {
+		return iApelacionMomentoDAO.save(apelacionmomento);
+	}
+	
+	public void eliminar(ApelacionMomento apelacionmomento){
+		iApelacionMomentoDAO.delete(apelacionmomento);
+	}
+	
+	public List<ApelacionMomento> buscarTodos() {
+		return iApelacionMomentoDAO.findAll();
+	}
+
+	public int contarTodos() {
+		return iApelacionMomentoDAO.findAll().size();
+	}
+
+	public ApelacionMomento crear() {
+		return new ApelacionMomento();
+	}
+
+}
