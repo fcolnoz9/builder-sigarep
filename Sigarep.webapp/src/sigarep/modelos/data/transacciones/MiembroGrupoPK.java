6f6f103e6898f3a86aea101d3b91c37f8d08205a
palencia77@gmail.com <palencia77@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-01-07 02:33:41 +0000

    Se elimino la propiedad unique=true

--- Sigarep.webapp/src/sigarep/modelos/data/transacciones/MiembroGrupoPK.java
+++ Sigarep.webapp/src/sigarep/modelos/data/transacciones/MiembroGrupoPK.java
@@ -12,10 +12,10 @@ public class MiembroGrupoPK implements Serializable {
 	//default serial version id, required for serializable classes.
 	private static final long serialVersionUID = 1L;
 
-	@Column(name="id_grupo", unique=true, nullable=false)
+	@Column(name="id_grupo", unique=false, nullable=false)
 	private Integer idGrupo;
 
-	@Column(name="nombre_usuario", unique=true, nullable=false, length=30)
+	@Column(name="nombre_usuario", unique=false, nullable=false, length=30)
 	private String nombreUsuario;
 
 	public MiembroGrupoPK() {
