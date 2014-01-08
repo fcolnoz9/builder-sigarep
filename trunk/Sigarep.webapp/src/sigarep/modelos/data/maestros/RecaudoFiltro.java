c0a25714852d60a00690f52a03fcca5276fbad89
jorgechaviel@gmail.com <jorgechaviel@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-01-08 06:20:09 +0000

    Se modificó el nombre del VMRecaudos a VMRecaudosEntregados en las transacciones, se agrego una funcion mas al IRecaudo, se agregó el VMrecaudo que implementó Beleanny, así como las modificaciones q ella hizo en el serviciorecaudo, en el servicioTipoMotivo, y se agregó el RecaudoFiltro, se modificarón las vistas RegistrarRecaudos, RegistrarTipoMotivo y VerificarRecaudos.zul.

---
+++ trunk/Sigarep.webapp/src/sigarep/modelos/data/maestros/RecaudoFiltro.java
@@ -0,0 +1,24 @@
+package sigarep.modelos.data.maestros;
+
+public class RecaudoFiltro {
+	private String nombre_recaudo = "", nombre_tipo_motivo = "";
+
+	public String getNombre_recaudo() {
+		return nombre_recaudo;
+	}
+
+	public void setNombre_recaudo(String nombre_recaudo) {
+		this.nombre_recaudo = nombre_recaudo;
+	}
+
+	public String getNombre_tipo_motivo() {
+		return nombre_tipo_motivo;
+	}
+
+	public void setNombre_tipo_motivo(String nombre_tipo_motivo) {
+		this.nombre_tipo_motivo = nombre_tipo_motivo;
+	}
+
+	
+}
+	
