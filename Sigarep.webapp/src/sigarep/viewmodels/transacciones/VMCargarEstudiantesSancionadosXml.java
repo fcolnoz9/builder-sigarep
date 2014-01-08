25d4f13003a743cf6218fc28451d29db74edec96
blumxp@gmail.com <blumxp@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-01-08 04:16:38 +0000

    se el agregaron nuevos metodos para la carga de estudiantes sancionados

--- Sigarep.webapp/src/sigarep/viewmodels/transacciones/VMCargarEstudiantesSancionadosXml.java
+++ Sigarep.webapp/src/sigarep/viewmodels/transacciones/VMCargarEstudiantesSancionadosXml.java
@@ -20,29 +20,52 @@ import org.zkoss.zk.ui.select.annotation.VariableResolver;
 import org.zkoss.zk.ui.select.annotation.WireVariable;
 import org.zkoss.zul.Messagebox;
 import sigarep.modelos.data.maestros.Estudiante;
+import sigarep.modelos.data.maestros.LapsoAcademico;
 import sigarep.modelos.data.maestros.ProgramaAcademico;
+import sigarep.modelos.data.maestros.SancionMaestro;
+import sigarep.modelos.data.transacciones.EstudianteSancionado;
+import sigarep.modelos.data.transacciones.EstudianteSancionadoPK;
 import sigarep.modelos.servicio.maestros.ServicioEstudiante;
+import sigarep.modelos.servicio.maestros.ServicioLapsoAcademico;
 import sigarep.modelos.servicio.maestros.ServicioProgramaAcademico;
+import sigarep.modelos.servicio.maestros.ServicioSancionMaestro;
+import sigarep.modelos.servicio.transacciones.ServicioEstudianteSancionado;
 
 @VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
 public class VMCargarEstudiantesSancionadosXml {
-	private Document document;
-	private String textoXML;
-	private Integer tamanoXML;
-	private String primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,email,telefono,cedula_estudiante,aux_fecha_nacimiento,aux_anio_ingreso;
-	private String sexo;
-	private Boolean estatus;
+	private Document document;// Se Usa para poder leer el Xml, instancia la libreria Jdom-2.0.3 y poder usar sus propiedades
+	private Integer tamanoXML,id_programa;
+	private String primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,email,telefono,cedula_estudiante,aux_fecha_nacimiento,aux_anio_ingreso,textoXML,sexo;//datos del Estudiante
+	private Boolean estatus;//estatus del estudiante
     private Date fecha_nacimiento,anio_ingreso;
-    private Integer id_programa;
-	@WireVariable  ServicioEstudiante servicioestudiante;
-	@WireVariable
+	@WireVariable 
+	private  ServicioEstudiante servicioestudiante;
+	@WireVariable 
 	private Estudiante estudiante;
 	@WireVariable 
 	private ProgramaAcademico programa_academico;
 	@WireVariable 
-	private ProgramaAcademico aux_programaacademico;
-	@WireVariable 
 	private ServicioProgramaAcademico servicioprogramaacademico;
+	//Estudiante sancionado
+	private String codigo_lapso,aux_indice_grado,lapsos_academicos_RP;
+	private Integer unidades_cursadas,unidades_aprobadas,semestre,id_sancion;
+	private float indice_grado;
+	@WireVariable 
+	private LapsoAcademico lapsoAcademico;
+	@WireVariable 
+	private SancionMaestro sancionMaestro;
+	@WireVariable 
+	private EstudianteSancionado estudiante_sancionado;
+	@WireVariable 
+	private EstudianteSancionadoPK id;
+	@WireVariable 
+	private EstudianteSancionado estudianteSancionado;
+	@WireVariable 
+	private ServicioSancionMaestro serviciosancionmaestro;
+	@WireVariable 
+	private ServicioLapsoAcademico serviciolapsoacademico;
+	@WireVariable 
+	private ServicioEstudianteSancionado servicioestudiantesancionado;
 	
 	public String getTextoXML() {
 		return textoXML;
@@ -50,7 +73,6 @@ public class VMCargarEstudiantesSancionadosXml {
 	public void setTextoXML(String textoXML) {
 		this.textoXML = textoXML;
 	}
-	
 	public Integer getTamanoXML() {
 		return tamanoXML;
 	}
@@ -80,7 +102,6 @@ public class VMCargarEstudiantesSancionadosXml {
 	@NotifyChange({"texto","tamanoXML"})
 	public String leerXml(File f){
 		String datos="";
-		int tamaño=0;
 		SAXBuilder saxBuilder = new SAXBuilder();
 		try {
 			document = saxBuilder.build(f);
@@ -134,4 +155,76 @@ public class VMCargarEstudiantesSancionadosXml {
 		}
 		return datos;
 	}
+	
+	@Command
+	@NotifyChange({"textoXML","tamanoXML"})
+	public void cargaSancionados(){
+		JFileChooser selector=new JFileChooser();
+		FileNameExtensionFilter filtro=new FileNameExtensionFilter("Archivos XML","xml");
+	    selector.setFileFilter(filtro);
+	    int r=selector.showOpenDialog(null);
+		if(r==JFileChooser.APPROVE_OPTION){
+			if(selector.getFileFilter()==filtro){
+				leerXmlSancionados(selector.getSelectedFile());
+			System.out.println("extension :"+filtro.getExtensions());
+			}
+			else{
+				Messagebox.show("La Extension del Archivo no es XML", "ERROR",
+						Messagebox.OK, Messagebox.ERROR);
+				System.out.println("extension :"+filtro.getExtensions());
+			}
+		}
+	}
+	@Command
+	@NotifyChange({"texto","tamanoXML"})
+	public String leerXmlSancionados(File f){
+		String datos="";
+		SAXBuilder saxBuilder = new SAXBuilder();
+		try {
+			document = saxBuilder.build(f);
+			XMLOutputter output = new XMLOutputter();
+			System.out.println("Data Archivo"+output.outputString(document));
+			Element rootNode = document.getRootElement();
+			List list = rootNode.getChildren("EstudiantesSancionados");
+			tamanoXML=list.size();
+			for (int i = 0; i < list.size(); i++) {
+				   Element node = (Element) list.get(i);
+				   codigo_lapso=node.getChildText("codigo_lapso");
+				   cedula_estudiante=node.getChildText("cedula_estudiante");
+				   indice_grado=Float.parseFloat(node.getChildText("indice_grado"));
+				   lapsos_academicos_RP=node.getChildText("lapsos_academicos_RP");
+				   id_sancion=Integer.parseInt(node.getChildText("id_sancion"));
+				   unidades_cursadas=Integer.parseInt(node.getChildText("unidades_cursadas"));
+				   unidades_aprobadas=Integer.parseInt(node.getChildText("unidades_aprobadas"));
+				   semestre=Integer.parseInt(node.getChildText("semestre"));
+				   if(node.getChildText("estatus").equals("true")){
+					   estatus=true;
+				   }
+				   else{
+					   estatus=false; 
+				   }
+				   estudiante= new Estudiante();
+				   estudiante=servicioestudiante.buscarEstudiante(cedula_estudiante);//busca al estudiante por cedula y se lo asigna al objeto estudiante
+				   id =new EstudianteSancionadoPK();
+				   id.setCedulaEstudiante(cedula_estudiante);
+				   id.setCodigoLapso(codigo_lapso);
+				   sancionMaestro = new SancionMaestro();
+				   sancionMaestro =serviciosancionmaestro.buscarUnaSancion(id_sancion);
+				   estudiante_sancionado=new EstudianteSancionado();
+				   lapsoAcademico=new LapsoAcademico();
+				   lapsoAcademico= serviciolapsoacademico.buscarUnLapsoAcademico(codigo_lapso);
+				   estudiante_sancionado=new EstudianteSancionado(id,indice_grado,lapsos_academicos_RP,semestre,unidades_aprobadas,unidades_cursadas,estudiante,lapsoAcademico,sancionMaestro);
+				   servicioestudiantesancionado.guardar(estudiante_sancionado);
+				   
+				}
+			textoXML=(output.outputString(document));
+			Messagebox.show("Se ha Terminado la Carga de "+tamanoXML+" Estudiantes", "Informacion",Messagebox.OK, Messagebox.INFORMATION);
+		} catch (JDOMException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return datos;
+	}
+	
+	
 }
