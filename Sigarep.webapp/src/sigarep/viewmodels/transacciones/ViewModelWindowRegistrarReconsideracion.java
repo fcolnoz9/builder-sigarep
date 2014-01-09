4bc36debf134aec4518c1a728bb1caf06a45bd22
palencia77@gmail.com <palencia77@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-01-09 13:51:46 +0000

    Commit referente a cambios necesarios para reemplazar la tabla momento por estado_apelacion

--- Sigarep.webapp/src/sigarep/viewmodels/transacciones/ViewModelWindowRegistrarReconsideracion.java
+++ Sigarep.webapp/src/sigarep/viewmodels/transacciones/ViewModelWindowRegistrarReconsideracion.java
@@ -1,53 +1,17 @@
 package sigarep.viewmodels.transacciones;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
 
-import net.sf.jasperreports.engine.JRException;
-
-import org.zkoss.bind.annotation.AfterCompose;
-import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.ContextParam;
 import org.zkoss.bind.annotation.ContextType;
 import org.zkoss.bind.annotation.ExecutionArgParam;
 import org.zkoss.bind.annotation.Init;
-import org.zkoss.bind.annotation.NotifyChange;
-import org.zkoss.util.media.AMedia;
-import org.zkoss.util.resource.Labels;
-import org.zkoss.zhtml.Messagebox;
+
 import org.zkoss.zk.ui.Component;
-import org.zkoss.zk.ui.Executions;
-import org.zkoss.zk.ui.event.Event;
-import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.Selectors;
 import org.zkoss.zk.ui.select.annotation.VariableResolver;
 import org.zkoss.zk.ui.select.annotation.Wire;
-import org.zkoss.zk.ui.select.annotation.WireVariable;
-import org.zkoss.zkplus.databind.BindingListModel;
-import org.zkoss.zkplus.databind.BindingListModelArray;
-import org.zkoss.zkplus.databind.BindingListModelList;
-import org.zkoss.zul.Button;
-import org.zkoss.zul.Combobox;
-import org.zkoss.zul.Filedownload;
-import org.zkoss.zul.Label;
-import org.zkoss.zul.ListModel;
-import org.zkoss.zul.ListModelArray;
 import org.zkoss.zul.Window;
-
-import sigarep.modelos.data.maestros.*;
-import sigarep.modelos.data.transacciones.ApelacionMomento;
-import sigarep.modelos.data.transacciones.EstudianteSancionado;
-import sigarep.modelos.data.transacciones.SolicitudApelacion;
-import sigarep.modelos.servicio.maestros.*;
-import sigarep.modelos.servicio.transacciones.ListaApelacionMomento;
-import sigarep.modelos.servicio.transacciones.ServicioApelacion;
 //import sigarep.modelos.servicio.transacciones.ServicioApelacionMomento;
 //import sigarep.modelos.servicio.transacciones.ServicioEstudianteSancionado;
 //import sigarep.modelos.servicio.transacciones.ServicioSolicitudApelacion;
