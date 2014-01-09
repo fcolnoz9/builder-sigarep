eebaa317c82f92df956c1155c96097020ca5285d
palencia77@gmail.com <palencia77@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-01-09 13:47:12 +0000

    Commit referente a cambios necesarios para reemplazar la tabla momento por estado_apelacion

--- Sigarep.webapp/src/sigarep/modelos/repositorio/transacciones/IApelacionEstadoApelacionDAO.java
+++ Sigarep.webapp/src/sigarep/modelos/repositorio/transacciones/IApelacionEstadoApelacionDAO.java
@@ -6,25 +6,25 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import sigarep.modelos.data.maestros.PreguntaBasica;
-import sigarep.modelos.data.transacciones.ApelacionMomento;
-import sigarep.modelos.data.transacciones.ApelacionMomentoPK;
+import sigarep.modelos.data.transacciones.ApelacionEstadoApelacion;
+import sigarep.modelos.data.transacciones.ApelacionEstadoApelacionPK;
 import sigarep.modelos.data.transacciones.*;
 import sigarep.modelos.data.maestros.Estudiante;
 import sigarep.modelos.data.maestros.*;
 import sigarep.modelos.data.transacciones.EstudianteSancionadoPK;
 import sigarep.modelos.data.transacciones.EstudianteSancionado;
 
-public interface IApelacionMomentoDAO extends
-		JpaRepository<ApelacionMomento, ApelacionMomentoPK> {
+public interface IApelacionEstadoApelacionDAO extends
+		JpaRepository<ApelacionEstadoApelacion, ApelacionEstadoApelacionPK> {
 	@Query("select es.cedulaEstudiante, es.primerNombre, es.primerApellido, " +
-			"sm.nombreSancion, m.idMomento, ia.idInstanciaApelada from Estudiante es, EstudianteSancionado esa, SolicitudApelacion sa, " +
-			"ApelacionMomento ap, Momento m, SancionMaestro sm, InstanciaApelada ia  where  es.cedulaEstudiante = " +
+			"sm.nombreSancion, ea.idEstadoApelacion, ia.idInstanciaApelada from Estudiante es, EstudianteSancionado esa, SolicitudApelacion sa, " +
+			"ApelacionMomento ap, EstadoApelacion ea, SancionMaestro sm, InstanciaApelada ia  where  es.cedulaEstudiante = " +
 			"cedula_estudiante and cedula_estudiante = " +
 			"sa.estudianteSancionado and sa.estudianteSancionado = " +
-			"cedula_estudiante and id_momento = m.idMomento and " +
-			"m.nombreMomento = 'veredicto' and id_sancion = sm.idSancion and " +
+			"cedula_estudiante and id_estado_apelacion = ea.idEstadoApelacion and " +
+			"ea.nombreEstado = 'veredicto' and id_sancion = sm.idSancion and " +
 			"id_instancia_apelada = ia.idInstanciaApelada and codigo_lapso = codigo_lapso")
-	public List<ApelacionMomento> buscarApelaciones();
+	public List<ApelacionEstadoApelacion> buscarApelaciones();
 
 
 }
--- Sigarep.webapp/src/sigarep/modelos/repositorio/transacciones/IServicioApelacion.java
+++ Sigarep.webapp/src/sigarep/modelos/repositorio/transacciones/IServicioApelacion.java
@@ -3,17 +3,17 @@ package sigarep.modelos.repositorio.transacciones;
 import java.util.List;
 
 import sigarep.modelos.data.maestros.Estudiante;
-import sigarep.modelos.data.maestros.Momento;
-import sigarep.modelos.data.transacciones.ApelacionMomento;
+import sigarep.modelos.data.maestros.EstadoApelacion;
+import sigarep.modelos.data.transacciones.ApelacionEstadoApelacion;
 import sigarep.modelos.data.transacciones.EstudianteSancionado;
 import sigarep.modelos.data.transacciones.SolicitudApelacion;
-import sigarep.modelos.servicio.transacciones.ListaApelacionMomento;
+import sigarep.modelos.servicio.transacciones.ListaApelacionEstadoApelacion;
 
 
 public interface IServicioApelacion {
 	
-public List<ListaApelacionMomento> buscarApelaciones(Estudiante estudiante, EstudianteSancionado estudiantesancionado,
+public List<ListaApelacionEstadoApelacion> buscarApelaciones(Estudiante estudiante, EstudianteSancionado estudiantesancionado,
 		SolicitudApelacion solicitudapelacion,
-		ApelacionMomento apelacionmomento, Momento momento);
+		ApelacionEstadoApelacion apelacionEstadoApelacion, EstadoApelacion estadoApelacion);
 
 }
