27e27ff60688e54b05ad3a138e7e317ad2b64d94
rosbe07@gmail.com <rosbe07@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-01-13 20:00:29 +0000

    query  busca los cronogramas donde el codigo del lapso del mismo coincida con el codigo del lapso que entra como parametro

--- Sigarep.webapp/src/sigarep/modelos/repositorio/transacciones/ICronogramaDAO.java
+++ Sigarep.webapp/src/sigarep/modelos/repositorio/transacciones/ICronogramaDAO.java
@@ -1,10 +1,20 @@
 package sigarep.modelos.repositorio.transacciones;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import sigarep.modelos.data.transacciones.Cronograma;
 import sigarep.modelos.data.transacciones.CronogramaPK;
 
 public interface ICronogramaDAO extends JpaRepository<Cronograma, CronogramaPK> {
+	 
+	@Query("select c from Cronograma c where c.estatus = true")
+	public List<Cronograma> buscar();
+
+	@Query("SELECT cro FROM Cronograma cro WHERE cro.id.codigoLapso = :codigoLapso")
+	public List<Cronograma> buscarCronogramas(@Param("codigoLapso")String codigoLapso);
 
 }
