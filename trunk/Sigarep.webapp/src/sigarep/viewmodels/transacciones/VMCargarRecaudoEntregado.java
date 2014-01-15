f1ac99aff15745691b50e1c377d697f723e95979
palencia77@gmail.com <palencia77@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-01-15 04:11:43 +0000

    ViewModels para la transaccion Cargar Recaudos

---
+++ trunk/Sigarep.webapp/src/sigarep/viewmodels/transacciones/VMCargarRecaudoEntregado.java
@@ -0,0 +1,336 @@
+package sigarep.viewmodels.transacciones;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import sigarep.herramientas.Documento;
+
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.ContextParam;
+import org.zkoss.bind.annotation.ContextType;
+import org.zkoss.bind.annotation.ExecutionArgParam;
+import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.util.media.Media;
+import org.zkoss.zhtml.Messagebox;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.event.UploadEvent;
+import org.zkoss.zk.ui.select.Selectors;
+import org.zkoss.zk.ui.select.annotation.VariableResolver;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zk.ui.select.annotation.WireVariable;
+import org.zkoss.zul.Filedownload;
+import org.zkoss.zul.Window;
+import sigarep.herramientas.mensajes;
+import sigarep.modelos.data.transacciones.Soporte;
+import sigarep.modelos.data.transacciones.SoportePK;
+import sigarep.modelos.servicio.transacciones.ListaBuscarRecaudosEntregados;
+import sigarep.modelos.servicio.transacciones.ServicioCargarRecaudoEntregado;
+import sigarep.modelos.servicio.transacciones.ServicioSoporte;
+
+@VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
+public class VMCargarRecaudoEntregado {
+	@Wire("#modalDialog")
+	private Window window;
+	private String cedula;
+	private String sancion;
+	private String programa;
+	private String email;
+	private String apellido;
+	private String nombre;
+	private String lapso;
+	private Integer instancia;
+	private String motivo;
+	private String recaudo;
+	private String segundoNombre;
+	private String segundoApellido;
+	private String nombres;
+	private String apellidos;
+	private String asignatura;
+	private Integer caso;
+	
+	private Documento doc = new Documento();
+	private Media media;
+	mensajes msjs = new mensajes();
+
+	@WireVariable
+	private ServicioCargarRecaudoEntregado serviciocargarrecaudoentregado;
+	@WireVariable
+	private ServicioSoporte serviciosoporte;
+	@WireVariable
+	private List<ListaBuscarRecaudosEntregados> listaRecaudos = new LinkedList<ListaBuscarRecaudosEntregados>();
+
+	public Documento getDoc() {
+		return doc;
+	}
+
+	public void setDoc(Documento doc) {
+		this.doc = doc;
+	}
+
+	public Media getMedia() {
+		return media;
+	}
+
+	public void setMedia(Media media) {
+		this.media = media;
+	}
+
+	public Integer getCaso() {
+		return caso;
+	}
+
+	public void setCaso(Integer caso) {
+		this.caso = caso;
+	}
+
+	public String getAsignatura() {
+		return asignatura;
+	}
+
+	public void setAsignatura(String asignatura) {
+		this.asignatura = asignatura;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public List<ListaBuscarRecaudosEntregados> getListaRecaudos() {
+		return listaRecaudos;
+	}
+
+	public void setListaRecaudos(
+			List<ListaBuscarRecaudosEntregados> listaRecaudos) {
+		this.listaRecaudos = listaRecaudos;
+	}
+
+	public String getRecaudo() {
+		return recaudo;
+	}
+
+	public void setRecaudo(String recaudo) {
+		this.recaudo = recaudo;
+	}
+
+	public String getSegundoNombre() {
+		return segundoNombre;
+	}
+
+	public void setSegundoNombre(String segundoNombre) {
+		this.segundoNombre = segundoNombre;
+	}
+
+	public String getSegundoApellido() {
+		return segundoApellido;
+	}
+
+	public void setSegundoApellido(String segundoApellido) {
+		this.segundoApellido = segundoApellido;
+	}
+
+	public String getMotivo() {
+		return motivo;
+	}
+
+	public void setMotivo(String motivo) {
+		this.motivo = motivo;
+	}
+
+	public Integer getInstancia() {
+		return instancia;
+	}
+
+	public void setInstancia(Integer instancia) {
+		this.instancia = instancia;
+	}
+
+	public String getLapso() {
+		return lapso;
+	}
+
+	public void setLapso(String lapso) {
+		this.lapso = lapso;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getSancion() {
+		return sancion;
+	}
+
+	public void setSancion(String sancion) {
+		this.sancion = sancion;
+	}
+
+	public String getPrograma() {
+		return programa;
+	}
+
+	public void setPrograma(String programa) {
+		this.programa = programa;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void concatenacionNombres() {
+
+		String nombre1 = nombre;
+		String nombre2 = segundoNombre;
+		nombres = nombre1 + " " + nombre2;
+		System.out.println(nombres);
+	}
+
+	public void concatenacionApellidos() {
+
+		String apellido1 = apellido;
+		String apellido2 = segundoApellido;
+		apellidos = apellido1 + " " + apellido2;
+
+	}
+
+	@Init
+	public void init(
+		@ContextParam(ContextType.VIEW) Component view,
+		@ExecutionArgParam("cedula") String v1,
+		@ExecutionArgParam("nombre") String v2,
+		@ExecutionArgParam("apellido") String v3,
+		@ExecutionArgParam("email") String v4,
+		@ExecutionArgParam("programa") String v5,
+		@ExecutionArgParam("sancion") String v6,
+		@ExecutionArgParam("lapso") String v7,
+		@ExecutionArgParam("instancia") Integer v8,
+		@ExecutionArgParam("segundoNombre") String v9,
+		@ExecutionArgParam("segundoApellido") String v10,
+		@ExecutionArgParam("asignatura") String v11,
+		@ExecutionArgParam("caso") Integer v12)
+	{
+		Selectors.wireComponents(view, this, false);
+		this.cedula = v1;
+		this.nombre = v2;
+		this.apellido = v3;
+		this.email = v4;
+		this.programa = v5;
+		this.sancion = v6;
+		this.lapso = v7;
+		this.instancia = v8;
+		this.segundoNombre = v9;
+		this.segundoApellido = v10;
+		this.asignatura = v11;
+		this.caso = v12;
+
+		concatenacionNombres();
+		concatenacionApellidos();
+		media = null;
+		doc = new Documento();
+		buscarRecaudosEntregados(cedula);
+	}
+	
+	@Command
+	@NotifyChange({ "listaRecaudos" })
+	public void buscarRecaudosEntregados(String cedula) {
+		listaRecaudos = serviciocargarrecaudoentregado.buscarRecaudosEntregados(cedula);
+	}
+
+	@Command
+	public void closeThis() {
+		window.detach();
+	}
+
+	@Command
+	public void descargarRecaudoEntregado(@ContextParam(ContextType.COMPONENT) Component componente) {
+		int idRecaudo = Integer.parseInt(componente.getAttribute("idRecaudo").toString());
+		for (int j = 0; j < listaRecaudos.size(); j++) {
+			if (listaRecaudos.get(j).getIdRecaudo() == idRecaudo)
+				Filedownload.save(listaRecaudos.get(j).getContenidoDocumento(),
+				listaRecaudos.get(j).getTipoDocumento(), listaRecaudos.get(j).getNombreDocumento());
+		}
+
+	}
+
+	@Command
+	@NotifyChange({"listaRecaudos" })
+	public void cargarRecaudoEntregado(
+			@ContextParam(ContextType.TRIGGER_EVENT) UploadEvent event,
+			@ContextParam(ContextType.COMPONENT) Component componente) {
+		media = event.getMedia();
+		if (media != null) {
+			if (media.getContentType().equals("image/jpeg")
+				|| media.getContentType().equals("application/pdf")
+				|| media.getContentType().equals("application/msword")
+				|| media.getContentType().equals("application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+				|| media.getContentType().equals("application/vnd.oasis.opendocument.text")
+				|| media.getContentType().equals("application/x-vnd.oasis.opendocument.text")) {
+					doc.setNombreDocumento(media.getName());
+					doc.setTipoDocumento(media.getContentType());
+					doc.setContenidoDocumento(media.getByteData());
+					
+					SoportePK soportePK = new SoportePK(Integer.parseInt(componente.getAttribute("idRecaudo").toString()), 
+														Integer.parseInt(componente.getAttribute("idTipoMotivo").toString()),
+														lapso, cedula, instancia);
+					
+					Soporte soporte = new Soporte(soportePK,true,new Date(),doc);
+					serviciosoporte.guardar(soporte);
+					buscarRecaudosEntregados(cedula);
+					msjs.informacionRegistroCorrecto();
+			} else {
+				Messagebox.show(media.getName()+ " No es un tipo de archivo valido!", "Error",Messagebox.OK, Messagebox.ERROR);
+			}
+		}
+	}
+	
+	@Command
+	@NotifyChange({"listaRecaudos" })
+	public void eliminarRecaudoEntregado(@ContextParam(ContextType.COMPONENT) Component componente) {			
+		SoportePK soportePK = new SoportePK(Integer.parseInt(componente.getAttribute("idRecaudo").toString()), 
+											Integer.parseInt(componente.getAttribute("idTipoMotivo").toString()),
+											lapso, cedula, instancia);
+		serviciosoporte.eliminar(soportePK);
+		buscarRecaudosEntregados(cedula);
+		msjs.informacionEliminarCorrecto();
+	}
+
+}
---
+++ trunk/Sigarep.webapp/src/sigarep/viewmodels/transacciones/VMListaCargarRecaudoEntregado.java
@@ -0,0 +1,314 @@
+package sigarep.viewmodels.transacciones;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.select.annotation.VariableResolver;
+import org.zkoss.zk.ui.select.annotation.WireVariable;
+import org.zkoss.zul.Window;
+
+import sigarep.modelos.data.maestros.ProgramaAcademico;
+import sigarep.modelos.data.maestros.TipoMotivo;
+import sigarep.modelos.data.transacciones.ApelacionEstadoApelacion;
+import sigarep.modelos.servicio.maestros.ServicioProgramaAcademico;
+import sigarep.modelos.servicio.maestros.ServicioTipoMotivo;
+import sigarep.modelos.servicio.transacciones.ListaApelacionEstadoApelacionFiltros;
+import sigarep.modelos.servicio.transacciones.ListaCargarRecaudoEntregado;
+import sigarep.modelos.servicio.transacciones.ListaCargarRecaudoEntregadoFiltros;
+import sigarep.modelos.servicio.transacciones.ListaEstudianteApelacion;
+import sigarep.modelos.servicio.transacciones.ServicioCargarRecaudoEntregado;
+import sigarep.modelos.servicio.transacciones.ServicioListaEstudiate;
+
+@VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
+public class VMListaCargarRecaudoEntregado {
+
+	@WireVariable
+	private ListaCargarRecaudoEntregado listaEstudiantesCargarRecaudos;
+	@WireVariable
+	private String nombrePrograma;
+	@WireVariable
+	private String nombreTipoMotivo;
+	@WireVariable
+	private ServicioTipoMotivo serviciotipomotivo;
+	@WireVariable
+	private ServicioProgramaAcademico servicioprogramaacademico;
+	@WireVariable
+	private ServicioCargarRecaudoEntregado serviciocargarrecaudoentregado;
+	
+	private List<ProgramaAcademico> listaPrograma;
+	private List<TipoMotivo> listaTipoMotivo;
+	private List<ListaCargarRecaudoEntregado> lista = new LinkedList<ListaCargarRecaudoEntregado>();
+	private ListaCargarRecaudoEntregadoFiltros filtros = new ListaCargarRecaudoEntregadoFiltros();
+	private String sancion;
+	private String programa;
+	private String email;
+	private String apellido;
+	private String nombre;
+	private String cedula;
+	private String lapso;
+	private Integer instancia;
+	private String segundoNombre;
+	private String segundoApellido;
+	private String asignatura;
+	private Integer caso;
+	
+	
+	public Integer getCaso() {
+		return caso;
+	}
+
+	public void setCaso(Integer caso) {
+		this.caso = caso;
+	}
+
+	public String getNombrePrograma() {
+		return nombrePrograma;
+	}
+
+	public String getNombreTipoMotivo() {
+		return nombreTipoMotivo;
+	}
+
+	public ServicioTipoMotivo getServiciotipomotivo() {
+		return serviciotipomotivo;
+	}
+
+	public ServicioProgramaAcademico getServicioprogramaacademico() {
+		return servicioprogramaacademico;
+	}
+
+	public ServicioCargarRecaudoEntregado getServicioCargarRecaudoEntregado() {
+		return serviciocargarrecaudoentregado;
+	}
+
+	public List<ListaCargarRecaudoEntregado> getLista() {
+		return lista;
+	}
+
+	public String getSancion() {
+		return sancion;
+	}
+
+	public String getPrograma() {
+		return programa;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public String getLapso() {
+		return lapso;
+	}
+
+	public Integer getInstancia() {
+		return instancia;
+	}
+
+	public void setNombrePrograma(String nombrePrograma) {
+		this.nombrePrograma = nombrePrograma;
+	}
+
+	public void setNombreTipoMotivo(String nombreTipoMotivo) {
+		this.nombreTipoMotivo = nombreTipoMotivo;
+	}
+
+	public void setServiciotipomotivo(ServicioTipoMotivo serviciotipomotivo) {
+		this.serviciotipomotivo = serviciotipomotivo;
+	}
+
+	public void setServicioprogramaacademico(
+			ServicioProgramaAcademico servicioprogramaacademico) {
+		this.servicioprogramaacademico = servicioprogramaacademico;
+	}
+
+	public void setServicioCargarRecaudoEntregado(
+			ServicioCargarRecaudoEntregado servicioCargarRecaudoEntregado) {
+		this.serviciocargarrecaudoentregado = servicioCargarRecaudoEntregado;
+	}
+
+	public void setLista(List<ListaCargarRecaudoEntregado> lista) {
+		this.lista = lista;
+	}
+
+	public void setSancion(String sancion) {
+		this.sancion = sancion;
+	}
+
+	public void setPrograma(String programa) {
+		this.programa = programa;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public void setLapso(String lapso) {
+		this.lapso = lapso;
+	}
+
+	public void setInstancia(Integer instancia) {
+		this.instancia = instancia;
+	}
+
+	public String getSegundoNombre() {
+		return segundoNombre;
+	}
+
+	public String getSegundoApellido() {
+		return segundoApellido;
+	}
+
+	public String getAsignatura() {
+		return asignatura;
+	}
+
+	public void setSegundoNombre(String segundoNombre) {
+		this.segundoNombre = segundoNombre;
+	}
+
+	public void setSegundoApellido(String segundoApellido) {
+		this.segundoApellido = segundoApellido;
+	}
+
+	public void setAsignatura(String asignatura) {
+		this.asignatura = asignatura;
+	}
+
+	@NotifyChange({"filtros"})
+	public ListaCargarRecaudoEntregadoFiltros getFiltros() {
+		return filtros;
+	}
+	
+	public void setFiltros(ListaCargarRecaudoEntregadoFiltros filtros) {
+		this.filtros = filtros;
+	}
+
+	/**
+	 * Otros Metodos
+	 */
+	
+	@Init
+    public void init(){
+		buscarTipoMotivo();
+		buscarProgramaA ();
+		buscarApelacionesRecaudoEntregado();
+    }
+	
+	@Command
+  	@NotifyChange({"listaTipoMotivo"})
+  	public void buscarTipoMotivo(){
+  		listaTipoMotivo = serviciotipomotivo.buscarP(nombreTipoMotivo);
+  	}
+	
+	@Command
+	@NotifyChange({ "listaPrograma" })
+	public void buscarProgramaA() {
+		listaPrograma = servicioprogramaacademico.buscarPr(nombrePrograma);
+		listaPrograma.add(new ProgramaAcademico(-1,"Todos",true));
+	}
+	
+	@Command
+	@NotifyChange({"lista"})
+	public void buscarApelacionesRecaudoEntregado(){
+	  			lista = serviciocargarrecaudoentregado.buscarApelaciones();
+	}
+	
+	@Command
+	@NotifyChange({"cedula", "nombre", "apellido","email", "telefono", "programa", 
+		"sancion", "lapso", "recaudo", "segundoNombre", "segundoApellido", "asignatura"})
+	public void showModal (){
+		cedula = listaEstudiantesCargarRecaudos.getCedulaEstudiante();
+		nombre = listaEstudiantesCargarRecaudos.getPrimerNombre();
+		apellido = listaEstudiantesCargarRecaudos.getPrimerApellido();
+		email = listaEstudiantesCargarRecaudos.getEmail();
+  		programa = listaEstudiantesCargarRecaudos.getPrograma();
+  		sancion = listaEstudiantesCargarRecaudos.getNombreSancion();
+  		lapso = listaEstudiantesCargarRecaudos.getLapso();
+  		instancia = listaEstudiantesCargarRecaudos.getInstancia();
+  		segundoNombre = listaEstudiantesCargarRecaudos.getSegundoNombre();
+  		segundoApellido = listaEstudiantesCargarRecaudos.getSegundoApellido();
+  		asignatura = listaEstudiantesCargarRecaudos.getAsignatura();
+  		caso = listaEstudiantesCargarRecaudos.getCaso();
+  		
+  		final HashMap<String, Object> map = new HashMap<String, Object>();
+	 	map.put("cedula", this.cedula );
+        map.put("nombre", this.nombre);
+        map.put("apellido", this.apellido);
+        map.put("email", this.email);
+        map.put("programa", this.programa);
+        map.put("sancion", this.sancion);
+        map.put("lapso", this.lapso);
+        map.put("instancia", this.instancia);
+        map.put("segundoNombre", this.segundoNombre);
+        map.put("segundoApellido", this.segundoApellido);
+        map.put("asignatura", this.asignatura);
+        map.put("caso", this.caso);
+        
+        final Window window = (Window) Executions.createComponents(
+        		"/WEB-INF/sigarep/vistas/transacciones/CargarRecaudoEntregado.zul", null, map);
+		window.setMaximizable(true);
+		window.doModal();
+  	}
+	
+	@Command
+	@NotifyChange({"lista"})
+	public void filtros(){
+		lista = serviciocargarrecaudoentregado.buscarPorFiltros(filtros);
+	}
+
+	public List<ProgramaAcademico> getListaPrograma() {
+		return listaPrograma;
+	}
+
+	public void setListaPrograma(List<ProgramaAcademico> listaPrograma) {
+		this.listaPrograma = listaPrograma;
+	}
+
+	public List<TipoMotivo> getListaTipoMotivo() {
+		return listaTipoMotivo;
+	}
+
+	public void setListaTipoMotivo(List<TipoMotivo> listaTipoMotivo) {
+		this.listaTipoMotivo = listaTipoMotivo;
+	}
+
+	public ListaCargarRecaudoEntregado getListaEstudiantesCargarRecaudos() {
+		return listaEstudiantesCargarRecaudos;
+	}
+
+	public void setListaEstudiantesCargarRecaudos(
+			ListaCargarRecaudoEntregado listaEstudiantesCargarRecaudos) {
+		this.listaEstudiantesCargarRecaudos = listaEstudiantesCargarRecaudos;
+	}
+}
+
