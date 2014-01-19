af75e64b6c992416cac3602c91f915b2fec469eb
palencia77@gmail.com <palencia77@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-01-19 05:24:14 +0000

    Cambios con respecto a la transaccion Veredicto

--- Sigarep.webapp/src/sigarep/modelos/servicio/transacciones/ServicioVeredicto.java
+++ Sigarep.webapp/src/sigarep/modelos/servicio/transacciones/ServicioVeredicto.java
@@ -21,7 +21,7 @@ import sigarep.modelos.data.transacciones.ApelacionEstadoApelacion;
 import sigarep.modelos.data.maestros.EstadoApelacion;
 
 
-@Service("serviciolistaveredicto")
+@Service("servicioveredicto")
 public class ServicioVeredicto {
 
 	@PersistenceContext
@@ -30,99 +30,66 @@ public class ServicioVeredicto {
 	@Autowired
 	private IApelacionEstadoApelacionDAO apelacionestadoapelacion;
 	
-	public List<ListaApelacionVeredicto1> buscarApelacionesVeredicto() {
-//		String queryStatement = 
-//				"SELECT es.cedula_estudiante, es.primer_nombre, es.primer_apellido," +
-//				" sa.nombre_sancion FROM sigarep.sancionmaestro sa, sigarep.estudiante es " +
-//				" INNER JOIN sigarep.estudiantesancionado AS esa ON es.cedula_estudiante =" +
-//				" esa.cedula_estudiante" +
-//				"INNER JOIN sigarep.solicitudapelacion  AS sa ON esa.cedula_estudiante = " +
-//				"sa.cedula_estudiante" +
-//				"INNER JOIN sigarep.apelacionmomento AS ap ON sa.cedula_estudiante = " +
-//				"ap.cedula_estudiante" +
-//				"INNER JOIN sigarep.momento AS m ON m.id_momento = ap.id_momento" +
-//				"WHERE es.cedula_estudiante = ? AND esa.cedula_estudiante = ? " +
-//				"AND sa.cedula_estudiante = ? AND ap.cedula_estudiante = ? AND m.id_momento = ? ";
-//				//"m.nombre_momento = 'veredicto'";
-//		
+	public List<ListaVeredicto> buscarApelacionesVeredicto1() {
+		
 		String queryStatementVer =
-			"SELECT es.cedula_estudiante, es.primer_nombre, es.primer_apellido," +
-			"sa.nombre_sancion,  p.nombre_programa, esa.indice_grado, esa.codigo_lapso FROM sancion_maestro sa," +
-			"programa_academico p, estudiante_sancionado esa, lapso_academico la, instancia_apelada i,  estudiante es " +
-//			"INNER JOIN estudiante_sancionado AS esa ON es.cedula_estudiante = esa.cedula_estudiante " +
-			"INNER JOIN solicitud_apelacion  AS sap ON es.cedula_estudiante = sap.cedula_estudiante " +
-			"INNER JOIN apelacion_estado_apelacion AS aea ON aea.cedula_estudiante = sap.cedula_estudiante " +
-			"INNER JOIN estado_apelacion AS ea ON ea.id_estado_apelacion = aea.id_estado_apelacion WHERE sa.id_sancion = esa.id_sancion " +
-			"AND ea.nombre_estado = 'verificado' " +
-			"AND es.cedula_estudiante = esa.cedula_estudiante " +
-			"AND esa.codigo_lapso = la.codigo_lapso AND i.id_instancia_apelada = sap.id_instancia_apelada " +
-			"AND sap.id_instancia_apelada = aea.id_instancia_apelada AND es.id_programa= p.id_programa";
-
-		Query query = em.createNativeQuery(queryStatementVer);
-//		query.setParameter(1, estudiante.getCedulaEstudiante());
-//		query.setParameter(1, estudiantesancionado.getId().getCedulaEstudiante());
-//		query.setParameter(2, solicitudapelacion.getId().getCedulaEstudiante());
-//		query.setParameter(3, apelacionmomento.getId().getCedulaEstudiante());
-//		query.setParameter(4, momento.getIdMomento());
-		
-		
+				"SELECT es.cedula_estudiante, es.primer_nombre, es.segundo_nombre, es.primer_apellido, es.segundo_apellido, p.nombre_programa," +
+				"es.email, esa.indice_grado, esa.codigo_lapso, sa.nombre_sancion, i.id_instancia_apelada, sap.numero_caso FROM sancion_maestro sa," +
+				"programa_academico p, estudiante_sancionado esa, lapso_academico la, instancia_apelada i,  estudiante es " + 
+				"INNER JOIN solicitud_apelacion  AS sap ON es.cedula_estudiante = sap.cedula_estudiante " +
+				"WHERE sa.id_sancion = esa.id_sancion " +
+				"AND es.cedula_estudiante = esa.cedula_estudiante " +
+				"AND esa.codigo_lapso = la.codigo_lapso AND i.id_instancia_apelada = sap.id_instancia_apelada " +
+				"AND es.id_programa= p.id_programa " +
+				"AND sap.analizado=TRUE " +
+				"AND i.id_instancia_apelada=1 " +
+				"AND (sap.veredicto IS NULL OR sap.codigo_sesion IS NULL)";
+		Query query = em.createNativeQuery(queryStatementVer);	
 	
 		@SuppressWarnings("unchecked")
 		List<Object[]> resultSet = query.getResultList();
 		
-		List<ListaApelacionVeredicto1> results = new ArrayList<ListaApelacionVeredicto1>();
+		List<ListaVeredicto> results = new ArrayList<ListaVeredicto>();
 		for (Object[] resultRow : resultSet) {
-			System.out.println(resultRow[0]);
-			System.out.println(resultRow[1]);
-			System.out.println(resultRow[2]);
-			System.out.println(resultRow[3]);
-			System.out.println(resultRow[4]);
-			System.out.println(resultRow[5]);
-			System.out.println(resultRow[6]);
 			results.add(
-					new ListaApelacionVeredicto1(
+					new ListaVeredicto(
 							(String) resultRow[0],
 							(String) resultRow[1],
-							(String) resultRow[2], 
-							(String) resultRow[3], 
+							(String) resultRow[2],
+							(String)resultRow[3],
 							(String) resultRow[4], 
-							(Float) resultRow[5], 
-							(String) resultRow[6] ));
+							(String) resultRow[5], 
+							(String) resultRow[6],
+							(Float) resultRow[7], 
+							(String) resultRow[8],
+							(String) resultRow[9],
+							(Integer) resultRow[10],
+							(Integer) resultRow[11]));
 		}
-		
 		return results;
 	}
 
-	public List<ListaApelacionVeredicto1> buscarP(FiltroListaApelacionVeredicto1 filtros){
-		List<ListaApelacionVeredicto1> result = new ArrayList<ListaApelacionVeredicto1>();
+	public List<ListaVeredicto> buscarPorFiltros(FiltroListaApelacionVeredicto1 filtros){
+		List<ListaVeredicto> result = new ArrayList<ListaVeredicto>();
 		String ced = filtros.getCedula().toLowerCase();
 		String nom = filtros.getNombre().toLowerCase();
         String ape = filtros.getApellido().toLowerCase();
         String san = filtros.getSancion().toLowerCase();
         if(ced==null || nom==null || ape==null ||  san==null){
-        	result= buscarApelacionesVeredicto();
+        	result= buscarApelacionesVeredicto1();
         }
         else{
-			for (ListaApelacionVeredicto1 b: buscarApelacionesVeredicto())
+			for (ListaVeredicto b: buscarApelacionesVeredicto1())
 			{
-				if (b.getCedula().toLowerCase().contains(ced)&&
-					b.getNombre().toLowerCase().contains(nom)&&
-					b.getApellido().toLowerCase().contains(ape)&&
-					b.getSancion().toLowerCase().contains(san)){
+				if (b.getCedulaEstudiante().toLowerCase().contains(ced)&&
+					b.getPrimerNombre().toLowerCase().contains(nom)&&
+					b.getPrimerApellido().toLowerCase().contains(ape)&&
+					b.getNombreSancion().toLowerCase().contains(san)){
 					result.add(b);
 				}
 			}
         }
 		return result;
-        } 
-
-//	@Override
-//	public List<ListaApelacionMomento> buscarApelaciones(
-//			EstudianteSancionado estudiantesancionado,
-//			SolicitudApelacion solicitudapelacion,
-//			ApelacionMomento apelacionmomento, Momento momento) {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
+	}
 
 }
