bc830ecf0b4a24b2ece8a188503c7c9bf0a9f82d
mima350@gmail.com <mima350@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-01-20 22:30:57 +0000

    metodo para descargar reglamento desde el portal

--- trunk/Sigarep.webapp/src/sigarep/modelos/servicio/maestros/ServicioReglamento.java
+++ trunk/Sigarep.webapp/src/sigarep/modelos/servicio/maestros/ServicioReglamento.java
@@ -28,88 +28,92 @@ public class ServicioReglamento {
 	
 
 
-public void guardarReglamento(Reglamento r){
-	rg.save(r);
-}
-
-
-//Permite la eliminación lógica del registro, por id, busca el id y cambia su estatus a false, 
-	//la llamada reglamento.save(enlaceBorrarLogico); actualiza el estatus del registro.
-		public void eliminar(Integer idDocumento) {
-			Reglamento reglamentoBorrarLogico = rg.findOne(idDocumento);
-			reglamentoBorrarLogico.setEstatus(false);
-			rg.save(reglamentoBorrarLogico);
+	public void guardarReglamento(Reglamento r){
+		rg.save(r);
+	}
+	
+	
+	//Permite la eliminación lógica del registro, por id, busca el id y cambia su estatus a false, 
+		//la llamada reglamento.save(enlaceBorrarLogico); actualiza el estatus del registro.
+			public void eliminar(Integer idDocumento) {
+				Reglamento reglamentoBorrarLogico = rg.findOne(idDocumento);
+				reglamentoBorrarLogico.setEstatus(false);
+				rg.save(reglamentoBorrarLogico);
+			}
+	
+	public Reglamento buscarReglamento(Integer idDocumento){
+		return rg.findOne(idDocumento);
+	}
+	
+	//Utiliza IDAO, el cual trae todos los registros en true,los que no han sido eliminado logicamente
+			public List<Reglamento> listaReglamento() {
+				return rg.listaReglamentoLogico();
+			}
+	
+	public List<Reglamento> buscarReglamentoTitulo(ReglamentoFiltros filtros, List<Reglamento> listaFiltrada){
+		List<Reglamento> resultado = new LinkedList<Reglamento>();
+		String titulo = filtros.getTitulo().toLowerCase();
+		if (titulo == null){		
+			resultado = listaFiltrada;
 		}
-
-public Reglamento buscarReglamento(Integer idDocumento){
-	return rg.findOne(idDocumento);
-}
-
-//Utiliza IDAO, el cual trae todos los registros en true,los que no han sido eliminado logicamente
-		public List<Reglamento> listaReglamento() {
-			return rg.listaReglamentoLogico();
+		else
+		{
+			for (Reglamento r : listaFiltrada){
+				System.out.print(r);
+				if (r.getTitulo().toLowerCase().contains(titulo)){
+					resultado.add(r);
+				}
+					
+			}		
 		}
-
-public List<Reglamento> buscarReglamentoTitulo(ReglamentoFiltros filtros, List<Reglamento> listaFiltrada){
-	List<Reglamento> resultado = new LinkedList<Reglamento>();
-	String titulo = filtros.getTitulo().toLowerCase();
-	if (titulo == null){		
-		resultado = listaFiltrada;
+		return resultado;
+		
+		
 	}
-	else
-	{
-		for (Reglamento r : listaFiltrada){
-			System.out.print(r);
-			if (r.getTitulo().toLowerCase().contains(titulo)){
-				resultado.add(r);
-			}
-				
-		}		
-	}
-	return resultado;
 	
-	
-}
-
-public List<Reglamento> buscarReglamentoDescripcion(ReglamentoFiltros filtros, List<Reglamento> listaFiltrada){
-	List<Reglamento> resultado = new LinkedList<Reglamento>();
-	String descripcion = filtros.getDescripcion().toLowerCase();
-	if (descripcion == null){		
-		resultado = listaFiltrada;
+	public List<Reglamento> buscarReglamentoDescripcion(ReglamentoFiltros filtros, List<Reglamento> listaFiltrada){
+		List<Reglamento> resultado = new LinkedList<Reglamento>();
+		String descripcion = filtros.getDescripcion().toLowerCase();
+		if (descripcion == null){		
+			resultado = listaFiltrada;
+		}
+		else
+		{
+			for (Reglamento r : listaFiltrada){
+				System.out.print(r);
+				if (r.getDescripcion().toLowerCase().contains(descripcion)){
+					resultado.add(r);
+				}
+					
+			}		
+		}
+		return resultado;
+		
+		
 	}
-	else
-	{
-		for (Reglamento r : listaFiltrada){
-			System.out.print(r);
-			if (r.getDescripcion().toLowerCase().contains(descripcion)){
-				resultado.add(r);
-			}
-				
-		}		
-	}
-	return resultado;
 	
-	
-}
-
-public List<Reglamento> buscarReglamentoCategoria(ReglamentoFiltros filtros, List<Reglamento> listaFiltrada){
-	List<Reglamento> resultado = new LinkedList<Reglamento>();
-	String categoria = filtros.getCategoria().toLowerCase();
-	if (categoria == null){		
-		resultado = listaFiltrada;
+	public List<Reglamento> buscarReglamentoCategoria(ReglamentoFiltros filtros, List<Reglamento> listaFiltrada){
+		List<Reglamento> resultado = new LinkedList<Reglamento>();
+		String categoria = filtros.getCategoria().toLowerCase();
+		if (categoria == null){		
+			resultado = listaFiltrada;
+		}
+		else
+		{
+			for (Reglamento r : listaFiltrada){
+				System.out.print(r);
+				if (r.getCategoria().toLowerCase().contains(categoria)){
+					resultado.add(r);
+				}
+					
+			}		
+		}
+		return resultado;
+		
 	}
-	else
-	{
-		for (Reglamento r : listaFiltrada){
-			System.out.print(r);
-			if (r.getCategoria().toLowerCase().contains(categoria)){
-				resultado.add(r);
-			}
-				
-		}		
-	}
-	return resultado;
 	
-}
+	public List<Reglamento> buscarReglamentoPortal(){
+		return rg.buscarReglamento();
+	}
 
 }
