59d6883adbe547f1abaa88db2731b15966c9c4be
gladmujica@gmail.com <gladmujica@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-01-20 00:39:30 +0000

    Estandarizado Maestro TipoMotivo

--- Sigarep.webapp/src/sigarep/modelos/repositorio/maestros/ITipoMotivoDAO.java
+++ Sigarep.webapp/src/sigarep/modelos/repositorio/maestros/ITipoMotivoDAO.java
@@ -6,11 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import sigarep.modelos.data.maestros.InstanciaApelada;
 import sigarep.modelos.data.maestros.TipoMotivo;
 
 public interface ITipoMotivoDAO extends JpaRepository<TipoMotivo, Integer> {
 
 	@Query("Select tipo FROM TipoMotivo AS tipo WHERE estatus = TRUE")
 	public List<TipoMotivo> buscarTipoMotivoActivas();
+	
+	@Query("SELECT tipo from TipoMotivo AS tipo")
+	public List<TipoMotivo> buscarTodas();
 	
 }
