603e8e17395b6c8c7e40e4298715b519f1a5f471
beleanny17@gmail.com <beleanny17@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-01-21 03:15:36 +0000

    Comentarios de estandares

--- trunk/Sigarep.webapp/src/sigarep/modelos/repositorio/maestros/IRecaudoDAO.java
+++ trunk/Sigarep.webapp/src/sigarep/modelos/repositorio/maestros/IRecaudoDAO.java
@@ -2,6 +2,18 @@ package sigarep.modelos.repositorio.maestros;
 
 import java.util.List;
 
+/*
+ * @ (#) EstadoApelacion.java 
+ *
+ * Copyright 2013 Builder. Todos los derechos reservados.
+ * CONFIDENCIAL. El uso está sujeto a los términos de la licencia.
+ */
+/*
+** Archivo del repositorio  del registro del maestro "Recaudo"
+ * @ Author Beleanny Atacho 
+ * @ Version 1.0, 16/12/13
+*/
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
