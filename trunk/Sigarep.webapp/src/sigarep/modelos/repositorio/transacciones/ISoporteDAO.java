097e82cc685b465905854330020f7f217b6a17fd
palencia77@gmail.com <palencia77@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-01-21 19:59:08 +0000

    Se elimino la clase SoportePK ya que la clase Soporte ahora tiene un Id propio

--- trunk/Sigarep.webapp/src/sigarep/modelos/repositorio/transacciones/ISoporteDAO.java
+++ trunk/Sigarep.webapp/src/sigarep/modelos/repositorio/transacciones/ISoporteDAO.java
@@ -3,7 +3,6 @@ package sigarep.modelos.repositorio.transacciones;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import sigarep.modelos.data.transacciones.Soporte;
-import sigarep.modelos.data.transacciones.SoportePK;
 
 public interface ISoporteDAO extends JpaRepository<Soporte, Integer> {
 
