aaff6634562b5f8e8a37f0b1121fe6856eea73ed
palencia77@gmail.com <palencia77@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-01-21 19:57:14 +0000

    Jesus de babieco

--- Sigarep.webapp/src/sigarep/modelos/data/transacciones/Soporte.java
+++ Sigarep.webapp/src/sigarep/modelos/data/transacciones/Soporte.java
@@ -62,7 +62,7 @@ public class Soporte implements Serializable {
 		return this.idSoporte;
 	}
 
-	public void setId(Integer idSoporte) {
+	public void setIdSoporte(Integer idSoporte) {
 		this.idSoporte = idSoporte;
 	}
 
