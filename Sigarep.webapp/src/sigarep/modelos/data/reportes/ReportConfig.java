c5fe08541688b3cf91a94f0120d08c0df0e89f7a
six1409@gmail.com <six1409@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-01-22 13:46:41 +0000

    Reporte Asignatura con Mayor Cantidad de Sancionados

--- Sigarep.webapp/src/sigarep/modelos/data/reportes/ReportConfig.java
+++ Sigarep.webapp/src/sigarep/modelos/data/reportes/ReportConfig.java
@@ -3,6 +3,8 @@ package sigarep.modelos.data.reportes;
 import java.util.HashMap;
 import java.util.Map;
 import org.zkoss.zk.ui.Sessions;
+
+import sigarep.modelos.data.maestros.ProgramaAcademico;
 import  net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 public class ReportConfig {
 	private String folder = Sessions.getCurrent().getWebApp().getRealPath("/WEB-INF/");
@@ -10,6 +12,7 @@ public class ReportConfig {
 	private Map<String, Object> parametros;
 	private JRBeanCollectionDataSource dataSource;
 	private ReportType type;
+	//private ProgramaAcademico programaAcademico;
 
 	public ReportConfig() {
 		parametros = new HashMap<String, Object>();
@@ -17,6 +20,7 @@ public class ReportConfig {
 		parametros.put("ICON_RIGHT_HEADER", "../../imagenes/imagenes-reportes/logo-decanato.jpg");
 		parametros.put("ICON_FOOTER", "../../imagenes/imagenes-reportes/pie-reporte.jpg");
 		parametros.put("ImagenSuperior", "../../imagenes/imagenes-reportes/cabecera-reporte.jpg");
+		//parametros.put("nombrePrograma", programaAcademico.getIdPrograma());
 		System.out.println(parametros);
 	}
 	
