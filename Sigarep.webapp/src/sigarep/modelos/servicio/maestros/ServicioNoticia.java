c17a6e0fe2ae9d750fa220d9d13cac7d2e2ace25
rosbe07@gmail.com <rosbe07@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-01-22 21:15:05 +0000

    Se agrego documentacion

--- Sigarep.webapp/src/sigarep/modelos/servicio/maestros/ServicioNoticia.java
+++ Sigarep.webapp/src/sigarep/modelos/servicio/maestros/ServicioNoticia.java
@@ -15,32 +15,58 @@ import sigarep.modelos.data.maestros.NoticiaFiltro;
 import sigarep.modelos.lista.ListaGenericaSancionados;
 import sigarep.modelos.repositorio.maestros.INoticiaDAO;
 
+/**Noticia
+ * UCLA DCYT Sistemas de Informacion.
+ * @author Equipo : Builder-Sigarep Lapso 2013-2
+ * @version 1.0
+ * @since 22/01/14
+ */
 @Service("servicionoticia")
 public class ServicioNoticia {
 	private @Autowired
 	INoticiaDAO iNoticia;
 
+	/** guardar
+	 * @param noticia.
+	 * @return No devuelve ningun valor.
+	 */
 	public void guardar(Noticia noticia) {
 		iNoticia.save(noticia);
 	}
-
+	/** eliminar
+	 * @param idNoticia
+	 * @return No devuelve ningun valor.
+	 */
 	public void eliminar(Integer idNoticia) {
 		Noticia n = iNoticia.findOne(idNoticia);
 		n.setEstatus(false);
 		iNoticia.save(n);
 	}
 
+	/** listadoNoticia
+	 * @param una listadoNoticia
+	 * @return No devuelve ningun valor.
+	 */
 	public List<Noticia> listadoNoticia() {
 		List<Noticia> noticiaLista = iNoticia.busar();
 		return noticiaLista;
 	}
-	
+
+	/** buscarn
+	 * @param idNoticia
+	 * @return r es una lista de Noticia.
+	 */
 	public  List<Noticia> buscarn(Integer idNoticia) {
 		List<Noticia> r = new LinkedList<Noticia>();
 		r = iNoticia.findAll();
 		return r;
 	}
 
+	/** buscarNoticia
+	 * @param nombre
+	 * @return resultado es un listadoNoticia.
+	 * @throws la Excepcion es que el nombre este en blanco.
+	 */
 	public List<Noticia> buscarNoticia(String nombre) {
 		List<Noticia> resultado = new LinkedList<Noticia>();
 		if (nombre == null || "".equals(nombre)) {
@@ -56,41 +82,49 @@ public class ServicioNoticia {
 		}
 		return resultado;
 	}
-	
+
+	/** buscarNoticias
+	 * @param noticias
+	 * @return result que es un listadoNoticia.
+	 */
 	public List<Noticia> buscarNoticias(NoticiaFiltro noticias){
 		List<Noticia> result = new ArrayList<Noticia>();
-        String titulo = noticias.getTitulo().toLowerCase();
-        String contenido = noticias.getContenido().toLowerCase();
-        if(titulo==null || contenido==null){
-        	result= listadoNoticia();
-        }
-        else{
+		String titulo = noticias.getTitulo().toLowerCase();
+		String contenido = noticias.getContenido().toLowerCase();
+		if(titulo==null || contenido==null){
+			result= listadoNoticia();
+		}
+		else{
 			for (Noticia n: listadoNoticia())
 			{
 				if (n.getTitulo().toLowerCase().contains(titulo)&&
-					n.getContenido().toLowerCase().contains(contenido)){
+						n.getContenido().toLowerCase().contains(contenido)){
 					result.add(n);
 				}
 			}
-        }
+		}
 		return result;
-        } 
-	
+	} 
+
+	/** filtrarApelacionesCargarRecaudo
+	 * @param titulof
+	 * @return result que es un listadoNoticia.
+	 * @throws las Excepciones son que el titulo sea null
+	 */
 	public List<Noticia> filtrarApelacionesCargarRecaudo(String titulof){
 		List<Noticia> result = new ArrayList<Noticia>();
-        if(titulof==null){
-        	result= listadoNoticia();
-        }
-        else{
+		if(titulof==null){
+			result= listadoNoticia();
+		}
+		else{
 			for (Noticia n : listadoNoticia())
 			{
 				if (n.getTitulo().toLowerCase().contains(titulof.toLowerCase())){
 					result.add(n);
 				}
 			}
-        }
+		}
 		return result;
 	} 
-	
-}
 
+}
