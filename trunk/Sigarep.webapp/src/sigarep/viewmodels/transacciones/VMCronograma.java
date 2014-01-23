3f7c7ec84bd4d271fca35792bda74fbbc3c5febe
rosbe07@gmail.com <rosbe07@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-01-23 19:16:23 +0000

    Se agrego documentacion y filtros

--- trunk/Sigarep.webapp/src/sigarep/viewmodels/transacciones/VMCronograma.java
+++ trunk/Sigarep.webapp/src/sigarep/viewmodels/transacciones/VMCronograma.java
@@ -2,6 +2,7 @@ package sigarep.viewmodels.transacciones;
 import java.io.IOException;
 import java.sql.Time;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.persistence.Temporal;
@@ -28,258 +29,251 @@ import org.zkoss.zul.Timebox;
 import org.zkoss.zul.Window;
 
 import sigarep.modelos.data.maestros.Actividad;
+import sigarep.modelos.data.maestros.InstanciaApelada;
 import sigarep.modelos.data.maestros.LapsoAcademico;
 import sigarep.modelos.data.maestros.TipoMotivo;
 import sigarep.modelos.data.transacciones.Cronograma;
 import sigarep.modelos.data.transacciones.CronogramaPK;
+import sigarep.modelos.lista.ListaGenericaSancionados;
 import sigarep.modelos.servicio.maestros.ServicioActividad;
+import sigarep.modelos.servicio.maestros.ServicioInstanciaApelada;
 import sigarep.modelos.servicio.maestros.ServicioLapsoAcademico;
 import sigarep.modelos.servicio.transacciones.ServicioCronograma;
+
+/**Cronograma
+ * UCLA DCYT Sistemas de Informacion.
+ * @author Equipo : Builder-Sigarep Lapso 2013-2
+ * @version 1.0
+ * @since 22/01/14
+ */
 
 @SuppressWarnings("serial")
 @VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
 public class VMCronograma {
 
 	@WireVariable ServicioCronograma serviciocronograma;
-	private CronogramaPK id;
-	private Boolean estatus;
 	private Date fechaInicio;
 	private Date fechaFin;
 	private Time horaInicio;
 	private String lugar;
 	private String observacion;
-	private String codigoLapso;
+	private String codigoLapso;	
+	private String lugarf="";
+	private String actividadf="";
+	private String responsablef="";
 	private LapsoAcademico lapsoActivo;
-	@WireVariable
 	private LapsoAcademico lapsoAcademico = new LapsoAcademico();
-	@WireVariable
 	private Actividad actividad = new Actividad();
+	private InstanciaApelada responsable = new InstanciaApelada();
 	@WireVariable
 	private ServicioLapsoAcademico serviciolapsoacademico;
 	@WireVariable
 	private ServicioActividad servicioactividad;
-	
-	@Wire
-	private Textbox txtLapsoAcademico;
-	@Wire
-	private Combobox cmbActividad;
-	@Wire
-	private Datebox dtbFechaInicio;
-	@Wire
-	private Datebox dtbFechaFin;
-	@Wire
-	private Textbox txtLugar;
-	@Wire
-	private Timebox tmbHora;
-	@Wire
-	private Textbox txtObservacion;
-	@Wire Window ventana;
-	
 	@WireVariable
-	private List<Cronograma> listaCronograma;
-	@WireVariable
+	private ServicioInstanciaApelada servicioInstanciaApelada;
+	private List<Cronograma> listaCronograma = new LinkedList<Cronograma>(); //Lista de Cronogramas
 	private List<Actividad> listaActividad;
-	@WireVariable
+	private List<InstanciaApelada> listaResponsable;
 	private Cronograma cronogramaSeleccionado;
 	CronogramaPK cronogramaPK = new CronogramaPK();
 	Cronograma cronograma = new Cronograma();
-	mensajes msjs = new mensajes(); //para llamar a los diferentes mensajes de dialogo
-	MensajesAlUsuario mensajesAlUsuario = new MensajesAlUsuario();
+	MensajesAlUsuario mensajesAlUsuario = new MensajesAlUsuario();//Llama a los diferentes mensajes de dialogo
 
 	// Metodos GETS Y SETS
-    
+	public String getLugarf() {
+		return lugarf;
+	}
+	public String getResponsablef() {
+		return responsablef;
+	}
+	public String getActividadf() {
+		return actividadf;
+	}
+	public void setLugarf(String lugarf) {
+		this.lugarf = lugarf;
+	}
+	public void setResponsablef(String responsablef) {
+		this.responsablef = responsablef;
+	}
+	public void setActividadf(String actividadf) {
+		this.actividadf = actividadf;
+	}
+	public List<InstanciaApelada> getListaResponsable() {
+		return listaResponsable;
+	}
+	public void setListaResponsable(List<InstanciaApelada> listaResponsable) {
+		this.listaResponsable = listaResponsable;
+	}
+	public InstanciaApelada getResponsable() {
+		return responsable;
+	}
+	public void setResponsable(InstanciaApelada responsable) {
+		this.responsable = responsable;
+	}
 	public List<Actividad> getListaActividad() {
 		return listaActividad;
 	}
-
 	public void setListaActividad(List<Actividad> listaActividad) {
 		this.listaActividad = listaActividad;
 	}
-
 	public ServicioCronograma getServiciocronograma() {
 		return serviciocronograma;
 	}
-
-	public CronogramaPK getId() {
-		return id;
-	}
-
-	public Boolean getEstatus() {
-		return estatus;
-	}
-
 	public Date getFechaInicio() {
 		return fechaInicio;
 	}
-
 	public Date getFechaFin() {
 		return fechaFin;
 	}
-
 	public Time getHoraInicio() {
 		return horaInicio;
 	}
-
 	public String getLugar() {
 		return lugar;
 	}
-
 	public String getObservacion() {
 		return observacion;
 	}
-
 	public LapsoAcademico getLapsoAcademico() {
 		return lapsoAcademico;
 	}
-
 	public Actividad getActividad() {
 		return actividad;
 	}
-
 	public ServicioLapsoAcademico getServiciolapsoacademico() {
 		return serviciolapsoacademico;
 	}
-
 	public ServicioActividad getServicioactividad() {
 		return servicioactividad;
 	}
-
 	public List<Cronograma> getListaCronograma() {
 		return listaCronograma;
 	}
 	@Command
-	@NotifyChange({"fechaInicio", "fechaFin", "horaInicio", "lugar", "listaCronograma"})
+	@NotifyChange({"fechaInicio", "fechaFin", "horaInicio", "lugar", "observacion", "responsable", "listaCronograma"})
 	public Cronograma getCronogramaSeleccionado() {
 		return cronogramaSeleccionado;
 	}
 	public void setServiciocronograma(ServicioCronograma serviciocronograma) {
 		this.serviciocronograma = serviciocronograma;
 	}
-
-	public void setId(CronogramaPK id) {
-		this.id = id;
-	}
-
-	public void setEstatus(Boolean estatus) {
-		this.estatus = estatus;
-	}
-
 	public void setFechaInicio(Date fechaInicio) {
 		this.fechaInicio = fechaInicio;
 	}
-
 	public void setFechaFin(Date fechaFin) {
 		this.fechaFin = fechaFin;
 	}
-
 	public void setHoraInicio(Time horaInicio) {
 		this.horaInicio = horaInicio;
 	}
-
 	public void setLugar(String lugar) {
 		this.lugar = lugar;
 	}
-
 	public void setObservacion(String observacion) {
 		this.observacion = observacion;
 	}
-
 	public void setLapsoAcademico(LapsoAcademico lapsoAcademico) {
 		this.lapsoAcademico = lapsoAcademico;
 	}
-
 	public void setActividad(Actividad actividad) {
 		this.actividad = actividad;
 	}
-
 	public void setServiciolapsoacademico(ServicioLapsoAcademico serviciolapsoacademico) {
 		this.serviciolapsoacademico = serviciolapsoacademico;
 	}
-
 	public void setServicioactividad(ServicioActividad servicioactividad) {
 		this.servicioactividad = servicioactividad;
 	}
-
 	public void setListaCronograma(List<Cronograma> listaCronograma) {
 		this.listaCronograma = listaCronograma;
 	}
-
 	public void setCronogramaSeleccionado(Cronograma cronogramaSeleccionado) {
 		this.cronogramaSeleccionado = cronogramaSeleccionado;
 	}
-
 	public String getCodigoLapso() {
 		return codigoLapso;
 	}
-
 	public void setCodigoLapso(String codigoLapso) {
 		this.codigoLapso = codigoLapso;
 	}
-
 	public LapsoAcademico getLapsoActivo() {
 		return lapsoActivo;
 	}
-
 	public void setLapsoActivo(LapsoAcademico lapsoActivo) {
 		this.lapsoActivo = lapsoActivo;
 	}
-    //Fin de los metodod gets y sets
-	
-    // OTROS METODOS
+
+	// OTROS METODOS
 	@Init
 	public void init(){
-        //initialization code
+		//initialization code
 		buscarCronograma();
 		buscarActividad();
+		buscarResponsable();
 		lapsoActivo = serviciolapsoacademico.encontrarLapsoActivo();
 		if (serviciolapsoacademico.encontrarLapsoActivo() == null){
 			mensajesAlUsuario.ErrorLapsoActivoNoExistente();
-			
+
 		}
 		else
 			codigoLapso = lapsoActivo.getCodigoLapso();
-    }
+	}
 
-		@Command
-		@NotifyChange({"fechaInicio", "fechaFin", "horaInicio", "lugar", "observacion", "listaCronograma", "actividad"})
-		public void guardarCronograma() {
-			if (codigoLapso == null)
-				mensajesAlUsuario.ErrorLapsoActivoNoExistente();
-			else{
-				if(fechaInicio==null || fechaFin ==null || horaInicio ==null || lugar.equals(""))
-					msjs.advertenciaLlenarCampos();
-				else {
-					cronogramaPK.setIdActividad(actividad.getIdActividad());
-					cronogramaPK.setCodigoLapso(codigoLapso);
-					
-					try {
-						serviciocronograma.guardar(new Cronograma(cronogramaPK, true, fechaFin, fechaInicio, horaInicio, lugar, observacion));
-		
-					} catch (Exception e) {
-						System.out.println(e.getMessage());
-					}
-					msjs.informacionRegistroCorrecto();
-					buscarCronograma();
-					limpiar();
+	/** guardarCronograma. 
+	 * @param fechaInicio, fechaFin, horaInicio, lugar, observacion, responsable, listaCronograma, actividad.
+	 * @return No devuelve ningun valor.
+	 * @throws las Excepciones son que se quiera registrar un cronograma y no haya un lapso academico activo y
+	 *         que quiera guardar con campos aun en blanco.
+	 */
+	@Command
+	@NotifyChange({"fechaInicio", "fechaFin", "horaInicio", "lugar", "observacion", "responsable", "listaCronograma", "actividad"})
+	public void guardarCronograma() {
+		if (codigoLapso == null)
+			mensajesAlUsuario.ErrorLapsoActivoNoExistente();
+		else{
+			if(fechaInicio==null || fechaFin ==null || horaInicio ==null || lugar.equals(""))
+				mensajesAlUsuario.advertenciaLlenarCampos();
+			else {
+				cronogramaPK.setIdActividad(actividad.getIdActividad());
+				cronogramaPK.setCodigoLapso(codigoLapso);
+
+				try {
+					responsable=servicioInstanciaApelada.buscar(responsable.getIdInstanciaApelada());
+					serviciocronograma.guardar(new Cronograma(cronogramaPK, true, fechaFin, fechaInicio, horaInicio, lugar, observacion, responsable));
+
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
 				}
+				mensajesAlUsuario.informacionRegistroCorrecto();
+				buscarCronograma();
+				limpiar();
 			}
 		}
-	
-		@Command
-		@NotifyChange({"fechaInicio", "fechaFin", "horaInicio", "lugar", "observacion","actividad",  "listaCronograma" })
-	    public void buscarDetalleActividadCombo() {
+	}
+
+	/** buscarDetalleActividadCombo. Al escoger una actividad en el combo, busca los datos asociados a ella(el detalle)
+	 * @param fechaInicio, fechaFin, horaInicio, lugar, observacion, responsable, listaCronograma, actividad..
+	 * @return No devuelve ningun valor.
+	 * @throws la Excepcion es que el Id de actividad no coincida con el Id de la actividad que se encuentra 
+	 *         en la lista, es decir que no lo consiga en ella.
+	 */
+	@Command
+	@NotifyChange({"fechaInicio", "fechaFin", "horaInicio", "lugar", "observacion", "responsable", "actividad",  "listaCronograma" })
+	public void buscarDetalleActividadCombo() {
 		boolean check=false;
 		for (int i = 0; i < listaCronograma.size(); i++) {
 			if(actividad.getIdActividad() == listaCronograma.get(i).getActividad().getIdActividad()){
 				fechaInicio=listaCronograma.get(i).getFechaInicio();
-			    fechaFin=listaCronograma.get(i).getFechaFin();
-			    lugar=listaCronograma.get(i).getLugar();
-			    horaInicio=listaCronograma.get(i).getHoraInicio();
-			    observacion=listaCronograma.get(i).getObservacion();
-			   
-			    check=true;
+				fechaFin=listaCronograma.get(i).getFechaFin();
+				lugar=listaCronograma.get(i).getLugar();
+				horaInicio=listaCronograma.get(i).getHoraInicio();
+				observacion=listaCronograma.get(i).getObservacion();
+				responsable=listaCronograma.get(i).getResponsable();
+
+
+				check=true;
 			}
-		
+
 		}
 		if(!check){
 			fechaInicio=null;
@@ -287,25 +281,50 @@ public class VMCronograma {
 			horaInicio=null;
 			lugar="";
 			observacion="";
+			responsable = new InstanciaApelada();
 		}
 	}
-	
-	//Metodo que busca una noticia partiendo por su titulo
+
+	/** listaCronograma.
+	 * @param listaCronograma.
+	 * @return La listaCronograma cargada con los cronogramas.
+	 * @throws No dispara ninguna excepcion.
+	 */
 	@Command
 	@NotifyChange({"listaCronograma"})
 	public void buscarCronograma(){
 		listaCronograma =serviciocronograma.listadoCronograma();
 	}
-	
-	
+
+	/** listaResponsable.
+	 * @param listaResponsable.
+	 * @return La listaResponsable cargada con los responsables (Instancia Apelada).
+	 * @throws No dispara ninguna excepcion.
+	 */
+	@Command
+	@NotifyChange({"listaResponsable"})
+	public void buscarResponsable(){
+		listaResponsable =servicioInstanciaApelada.listadoInstanciaApelada(); 
+	}
+
+	/** listaActividad.
+	 * @param listaActividad.
+	 * @return La listaActividad cargada con las actividades.
+	 * @throws No dispara ninguna excepcion.
+	 */
 	@Command
 	@NotifyChange({"listaActividad"})
 	public void buscarActividad(){
 		listaActividad =servicioactividad.listadoActividad();
 	}
-	//Metodo que limpia todos los campos de la pantalla
+
+	/** limpiar. Limpia todos los campos.
+	 * @param fechaInicio, fechaFin, horaInicio, lugar, observacion, responsable, listaCronograma, actividad..
+	 * @return No devuelve ningun valor.
+	 * @throws No dispara ninguna excepcion.
+	 */
 	@Command
-	@NotifyChange({"fechaInicio", "fechaFin", "horaInicio", "lugar", "observacion", "listaCronograma", "actividad"})
+	@NotifyChange({"fechaInicio", "fechaFin", "horaInicio", "lugar", "observacion", "responsable", "listaCronograma", "actividad"})
 	public void limpiar(){
 		// se utiliza la fecha del sistema para colocarla al momento de limpiar
 		fechaInicio=null;
@@ -314,44 +333,73 @@ public class VMCronograma {
 		lugar="";
 		observacion="";
 		actividad= new Actividad();
-		
+		responsable = new InstanciaApelada();
+
 	}
-	
-	//Metodo que elimina una noticia tomando en cuenta el idNoticia
+
+	/** eliminar.
+	 * @param fechaInicio, fechaFin, horaInicio, lugar, observacion, responsable, listaCronograma, actividad.
+	 * @return No devuelve ningun valor.
+	 * @throws la Excepcion es que quiera eliminar con los campos vacios, sin seleccionar ningun registro
+	 */
 	@Command
-	@NotifyChange({"fechaInicio", "fechaFin", "horaInicio", "lugar", "observacion", "listaCronograma"})
+	@NotifyChange({"fechaInicio", "fechaFin", "horaInicio", "lugar", "observacion", "responsable", "listaCronograma", "actividad"})
 	public void eliminar(){
-		serviciocronograma.eliminarCronograma(getCronogramaSeleccionado().getId());
-		limpiar();
-		msjs.informacionEliminarCorrecto();
-		buscarCronograma();
-		
+
+		if(fechaInicio==null || fechaFin ==null || horaInicio ==null || lugar.equals(""))
+			mensajesAlUsuario.advertenciaSeleccionarParaEliminar();
+		else {
+			serviciocronograma.eliminarCronograma(getCronogramaSeleccionado().getId());
+			limpiar();
+			mensajesAlUsuario.informacionEliminarCorrecto();
+			buscarCronograma();
+		}
+
 	}
-	
-	//permite tomar los datos del objeto noticiaseleccionada
+
+	/** mostrarSeleccionado. Muestra los datos del cronograma seleccionado previamente.
+	 * @param fechaInicio, fechaFin, horaInicio, lugar, observacion, responsable, listaCronograma, actividad.
+	 * @return No devuelve ningun valor.
+	 * @throws No dispara ninguna excepcion.
+	 */
 	@Command
-	@NotifyChange({"fechaInicio", "fechaFin", "horaInicio", "lugar", "observacion", "listaCronograma", "actividad"})
+	@NotifyChange({"fechaInicio", "fechaFin", "horaInicio", "lugar", "observacion", "responsable", "listaCronograma", "actividad"})
 	public void mostrarSeleccionado(){
-		 Cronograma cro = getCronogramaSeleccionado();
-		 fechaInicio = cro.getFechaInicio();
-		 fechaFin = cro.getFechaFin();
-		 horaInicio = cro.getHoraInicio();
-		 lugar = cro.getLugar();
-		 observacion = cro.getObservacion();
-		 actividad=getCronogramaSeleccionado().getActividad();
+		Cronograma cro = getCronogramaSeleccionado();
+		fechaInicio = cro.getFechaInicio();
+		fechaFin = cro.getFechaFin();
+		horaInicio = cro.getHoraInicio();
+		lugar = cro.getLugar();
+		observacion = cro.getObservacion();
+		actividad=cro.getActividad();
+		responsable=cro.getResponsable();
 	}
-	
+
+	/** validarFecha.
+	 * @param fechaInicio, fechaFin.
+	 * @return No devuelve ningun valor.
+	 * @throws la Excepcion es que quiera introducir en fecha fin una fecha menor o antes que la fecha de inicio.
+	 */
 	@Command
 	@NotifyChange({"fechaInicio", "fechaFin"})
 	public void validarFecha(){
 		if (fechaInicio != null && fechaFin != null){
 			if (fechaInicio.compareTo(fechaFin) > 0){
-				Messagebox.show("Rango de fechas Invalido", "Advertencia", Messagebox.OK, Messagebox.ERROR);
+				mensajesAlUsuario.ErrorRangoFechas();
 				fechaFin=null;
 			}
 		}
 	}
-	
-	
-//Fin de los otros metodos.
+
+	/** filtroCronograma. Hace filtrado de los campos mencionados que entran como parametro.
+	 * @param listaConograma, responsablef, lugarf, actividadf.
+	 * @return No devuelve ningun valor.
+	 * @throws No dispara ninguna excepcion.
+	 */
+	@Command
+	@NotifyChange({"listaCronograma", "responsablef", "lugarf", "actividadf"})
+	public void filtroCronograma(){
+		listaCronograma = serviciocronograma.filtrarCronograma(responsablef, lugarf, actividadf);
+	}
 }
+
