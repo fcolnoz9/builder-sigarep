76bb779f5c9e1cd23b2e9e0e1656bcfa3cca0cf8
rosbe07@gmail.com <rosbe07@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-01-23 19:16:56 +0000

    Se agrego documentacion y filtros

--- trunk/Sigarep.webapp/src/sigarep/viewmodels/transacciones/VMHistorialCronograma.java
+++ trunk/Sigarep.webapp/src/sigarep/viewmodels/transacciones/VMHistorialCronograma.java
@@ -2,12 +2,14 @@ package sigarep.viewmodels.transacciones;
 import java.io.IOException;
 import java.sql.Time;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import sigarep.herramientas.Archivo;
+import sigarep.herramientas.MensajesAlUsuario;
 import sigarep.herramientas.mensajes;
 
 import org.zkoss.bind.annotation.Command;
@@ -27,6 +29,7 @@ import org.zkoss.zul.Timebox;
 import org.zkoss.zul.Window;
 
 import sigarep.modelos.data.maestros.Actividad;
+import sigarep.modelos.data.maestros.InstanciaApelada;
 import sigarep.modelos.data.maestros.LapsoAcademico;
 import sigarep.modelos.data.maestros.TipoMotivo;
 import sigarep.modelos.data.transacciones.Cronograma;
@@ -35,136 +38,131 @@ import sigarep.modelos.servicio.maestros.ServicioActividad;
 import sigarep.modelos.servicio.maestros.ServicioLapsoAcademico;
 import sigarep.modelos.servicio.transacciones.ServicioCronograma;
 
+/**Historial de Cronograma
+ * UCLA DCYT Sistemas de Informacion.
+ * @author Equipo : Builder-Sigarep Lapso 2013-2
+ * @version 1.0
+ * @since 22/01/14
+ */
+
 @SuppressWarnings("serial")
 @VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
 public class VMHistorialCronograma {
 
-	@WireVariable ServicioCronograma serviciocronograma;
-	private Date fechaInicio;
-	private Date fechaFin;
-	private Time horaInicio;
-	private String lugar;
-	private String observacion;
-	@WireVariable
-	private LapsoAcademico lapsoAcademico = new LapsoAcademico();
-	@WireVariable
-	private Actividad actividad = new Actividad();
+	@WireVariable 
+	private ServicioCronograma serviciocronograma;
 	@WireVariable
 	private ServicioLapsoAcademico serviciolapsoacademico;
-	@WireVariable
-	private List<Cronograma> listaCronogramas;
-	@WireVariable
-	private List<LapsoAcademico> listaLapsoAcademico;
-	@WireVariable
-	mensajes msjs = new mensajes(); //para llamar a los diferentes mensajes de dialogo
+	private String lugarf=""; // Inicializacion de variable lugarf para filtrar
+	private String responsablef="";// Inicializacion de variable responsablef para filtrar
+	private String actividadf="";// Inicializacion de variable actividadf para filtrar
+	private LapsoAcademico lapsoAcademico = new LapsoAcademico();
+	private Actividad actividad = new Actividad();
+	private InstanciaApelada  responsable = new InstanciaApelada();
+	private List<Cronograma> listaCronogramas = new LinkedList<Cronograma>();//Lista de Cronogramas
+	private List<LapsoAcademico> listaLapsoAcademico;//Lista de Lapso Academico
+	MensajesAlUsuario mensajesAlUsuario = new MensajesAlUsuario();//Llama a los diferentes mensajes de dialogo
 
 	// Metodos GETS Y SETS
-
+	public String getLugarf() {
+		return lugarf;
+	}
+	public String getResponsablef() {
+		return responsablef;
+	}
+	public String getActividadf() {
+		return actividadf;
+	}
+	public void setLugarf(String lugarf) {
+		this.lugarf = lugarf;
+	}
+	public void setResponsablef(String responsablef) {
+		this.responsablef = responsablef;
+	}
+	public void setActividadf(String actividadf) {
+		this.actividadf = actividadf;
+	}
+	public InstanciaApelada getResponsable() {
+		return responsable;
+	}
+	public void setResponsable(InstanciaApelada responsable) {
+		this.responsable = responsable;
+	}
 	public List<Cronograma> getListaCronogramas() {
 		return listaCronogramas;
 	}
-
 	public void setListaCronogramas(List<Cronograma> listaCronogramas) {
 		this.listaCronogramas = listaCronogramas;
 	}
-
 	public List<LapsoAcademico> getListaLapsoAcademico() {
 		return listaLapsoAcademico;
 	}
-
 	public void setListaLapsoAcademico(List<LapsoAcademico> listaLapsoAcademico) {
 		this.listaLapsoAcademico = listaLapsoAcademico;
 	}
-
 	public ServicioCronograma getServiciocronograma() {
 		return serviciocronograma;
 	}
-	public Date getFechaInicio() {
-		return fechaInicio;
-	}
-
-	public Date getFechaFin() {
-		return fechaFin;
-	}
-
-	public Time getHoraInicio() {
-		return horaInicio;
-	}
-
-	public String getLugar() {
-		return lugar;
-	}
-
-	public String getObservacion() {
-		return observacion;
-	}
-
 	public LapsoAcademico getLapsoAcademico() {
 		return lapsoAcademico;
 	}
-
 	public Actividad getActividad() {
 		return actividad;
 	}
-
 	public ServicioLapsoAcademico getServiciolapsoacademico() {
 		return serviciolapsoacademico;
 	}
-
 	public void setServiciocronograma(ServicioCronograma serviciocronograma) {
 		this.serviciocronograma = serviciocronograma;
 	}
-
-	public void setFechaInicio(Date fechaInicio) {
-		this.fechaInicio = fechaInicio;
-	}
-
-	public void setFechaFin(Date fechaFin) {
-		this.fechaFin = fechaFin;
-	}
-
-	public void setHoraInicio(Time horaInicio) {
-		this.horaInicio = horaInicio;
-	}
-
-	public void setLugar(String lugar) {
-		this.lugar = lugar;
-	}
-
-	public void setObservacion(String observacion) {
-		this.observacion = observacion;
-	}
-
 	public void setLapsoAcademico(LapsoAcademico lapsoAcademico) {
 		this.lapsoAcademico = lapsoAcademico;
 	}
-
 	public void setActividad(Actividad actividad) {
 		this.actividad = actividad;
 	}
-
 	public void setServiciolapsoacademico(ServicioLapsoAcademico serviciolapsoacademico) {
 		this.serviciolapsoacademico = serviciolapsoacademico;
 	}
-    //Fin de los metodod gets y sets
-	
-    // OTROS METODOS
-	
+
+	// OTROS METODOS
 	@Init
 	public void init(){
-        //initialization code
+		//initialization code
 		buscarLapsoAcademico();
-    }
+	}
 
+	/** buscarLapsoAcademico.
+	 * @param listaLapsoAcademico cargada con los lapsos academicos.
+	 * @return No devuelve ningun valor.
+	 * @throws No dispara ninguna excepcion.
+	 */
 	@Command
 	@NotifyChange({"listaLapsoAcademico"})
 	public void buscarLapsoAcademico(){
 		listaLapsoAcademico =serviciolapsoacademico.buscarTodosLosLapsos();
 	}
+
+	/** buscarDetalleLapsoAcademicoCombo. Al escoger una lapso en el combo, busca los datos asociados a el(el detalle)
+	 * @param fechaInicio, fechaFin, horaInicio, lugar, observacion, responsable, listaCronograma, listaLapsoAcademico, lapsoAcademico, actividad.
+	 * @return No devuelve ningun valor.
+	 * @throws No dispara ninguna excepcion.
+	 */
 	@Command
-	@NotifyChange({"fechaInicio", "fechaFin", "horaInicio", "lugar", "observacion", "listaCronogramas", "listaLapsoAcademico", "lapsoAcademico"})
+	@NotifyChange({"fechaInicio", "fechaFin", "horaInicio", "lugar", "observacion", "responsable", "listaCronogramas", "listaLapsoAcademico", "lapsoAcademico", "actividad"})
 	public void buscarDetalleLapsoAcademicoCombo() {
 		listaCronogramas = serviciocronograma.buscarTodosCronogramas(lapsoAcademico.getCodigoLapso());
 	}
-//Fin de los otros metodos.
+
+	/** filtroCronograma. Hace filtrado de los campos mencionados que entran como parametro.
+	 * @param listaConogramas, responsablef, lugarf, actividadf.
+	 * @return No devuelve ningun valor.
+	 * @throws No dispara ninguna excepcion.
+	 */
+	@Command
+	@NotifyChange({"listaCronogramas", "responsablef", "lugarf", "actividadf"})
+	public void filtroCronograma(){
+		listaCronogramas = serviciocronograma.filtrarCronograma(responsablef, lugarf, actividadf);
+	}
+
 }
