4599d89f4114bafbad4ace62654d4326692eb6ca
marinelperaza@gmail.com <marinelperaza@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-01-25 15:32:56 +0000

    Mensajes Comentarios de los metodos

--- Sigarep.webapp/src/sigarep/modelos/servicio/maestros/ServicioReglamento.java
+++ Sigarep.webapp/src/sigarep/modelos/servicio/maestros/ServicioReglamento.java
@@ -9,30 +9,46 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import sigarep.modelos.data.maestros.EnlaceInteres;
-import sigarep.modelos.data.maestros.Recaudo;
-import sigarep.modelos.data.maestros.RecaudoFiltro;
+
+
 import sigarep.modelos.data.maestros.Reglamento;
 import sigarep.modelos.data.maestros.ReglamentoFiltros;
 import sigarep.modelos.repositorio.maestros.IReglamentoDAO;
+
+/**Reglamento
+ * UCLA DCYT Sistemas de Información
+ * @author Equipo : Builder-Sigarep Lapso 2013-1
+ * @version 1.0 
+ *@since 22/01/14
+ */
 
 
 @Service("servicioreglamento")
 public class ServicioReglamento {
 	private @Autowired IReglamentoDAO rg;
 
+
+/**Constructor Vacio
+ * @param constructor sin parametros	
+ */
 	public ServicioReglamento() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 	
-
+/**guardarReglamento
+ * @param r
+ * @return No devuelve ningun valor
+ */
 
 	public void guardarReglamento(Reglamento r){
 		rg.save(r);
 	}
 	
-	
+	/**elininar 
+	 * @param idDocumento
+	 * @return No devuelve ningun valor
+	 */
 	//Permite la eliminación lógica del registro, por id, busca el id y cambia su estatus a false, 
 		//la llamada reglamento.save(enlaceBorrarLogico); actualiza el estatus del registro.
 			public void eliminar(Integer idDocumento) {
@@ -41,76 +57,39 @@ public class ServicioReglamento {
 				rg.save(reglamentoBorrarLogico);
 			}
 	
+	/**buscarReglamento		
+	 * @param idDocumento
+	 * @return No devuelve ningun valor
+	 */
 	public Reglamento buscarReglamento(Integer idDocumento){
 		return rg.findOne(idDocumento);
 	}
 	
-	//Utiliza IDAO, el cual trae todos los registros en true,los que no han sido eliminado logicamente
+	/**listaReglamento
+	 * @param IDAO, el cual trae todos los registros en true,los que no han sido eliminado logicamente
+	 * @return listaReglamentoLogico
+	 */
 			public List<Reglamento> listaReglamento() {
 				return rg.listaReglamentoLogico();
 			}
-	
-	public List<Reglamento> buscarReglamentoTitulo(ReglamentoFiltros filtros, List<Reglamento> listaFiltrada){
-		List<Reglamento> resultado = new LinkedList<Reglamento>();
-		String titulo = filtros.getTitulo().toLowerCase();
-		if (titulo == null){		
-			resultado = listaFiltrada;
-		}
-		else
-		{
-			for (Reglamento r : listaFiltrada){
-				System.out.print(r);
-				if (r.getTitulo().toLowerCase().contains(titulo)){
-					resultado.add(r);
+			
+			public List<Reglamento> buscarReglamento(String tituloF,String  categoriaF) {
+				List<Reglamento> resultado = new LinkedList<Reglamento>();	
+				if (tituloF == null ||categoriaF==null ) {
+					resultado = listaReglamento();
+				} else {
+					for (Reglamento r : listaReglamento()) {
+						if (r.getTitulo().toLowerCase().contains(tituloF)
+								&& r.getCategoria().toLowerCase()
+								.contains(categoriaF))
+						{
+							resultado.add(r);
+						}
+					}
 				}
-					
-			}		
-		}
-		return resultado;
-		
-		
-	}
-	
-	public List<Reglamento> buscarReglamentoDescripcion(ReglamentoFiltros filtros, List<Reglamento> listaFiltrada){
-		List<Reglamento> resultado = new LinkedList<Reglamento>();
-		String descripcion = filtros.getDescripcion().toLowerCase();
-		if (descripcion == null){		
-			resultado = listaFiltrada;
-		}
-		else
-		{
-			for (Reglamento r : listaFiltrada){
-				System.out.print(r);
-				if (r.getDescripcion().toLowerCase().contains(descripcion)){
-					resultado.add(r);
-				}
-					
-			}		
-		}
-		return resultado;
-		
-		
-	}
-	
-	public List<Reglamento> buscarReglamentoCategoria(ReglamentoFiltros filtros, List<Reglamento> listaFiltrada){
-		List<Reglamento> resultado = new LinkedList<Reglamento>();
-		String categoria = filtros.getCategoria().toLowerCase();
-		if (categoria == null){		
-			resultado = listaFiltrada;
-		}
-		else
-		{
-			for (Reglamento r : listaFiltrada){
-				System.out.print(r);
-				if (r.getCategoria().toLowerCase().contains(categoria)){
-					resultado.add(r);
-				}
-					
-			}		
-		}
-		return resultado;
-		
-	}
+				return resultado;
+			}
+			
 	
 	public List<Reglamento> buscarReglamentoPortal(){
 		return rg.buscarReglamento();
