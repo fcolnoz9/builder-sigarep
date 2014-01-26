2b14077fdf4c4543dfd8d4e0736525e67e5bab63
saezgervazzikarol@gmail.com <saezgervazzikarol@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-01-26 03:10:16 +0000

    Arreglado las columnas de estándar y comentarizado

--- trunk/Sigarep.webapp/src/sigarep/modelos/servicio/maestros/ServicioEnlaceInteres.java
+++ trunk/Sigarep.webapp/src/sigarep/modelos/servicio/maestros/ServicioEnlaceInteres.java
@@ -4,76 +4,128 @@ import java.util.LinkedList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import sigarep.modelos.data.maestros.EnlaceInteres;
 import sigarep.modelos.repositorio.maestros.IEnlaceInteresDAO;
 
-
 @Service("servicioenlacesinteres")
 public class ServicioEnlaceInteres {
-	private @Autowired IEnlaceInteresDAO enlaceinteres;
+	private @Autowired
+	IEnlaceInteresDAO enlaceinteres;
 
-	//Guarda el objeto
+	/**
+	 * Guardar Enlace
+	 * 
+	 * @param EnlaceInteres
+	 *            enlace
+	 * @return Guarda el objeto
+	 * @throws No
+	 *             dispara ninguna excepcion.
+	 */
 	public void guardarEnlace(EnlaceInteres enlace) {
 		enlaceinteres.save(enlace);
-	}	
-	//Permite la eliminación lógica del registro, por id, busca el id y cambia su estatus a false, 
-	//la llamada enlaceinteres.save(enlaceBorrarLogico); actualiza el estatus del registro.
-		public void eliminar(Integer idEnlace) {
-			EnlaceInteres enlaceBorrarFisico = enlaceinteres.findOne(idEnlace);
-			enlaceinteres.delete(enlaceBorrarFisico);
-		}
-	//Utiliza IEnlaceInteresDAO, el cual trae todos los registros en true,los que no han sido eliminado logicamente
-		public List<EnlaceInteres> listadoEnlaceInteres() {
-			return enlaceinteres.findAll();
-		}
+	}
 
-		//Permite la búsqueda por id en el filtro buscarEnlaceFiltroId() de VMenlaceInteres
-		// utilizado en ActualizarEnlace.zul
-					public List<EnlaceInteres> buscarEnlacesCodigo(Integer idEnlace){
-						List<EnlaceInteres> result = new LinkedList<EnlaceInteres>();
-						if (idEnlace==null){//si el id es null,el resultado va a ser la lista completa de todos los enlaces
-							result = listadoEnlaceInteres();
-						}else{//caso contrario, se recorre toda la lista y busca los enlaces con el id indicado en el intbox. Realiza la busqueda por el primer número y número completo.
-							for (EnlaceInteres ei: listadoEnlaceInteres()){
-								if (ei.getIdEnlace().toString().contains(idEnlace.toString())){
-									result.add(ei);
-								}
-							}
-						}
-						return result;
-				}
-			
-		//Permite la búsqueda por nombre en el filtro buscarEnlaceFiltroNombreEnlace() de VMenlaceInteres
-		// utilizado en ActualizarEnlace.zul
-			public List<EnlaceInteres> buscarEnlacesNombre(String nombreEnlace){
-				List<EnlaceInteres> result = new LinkedList<EnlaceInteres>();
-				if (nombreEnlace==null || "".equals(nombreEnlace)){//si el nombre es null o vacio,el resultado va a ser la lista completa de todos los enlaces.
-					result = listadoEnlaceInteres();
-				}else{//caso contrario, se recorre toda la lista y busca los enlaces con el nombre indicado en la caja de texto y tambien busca todos los que tengan  las letras iniciales de ese nombre. Realiza la busqueda por nombre y por inicial del nombre.
-					for (EnlaceInteres e: listadoEnlaceInteres()){
-						if (e.getNombreEnlace().toLowerCase().contains(nombreEnlace.toLowerCase())){
-							result.add(e);
-						}
-					}
-				}
-				return result;
-			}	
+	/**
+	 * Eliminar enlace
+	 * 
+	 * @param Integer
+	 *            idEnlace
+	 * @return permite la eliminación física
+	 * @throws No
+	 *             dispara ninguna excepcion.
+	 */
+	public void eliminar(Integer idEnlace) {
+		EnlaceInteres enlaceBorrarFisico = enlaceinteres.findOne(idEnlace);
+		enlaceinteres.delete(enlaceBorrarFisico);
+	}
 
-		//Permite la búsqueda por dirección en el filtro buscarEnlaceFiltroDireccionEnlace() de VMenlaceInteres
-		// utilizado en ActualizarEnlace.zul		
-				public List<EnlaceInteres> buscarEnlacesDireccion(String direccionEnlace){
-					List<EnlaceInteres> result = new LinkedList<EnlaceInteres>();
-					if (direccionEnlace==null || "".equals(direccionEnlace)){//si la dirección es null o vacio,el resultado va a ser la lista completa de todos los enlaces
-						result = listadoEnlaceInteres();
-					}else{//caso contrario, se recorre toda la lista y busca los enlaces con la dirección indicada en la caja de texto. Realiza la busqueda con la dirección y con la inicial de la dirección.
-						for (EnlaceInteres e: listadoEnlaceInteres()){
-							if (e.getDireccionEnlace().toLowerCase().contains(direccionEnlace.toLowerCase())){
-								result.add(e);
-							}
-						}
-					}
-					return result;
+	/**
+	 * Listado Enlace Interes
+	 * 
+	 * @param listadoEnlaceInteres
+	 * @return buscar todos los enlaces
+	 * @throws No
+	 *             dispara ninguna excepcion.
+	 */
+	public List<EnlaceInteres> listadoEnlaceInteres() {
+		return enlaceinteres.findAll();
+	}
+
+	/**
+	 * Buscar Enlaces Codigo
+	 * 
+	 * @param Integer
+	 *            idEnlace
+	 * @return busca un enlace por id.
+	 * @throws No
+	 *             dispara ninguna excepcion.
+	 */
+	public List<EnlaceInteres> buscarEnlacesCodigo(Integer idEnlace) {
+		List<EnlaceInteres> result = new LinkedList<EnlaceInteres>();
+		if (idEnlace == null) {// si el id es null,el resultado va a ser la
+								// lista completa de todos los enlaces
+			result = listadoEnlaceInteres();
+		} else {// caso contrario, se recorre toda la lista y busca los enlaces
+				// con el id indicado en el intbox. Realiza la busqueda por el
+				// primer número y número completo.
+			for (EnlaceInteres ei : listadoEnlaceInteres()) {
+				if (ei.getIdEnlace().toString().contains(idEnlace.toString())) {
+					result.add(ei);
 				}
-		}//fin ServicioEnlaceInteres
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Buscar Enlaces Nombre
+	 * 
+	 * @param String
+	 *            nombreEnlace
+	 * @return busca un enlace por nombre en el filtro
+	 *         buscarEnlaceFiltroNombreEnlace() de VMenlaceInteres.
+	 * @throws No
+	 *             dispara ninguna excepcion.
+	 */
+
+	public List<EnlaceInteres> buscarEnlacesNombre(String nombreEnlace) {
+		List<EnlaceInteres> result = new LinkedList<EnlaceInteres>();
+		if (nombreEnlace == null || "".equals(nombreEnlace)) {// si el nombre es null o  vacio,el  resultado va  a ser la lista  completa de  todos los enlaces.
+			result = listadoEnlaceInteres();
+		} else {// caso contrario, se recorre toda la lista y busca los enlaces con el nombre indicado en la caja de texto y tambien busca todos los que tengan las letras iniciales de ese nombre. Realiza la busqueda por nombre y por inicial del nombre.
+			for (EnlaceInteres e : listadoEnlaceInteres()) {
+				if (e.getNombreEnlace().toLowerCase()
+						.contains(nombreEnlace.toLowerCase())) {
+					result.add(e);
+				}
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Buscar Enlaces Direccion
+	 * 
+	 * @param String
+	 *            nombreEnlace
+	 * @return busca un enlace por direccion en el filtro
+	 *         buscarEnlaceFiltroDireccionEnlace() de VMenlaceInteres.
+	 * @throws No
+	 *             dispara ninguna excepcion.
+	 */
+	public List<EnlaceInteres> buscarEnlacesDireccion(String direccionEnlace) {
+		List<EnlaceInteres> result = new LinkedList<EnlaceInteres>();
+		if (direccionEnlace == null || "".equals(direccionEnlace)) {// si la  dirección  es null o  vacio,el  resultado va a ser la lista  completa  de todos los  enlaces
+			result = listadoEnlaceInteres();
+		} else {// caso contrario, se recorre toda la lista y busca los enlaces  con la dirección indicada en la caja de texto. Realiza la busqueda con la dirección y con la inicial de la dirección.
+			for (EnlaceInteres e : listadoEnlaceInteres()) {
+				if (e.getDireccionEnlace().toLowerCase()
+						.contains(direccionEnlace.toLowerCase())) {
+					result.add(e);
+				}
+			}
+		}
+		return result;
+	}
+}// fin ServicioEnlaceInteres
 
--- trunk/Sigarep.webapp/src/sigarep/viewmodels/maestros/VMenlaceInteres.java
+++ trunk/Sigarep.webapp/src/sigarep/viewmodels/maestros/VMenlaceInteres.java
@@ -27,23 +27,20 @@ import sigarep.herramientas.MensajesAlUsuario;
 import sigarep.modelos.data.maestros.EnlaceInteres;
 import sigarep.modelos.servicio.maestros.ServicioEnlaceInteres;
 
-/*
- * @ (#) enlaceInteres.java 
- *
- * Copyright 2013 Builder. Todos los derechos reservados.
- * CONFIDENCIAL. El uso está sujeto a los términos de la licencia.
- * Esta clase es del registro del maestro "EstadoApelacion"
- * @ Author Builder 
- * @ Version 1.0, 16/12/13
+/**
+ * CargarEstudiante por XML UCLA DCYT Sistemas de Información.
+ * 
+ * @author Equipo : Builder-Sigarep Lapso 2013-2
+ * @version 2.5.2
  */
-
 
 @SuppressWarnings("serial")
 @VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
 public class VMenlaceInteres {
-	@WireVariable ServicioEnlaceInteres servicioenlacesinteres;
-    private Integer idEnlace;
-    private Integer idEnlaceFiltro;
+	@WireVariable
+	ServicioEnlaceInteres servicioenlacesinteres;
+	private Integer idEnlace;
+	private Integer idEnlaceFiltro;
 	private String nombreEnlace;
 	private String nombreEnlaceFiltro;
 	private String direccionEnlace;
@@ -56,61 +53,80 @@ public class VMenlaceInteres {
 	private List<EnlaceInteres> listaEnlaces;
 	private EnlaceInteres enlaceSeleccionado;
 	MensajesAlUsuario mensajeAlUsuario = new MensajesAlUsuario();
-    @Wire Textbox txtnombre_enlace;
-    @Wire Window ventana;
-    
-  //Getters and Setters
+	@Wire
+	Textbox txtnombre_enlace;
+	@Wire
+	Window ventana;
+
+	// Getters and Setters
 	public Integer getIdEnlace() {
 		return idEnlace;
 	}
+
 	public void setIdEnlace(Integer idEnlace) {
 		this.idEnlace = idEnlace;
 	}
+
 	public Integer getIdEnlaceFiltro() {
 		return idEnlaceFiltro;
 	}
+
 	public void setIdEnlaceFiltro(Integer idEnlaceFiltro) {
 		this.idEnlaceFiltro = idEnlaceFiltro;
 	}
+
 	public String getNombreEnlace() {
 		return nombreEnlace;
 	}
+
 	public void setNombreEnlace(String nombreEnlace) {
 		this.nombreEnlace = nombreEnlace;
 	}
+
 	public String getNombreEnlaceFiltro() {
 		return nombreEnlaceFiltro;
 	}
+
 	public void setNombreEnlaceFiltro(String nombreEnlaceFiltro) {
 		this.nombreEnlaceFiltro = nombreEnlaceFiltro;
 	}
+
 	public String getDireccionEnlace() {
 		return direccionEnlace;
 	}
+
 	public void setDireccionEnlace(String direccionEnlace) {
 		this.direccionEnlace = direccionEnlace;
 	}
+
 	public String getDireccionEnlaceFiltro() {
 		return direccionEnlaceFiltro;
 	}
+
 	public void setDireccionEnlaceFiltro(String direccionEnlaceFiltro) {
 		this.direccionEnlaceFiltro = direccionEnlaceFiltro;
 	}
+
 	public String getDescripcion() {
 		return descripcion;
 	}
+
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
+
 	public Boolean getEstatus() {
 		return estatus;
 	}
+
 	public void setEstatus(Boolean estatus) {
 		this.estatus = estatus;
 	}
+
 	public Archivo getImagen() {
 		return imagen;
 	}
+
 	public void setImagen(Archivo imagen) {
 		this.imagen = imagen;
 	}
@@ -118,141 +134,222 @@ public class VMenlaceInteres {
 	public AImage getImagenes() {
 		return imagenes;
 	}
+
 	public void setImagenes(AImage imagenes) {
 		this.imagenes = imagenes;
-		
+
 	}
+
 	public List<EnlaceInteres> getListaEnlaces() {
 		return listaEnlaces;
 	}
+
 	public void setListaEnlaces(List<EnlaceInteres> listaEnlaces) {
 		this.listaEnlaces = listaEnlaces;
 	}
-	
+
 	public EnlaceInteres getEnlaceSeleccionado() {
 		return enlaceSeleccionado;
 	}
+
 	public void setEnlaceSeleccionado(EnlaceInteres enlaceSeleccionado) {
 		this.enlaceSeleccionado = enlaceSeleccionado;
 	}
-	
-// fin Getters and Setters	
-	   
 
-	//Código de inicialización		
-    @Init
-    public void init(){
-    	imagen = new Archivo();
-    	buscarEnlaceInteres();
-    }
+	// fin Getters and Setters
 
- //Guarda el registro completo, el command indica a las variables en cambio que se hará en el objeto.
- // utiliza la clase mensajes del paquete herramientas.
-    @Command
-    @NotifyChange({"idEnlace", "nombreEnlace", "direccionEnlace", "descripcion","estatus", "imagenes","listaEnlaces" })
-    public void guardar(){
-    	if (nombreEnlace==null || direccionEnlace==null || descripcion==null )
-    		mensajeAlUsuario.advertenciaLlenarCampos();
-    	else if (imagen.getTamano() < 1)
-    		mensajeAlUsuario.advertenciaCargarImagen();
-    	else{
-    		EnlaceInteres enlace = new EnlaceInteres(idEnlace, nombreEnlace, direccionEnlace, descripcion, true, imagen);
-    		servicioenlacesinteres.guardarEnlace(enlace);
-    		limpiar();
-    		mensajeAlUsuario.informacionRegistroCorrecto();
-        	}
-    		
-    }
-     
+	/**
+	 * inicialización
+	 * 
+	 * @param init
+	 * @return código de inicialización
+	 * @throws No
+	 * dispara ninguna excepcion.
+	 */
 
- //Permite la carga de imágenes. utiliza Archivo del paquete herramientas.
-    @Command
+	@Init
+	public void init() {
+		imagen = new Archivo();
+		buscarEnlaceInteres();
+	}
+
+	/**
+	 * Guardar Enlace
+	 * 
+	 * @param guardar
+	 * @return Guarda el registro completo, el command indica a las variables el
+	 *         cambio que se hará en el objeto.
+	 * @throws No
+	 *             dispara ninguna excepcion.
+	 */
+	@Command
+	@NotifyChange({ "idEnlace", "nombreEnlace", "direccionEnlace",
+			"descripcion", "estatus", "imagenes", "listaEnlaces" })
+	public void guardar() {
+		if (nombreEnlace == null || direccionEnlace == null
+				|| descripcion == null)
+			mensajeAlUsuario.advertenciaLlenarCampos();
+		else if (imagen.getTamano() < 1)
+			mensajeAlUsuario.advertenciaCargarImagen();
+		else {
+			EnlaceInteres enlace = new EnlaceInteres(idEnlace, nombreEnlace,
+					direccionEnlace, descripcion, true, imagen);
+			servicioenlacesinteres.guardarEnlace(enlace);
+			limpiar();
+			mensajeAlUsuario.informacionRegistroCorrecto();
+		}
+
+	}
+
+	/**
+	 * Carga de imágenes
+	 * 
+	 * @param cargarImagen
+	 * @return Permite la carga de imágenes. utiliza Archivo del paquete
+	 *         herramientas.
+	 * @throws No
+	 *             dispara ninguna excepcion.
+	 */
+	@Command
 	@NotifyChange("imagenes")
-	public void cargarImagen(@ContextParam(ContextType.TRIGGER_EVENT) UploadEvent event){
+	public void cargarImagen(
+			@ContextParam(ContextType.TRIGGER_EVENT) UploadEvent event) {
 		media = event.getMedia();
 		if (media != null) {
 			if (media instanceof org.zkoss.image.Image) {
 				imagen.setNombreArchivo(media.getName());
 				imagen.setTipo(media.getContentType());
 				imagen.setContenidoArchivo(media.getByteData());
-		
+
 				imagenes = (AImage) media;
 			} else {
-				Messagebox.show("El archivo: "+media+" no es una imagen valida", "Error", Messagebox.OK, Messagebox.ERROR);
+				Messagebox.show("El archivo: " + media
+						+ " no es una imagen valida", "Error", Messagebox.OK,
+						Messagebox.ERROR);
 			}
-		} 
+		}
 	}
 
-//Limpia las cajas de texto.     
-    @Command
-    @NotifyChange({"idEnlace", "nombreEnlace", "direccionEnlace", "descripcion","estatus","imagenes" })
-    public void limpiar(){
-    	nombreEnlace= null;
-    	direccionEnlace=null;
-        descripcion = null;
-        idEnlace = null;
-        media = null;
+	/**
+	 * Limpiar.
+	 * 
+	 * @param limpiar
+	 * @return inicializa las cajas de texto
+	 * @throws No
+	 *             dispara ninguna excepcion.
+	 */
+	@Command
+	@NotifyChange({ "idEnlace", "nombreEnlace", "direccionEnlace",
+			"descripcion", "estatus", "imagenes" })
+	public void limpiar() {
+		nombreEnlace = null;
+		direccionEnlace = null;
+		descripcion = null;
+		idEnlace = null;
+		media = null;
 		imagenes = null;
 		imagen = new Archivo();
-        buscarEnlaceInteres();
-    }
-//Busca todos los registros. Inicializa el código.     
-    @Command
-    @NotifyChange({"listaEnlaces"})
+		buscarEnlaceInteres();
+	}
+
+	/**
+	 * Buscar enlaces de interés
+	 * 
+	 * @param buscarEnlacesInteres
+	 * @return Busca todos los registros. Inicializa el código.
+	 * @throws No
+	 *             dispara ninguna excepcion.
+	 */
+	@Command
+	@NotifyChange({ "listaEnlaces" })
 	private void buscarEnlaceInteres() {
 		listaEnlaces = servicioenlacesinteres.listadoEnlaceInteres();
 	}
-//Permite la búsqueda por id en el filtro,  en ActualizarEnlace.zul,viene de VMenlaceInteres	
-    @Command
-	@NotifyChange({"listaEnlaces"})
-    public void buscarEnlaceFiltroId(){
-    	listaEnlaces = servicioenlacesinteres.buscarEnlacesCodigo(idEnlaceFiltro);
-    }
-//Permite la búsqueda por nombre en el filtro,  en ActualizarEnlace.zul,viene de VMenlaceInteres    
-    @Command
-   	@NotifyChange({"listaEnlaces"})
-       public void buscarEnlaceFiltroNombreEnlace(){
-       	listaEnlaces = servicioenlacesinteres.buscarEnlacesNombre(nombreEnlaceFiltro);
-       }
-//Permite la búsqueda por dirección en el filtro,  en ActualizarEnlace.zul,viene de VMenlaceInteres    
-    @Command
-   	@NotifyChange({"listaEnlaces"})
-       public void buscarEnlaceFiltroDireccionEnlace(){
-       	listaEnlaces = servicioenlacesinteres.buscarEnlacesDireccion(direccionEnlaceFiltro);
-       }
-//Elimina un registro logicamente, utiliza la clase mensajes del paquete herramientas.  
-  	@Command
-  	@NotifyChange({"nombreEnlace", "direccionEnlace", "descripcion", "listaEnlaces", "imagenes"})
-  	public void eliminarEnlaceSeleccionado(){
-  		if (nombreEnlace==null || direccionEnlace==null ||descripcion==null || imagen.getTamano() < 1) {
-  			mensajeAlUsuario.advertenciaSeleccionarParaEliminar();
-		} 
-  		else {
-  		servicioenlacesinteres.eliminar(idEnlace);
-  		mensajeAlUsuario.informacionEliminarCorrecto();
-  		limpiar();
-  	}
-  		
- }   
-//muestra en el área de datos el registro seleccionado.	  	
+
+	/**
+	 * Buscar enlaces de interés por nombre
+	 * 
+	 * @param buscarEnlaceFiltroNombreEnlace
+	 * @return Permite la búsqueda por nombre en el filtro, en
+	 *         ActualizarEnlace.zul,viene de VMenlaceInteres
+	 * @throws No
+	 *             dispara ninguna excepcion.
+	 */
 	@Command
-    @NotifyChange({"idEnlace", "nombreEnlace","direccionEnlace", "descripcion","estatus", "imagenes", "imagen" })
-	public void mostrarEnlace(){
-		idEnlace= enlaceSeleccionado.getIdEnlace();
+	@NotifyChange({ "listaEnlaces" })
+	public void buscarEnlaceFiltroNombreEnlace() {
+		listaEnlaces = servicioenlacesinteres
+				.buscarEnlacesNombre(nombreEnlaceFiltro);
+	}
+
+	/**
+	 * Buscar enlaces de interés por dirección
+	 * 
+	 * @param buscarEnlaceFiltroDireccionEnlace
+	 * @return Permite la búsqueda por dirección en el filtro, en
+	 *         ActualizarEnlace.zul,viene de VMenlaceInteres
+	 * @throws No
+	 *             dispara ninguna excepcion.
+	 */
+
+	@Command
+	@NotifyChange({ "listaEnlaces" })
+	public void buscarEnlaceFiltroDireccionEnlace() {
+		listaEnlaces = servicioenlacesinteres
+				.buscarEnlacesDireccion(direccionEnlaceFiltro);
+	}
+
+	/**
+	 * Eliminar un enlace
+	 * 
+	 * @param eliminarEnlaceSeleccionado
+	 * @return Elimina un enlace fisicamente.
+	 * @throws No
+	 *             dispara ninguna excepcion.
+	 */
+	@Command
+	@NotifyChange({ "nombreEnlace", "direccionEnlace", "descripcion",
+			"listaEnlaces", "imagenes" })
+	public void eliminarEnlaceSeleccionado() {
+		if (nombreEnlace == null || direccionEnlace == null
+				|| descripcion == null || imagen.getTamano() < 1) {
+			mensajeAlUsuario.advertenciaSeleccionarParaEliminar();
+		} else {
+			servicioenlacesinteres.eliminar(idEnlace);
+			mensajeAlUsuario.informacionEliminarCorrecto();
+			limpiar();
+		}
+
+	}
+
+	/**
+	 * Mostrar Enlace
+	 * 
+	 * @param mostrarEnlace
+	 * @return muestra en el área de datos el registro seleccionado.
+	 * @throws No
+	 *             dispara ninguna excepcion.
+	 */
+	@Command
+	@NotifyChange({ "idEnlace", "nombreEnlace", "direccionEnlace",
+			"descripcion", "estatus", "imagenes", "imagen" })
+	public void mostrarEnlace() {
+		idEnlace = enlaceSeleccionado.getIdEnlace();
 		nombreEnlace = enlaceSeleccionado.getNombreEnlace();
 		direccionEnlace = enlaceSeleccionado.getDireccionEnlace();
 		descripcion = enlaceSeleccionado.getDescripcion();
 		imagen = enlaceSeleccionado.getImagen();
-		if (enlaceSeleccionado.getImagen().getTamano() > 0){
+		if (enlaceSeleccionado.getImagen().getTamano() > 0) {
 			try {
-				imagenes = new AImage(enlaceSeleccionado.getImagen().getNombreArchivo(), enlaceSeleccionado.getImagen().getContenidoArchivo());
+				imagenes = new AImage(enlaceSeleccionado.getImagen()
+						.getNombreArchivo(), enlaceSeleccionado.getImagen()
+						.getContenidoArchivo());
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 			}
+		} else {
+			imagenes = null;
 		}
-		else{
-			imagenes = null;}
-	}    
-}//fin VMenlaceInteres.
-
+	}
+}// fin VMenlaceInteres.
 
