01d6f58ed7bb4a65a560dd695e00f9ce3b44ecfd
palencia77@gmail.com <palencia77@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-01-26 01:12:12 +0000

    Agregando metodos correspondientes a la transaccion Veredicto I

--- trunk/Sigarep.webapp/src/sigarep/modelos/servicio/transacciones/ServicioSolicitudApelacion.java
+++ trunk/Sigarep.webapp/src/sigarep/modelos/servicio/transacciones/ServicioSolicitudApelacion.java
@@ -151,5 +151,9 @@ public class ServicioSolicitudApelacion {
 			}
         }
 		return result;
+	}
+
+	public List<SolicitudApelacion> buscarApelacionesVeredictoI() {
+		return iSolicitudApelacionDAO.buscarApelacionesVeredictoI();
 	} 
 }
