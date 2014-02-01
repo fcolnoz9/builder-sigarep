f1a097818df4aae021c563b8e16ab126192895a2
palencia77@gmail.com <palencia77@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-02-01 18:18:09 +0000

    Aplicando correcciones a los datos de la sesión como lo indicó el profesor

--- trunk/Sigarep.webapp/src/sigarep/viewmodels/transacciones/VMDatosSesionVeredicto.java
+++ trunk/Sigarep.webapp/src/sigarep/viewmodels/transacciones/VMDatosSesionVeredicto.java
@@ -2,6 +2,7 @@ package sigarep.viewmodels.transacciones;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 import org.zkoss.bind.annotation.Command;
@@ -11,29 +12,36 @@ import org.zkoss.bind.annotation.ExecutionArgParam;
 import org.zkoss.bind.annotation.Init;
 import org.zkoss.bind.annotation.NotifyChange;
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.Path;
 import org.zkoss.zk.ui.select.Selectors;
 import org.zkoss.zk.ui.select.annotation.VariableResolver;
 import org.zkoss.zk.ui.select.annotation.WireVariable;
+import org.zkoss.zul.Borderlayout;
+import org.zkoss.zul.Center;
+import org.zkoss.zul.Messagebox;
 
 import sigarep.herramientas.MensajesAlUsuario;
 import sigarep.modelos.data.transacciones.SolicitudApelacion;
 import sigarep.modelos.servicio.transacciones.ServicioSolicitudApelacion;
 
 @VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
-public class VMDatosSesionI {
+public class VMDatosSesionVeredicto {
 	
 	//Datos de la sesion
 	private String numeroSesion;
 	private String tipoSesion;
 	private Date fechaSesion = new Date();
+	private String titulo;
 	private List<SolicitudApelacion> listaSancionados = new ArrayList<SolicitudApelacion>();
 	
 	@WireVariable
 	private ServicioSolicitudApelacion serviciosolicitudapelacion;
 	
 	private MensajesAlUsuario mensajesAlUsuario = new MensajesAlUsuario();
+	private String rutaModal;
 	
-	public VMDatosSesionI() {
+	public VMDatosSesionVeredicto() {
 		
 	}
 	
@@ -62,12 +70,26 @@ public class VMDatosSesionI {
 	}
 	
 	@Init
-	public void init(
-		@ContextParam(ContextType.VIEW) Component view,
-		@ExecutionArgParam("listaSancionados") List<SolicitudApelacion> listaSancionados)
-	{
+	public void init(@ContextParam(ContextType.VIEW) Component view,
+					 @ExecutionArgParam("rutaModal") String rutaModal){
+		
 		Selectors.wireComponents(view, this, false);
-		this.listaSancionados = listaSancionados;
+		this.rutaModal=rutaModal;
+		buscarDatosSesion();
+	}
+	
+	@NotifyChange ({"fechaSesion", "tipoSesion", "numeroSesion","titulo"})
+	public void buscarDatosSesion(){
+		SolicitudApelacion solicitudApelacion = serviciosolicitudapelacion.buscarSolicitudParaDatosSesion();
+		if (solicitudApelacion != null){
+			fechaSesion = solicitudApelacion.getFechaSesion();
+			tipoSesion = solicitudApelacion.getTipoSesion();
+			numeroSesion = solicitudApelacion.getNumeroSesion();
+			titulo = "Puede continuar con la siguiente sesión o proporcionar los datos de una nueva.";
+			mensajesAlUsuario.informacionDatosDeSesionEncontrados();
+		}else{
+			titulo = "Proporcione los datos de una nueva sesión.";
+		}
 	}
 	
 	@Command
@@ -75,17 +97,7 @@ public class VMDatosSesionI {
 		if (fechaSesion == null || tipoSesion == null || numeroSesion == null)
 			mensajesAlUsuario.advertenciaLlenarCampos();
 		else{
-			int contador = 0;
-			for (int i = 0; i < listaSancionados.size(); i++) {
-				if (listaSancionados.get(i).getVeredicto() != null && !listaSancionados.get(i).getVeredicto().equals("")){
-					contador++;
-					listaSancionados.get(i).setFechaSesion(fechaSesion);
-					listaSancionados.get(i).setNumeroSesion(numeroSesion);
-					listaSancionados.get(i).setTipoSesion(tipoSesion);
-					serviciosolicitudapelacion.guardar(listaSancionados.get(i));
-				}
-			}
-			mensajesAlUsuario.informacionGuardarDatosSesion(contador);
+			mostrarListaSancionados();
 		}
 	}
 	
@@ -95,6 +107,31 @@ public class VMDatosSesionI {
 		fechaSesion = new Date();
 		tipoSesion = null;
 		numeroSesion = null;
+	}
+	
+	public void mostrarListaSancionados(){
+		final HashMap<String, Object> map = new HashMap<String, Object>();
+	 	map.put("rutaModal", rutaModal);
+	 	map.put("numeroSesion", numeroSesion);
+	 	map.put("tipoSesion", tipoSesion);
+	 	map.put("fechaSesion", fechaSesion);
+	 	
+	 	try {
+			Borderlayout bl = (Borderlayout) Path.getComponent("/mainBorderLayout");
+			Center center = bl.getCenter();
+			center.getChildren().clear();
+			Executions.createComponents("/WEB-INF/sigarep/vistas/transacciones/ListaGenericaSancionados.zul", center, map);
+		} catch (Exception e) {
+			Messagebox.show(e.toString());
+		}
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
 	}
 
 }
