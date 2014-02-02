a75e3aa0373b05b7ea4472b20f697b7a9a41342a
palencia77@gmail.com <palencia77@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-02-02 15:24:09 +0000

    Avances en la transaccion: Registrar Sancionado manualmente. Amanda Mujica

--- Sigarep.webapp/src/sigarep/modelos/repositorio/maestros/IAsignaturaDAO.java
+++ Sigarep.webapp/src/sigarep/modelos/repositorio/maestros/IAsignaturaDAO.java
@@ -1,9 +1,21 @@
 package sigarep.modelos.repositorio.maestros;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import sigarep.modelos.data.maestros.Asignatura;
+import sigarep.modelos.data.maestros.SancionMaestro;
 
 public interface IAsignaturaDAO extends JpaRepository<Asignatura, String> {
+	
+	@Query("Select san FROM Asignatura AS san WHERE estatus = TRUE")
+	public List<Asignatura> buscarAsignaturasActivas();
+
+	@Query("SELECT a FROM Asignatura AS a WHERE estatus = 'TRUE' " +
+			"AND a.programaAcademico.idPrograma = :idPrograma")
+	public List<Asignatura> buscarAsignaturasPorPrograma(@Param("idPrograma")Integer idPrograma);
 
 }
