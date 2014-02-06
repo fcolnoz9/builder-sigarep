3316ac6bad8f0995e425523aabb14ff46c1748c1
lseolivera@gmail.com <lseolivera@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-02-06 21:43:32 +0000

    Actualizacion de Comentarios en las Clases involucradas para InstanciaApelada

--- trunk/Sigarep.webapp/src/sigarep/modelos/servicio/maestros/ServicioInstanciaApelada.java
+++ trunk/Sigarep.webapp/src/sigarep/modelos/servicio/maestros/ServicioInstanciaApelada.java
@@ -5,9 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import sigarep.modelos.data.maestros.InstanciaApelada;
 import sigarep.modelos.repositorio.maestros.IInstanciaApeladaDAO;
-/**Servicio Instancia Apelada
-* UCLA DCYT Sistemas de Informacion.
-* @author Equipo: Builder-SIGAREP 
+/**Clase ServicioInstanciaApelada
+* Suministra los servicios al VMInstanciaApelada
+* @author Builder
 * @version 1.0
 * @since 20/12/13
 */
@@ -15,18 +15,41 @@ import sigarep.modelos.repositorio.maestros.IInstanciaApeladaDAO;
 public class ServicioInstanciaApelada{
 	private @Autowired IInstanciaApeladaDAO iInstancia ;
 
+	/** Guardar Instancia apelada 
+	 * @return nada
+	 * @parameters el objeto InstanciaApelada
+	 * @throws No dispara ninguna excepcion.
+	   */
 	public void guardar(InstanciaApelada pro) {
 		iInstancia.save(pro);
 	}
+	
+	/** Eliminar Instancia apelada 
+	 * @return nada
+	 * @parameters Entero codigoInstancia
+	 * @throws No dispara ninguna excepcion.
+	   */
 	public void eliminar(Integer codigoInstancia){
 		InstanciaApelada instanciaapelada = iInstancia.findOne(codigoInstancia);
 		instanciaapelada.setEstatus(false);
 		iInstancia.save(instanciaapelada);
 	}
+	
+	/** Lista de Instancia Apelada 
+	 * @return Lista de las InstanciasApeladas registradas y activas
+	 * @parameters vacio
+	 * @throws No dispara ninguna excepcion.
+	   */
 	public List<InstanciaApelada> listadoInstanciaApelada() {
 		List<InstanciaApelada> instanciaApeladaLista = iInstancia.buscarInstanciaActivo();
 	    return instanciaApeladaLista ;
 	}
+	
+	/** Buscar Instancia Apelada 
+	 * @return Lista de InstanciasApeladas buscadas
+	 * @parameters String Instancia, String Recurso
+	 * @throws No dispara ninguna excepcion.
+	   */
 	public List<InstanciaApelada> buscarInstancia(String instancia, String recurso) {
 		List<InstanciaApelada> resultado = new LinkedList<InstanciaApelada>();
 		if (instancia == null || recurso == null) {
@@ -41,12 +64,30 @@ public class ServicioInstanciaApelada{
 		}
 		return resultado;
 	}
+	
+	/** Buscar Instancia Apelada 
+	 * @return InstanciaApelada buscada
+	 * @parameters Integer codigoInstancia
+	 * @throws No dispara ninguna excepcion.
+	   */
 	public InstanciaApelada buscar(Integer codigoInstancia){
 		return iInstancia.findOne(codigoInstancia);
 	}
+	
+	/** Lista de Instancia Apelada 
+	 * @return Lista de todas las InstanciasApeladas activas e inactivas
+	 * @parameters vacio
+	 * @throws No dispara ninguna excepcion.
+	   */
 	public List<InstanciaApelada> buscarTodas(){
 		return iInstancia.buscarTodas();
 	}
+	
+	/** Lista de Instancia Apelada 
+	 * @return Lista de las InstanciasApeladas registradas y activas
+	 * @parameters vacio
+	 * @throws No dispara ninguna excepcion.
+	   */
 	public List<InstanciaApelada> buscarTodasLasInstancias(){
 		return iInstancia.buscarInstanciaActivo();
 	}
