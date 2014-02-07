bf7c7dde25fef4cfa1d307f7e25f31ae90ae8931
blumxp@gmail.com <blumxp@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-02-07 13:04:52 +0000

    Devolviendo el Cambio de Andrea ya que elimino metodos que estaban en uso.

--- Sigarep.webapp/src/sigarep/modelos/servicio/maestros/ServicioLapsoAcademico.java
+++ Sigarep.webapp/src/sigarep/modelos/servicio/maestros/ServicioLapsoAcademico.java
@@ -35,18 +35,22 @@ public class ServicioLapsoAcademico{
 		List<LapsoAcademico> LapsoAcademicoLista=iLapsoAcademico.buscarActivoLapso();
 	    return LapsoAcademicoLista ;
 	}
-	
-
+	public LapsoAcademico encontrarLapsoActivo(){
+	    return iLapsoAcademico.buscarLapsoActivo();
+	}
 	/** Buscar lapsos academicos por codigo de lapso
 	 * @return el lapso academico buscado si existe
 	 * @parameters codigo del lapso
 	 * @throws No dispara ninguna excepcion.
 	   */
-
 	public LapsoAcademico buscarLapsoActivo(){
 		return iLapsoAcademico.buscarLapsoActivo();
 	}
-	
+	// metodo del listado actualizado de los lapsos
+	public List<LapsoAcademico> listadoLapsoAcademicoInactivos() {
+	    List<LapsoAcademico> LapsoAcademicoLista=iLapsoAcademico.buscarInactivoLapso();
+	    return LapsoAcademicoLista ;
+	}
 	public List<LapsoAcademico> buscarTodosLosLapsos(){
 		return iLapsoAcademico.buscarLapsosAcademicos();
 	}
