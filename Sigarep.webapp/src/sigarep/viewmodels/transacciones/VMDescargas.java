13231a9514e8e1fed12fb6e47a2b7a40c20ac4e6
mima350@gmail.com <mima350@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-02-08 18:55:22 +0000

    Actualizar descargas del portal por Maria

--- Sigarep.webapp/src/sigarep/viewmodels/transacciones/VMDescargas.java
+++ Sigarep.webapp/src/sigarep/viewmodels/transacciones/VMDescargas.java
@@ -1,5 +1,7 @@
 package sigarep.viewmodels.transacciones;
 
+import java.util.HashMap;
+
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.select.annotation.VariableResolver;
@@ -18,10 +20,12 @@ public class VMDescargas {
 
 	@Command
 	public void modalReglamento() {
+		final HashMap<String, Object> map = new HashMap<String, Object>();
+	 	map.put("categoria", "REGLAMENTO");
 		final Window window = (Window) Executions
 				.createComponents(
-						"WEB-INF/sigarep/vistas/portal/externo/modales/Reglamentos.zul",
-						null, null);
+						"WEB-INF/sigarep/vistas/portal/externo/modales/descargarArchivo.zul",
+						null, map);
 		window.setMaximizable(true);
 		window.doModal();
 	}
@@ -32,6 +36,18 @@ public class VMDescargas {
 				.createComponents(
 						"WEB-INF/sigarep/vistas/portal/externo/modales/RecaudosPorMotivo.zul",
 						null, null);
+		window.setMaximizable(true);
+		window.doModal();
+	}
+	
+	
+	@Command
+	public void modalFormato() {
+		final HashMap<String, Object> map = new HashMap<String, Object>();
+	 	map.put("categoria", "FORMATO");
+		final Window window = (Window) Executions.createComponents(
+				"WEB-INF/sigarep/vistas/portal/externo/modales/descargarArchivo.zul",
+				 null, map);
 		window.setMaximizable(true);
 		window.doModal();
 	}
