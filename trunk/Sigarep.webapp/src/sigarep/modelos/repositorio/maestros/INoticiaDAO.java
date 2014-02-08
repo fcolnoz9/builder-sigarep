cda4acab4adc3048ffb579558e19c19e0d00bd7d
rosbe07@gmail.com <rosbe07@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-02-08 21:16:11 +0000

    documentación

--- trunk/Sigarep.webapp/src/sigarep/modelos/repositorio/maestros/INoticiaDAO.java
+++ trunk/Sigarep.webapp/src/sigarep/modelos/repositorio/maestros/INoticiaDAO.java
@@ -1,10 +1,17 @@
 package sigarep.modelos.repositorio.maestros;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import sigarep.modelos.data.maestros.Noticia;
+
+/**Noticia
+ * Registra y modifica una noticia. Utilizada en el portal web.
+ * UCLA DCYT Sistemas de Informacion.
+ * @author Equipo : Builder-Sigarep Lapso 2013-2
+ * @version 1.0
+ * @since 22/01/14 
+ */
 
 public interface INoticiaDAO extends JpaRepository<Noticia, Integer> {
 	
