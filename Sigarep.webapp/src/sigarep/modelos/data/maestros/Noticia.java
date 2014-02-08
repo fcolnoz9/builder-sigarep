1881dc35562976c9d159f7c3df77406e7cdfd2d9
rosbe07@gmail.com <rosbe07@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-02-08 21:13:02 +0000

    documentación

--- Sigarep.webapp/src/sigarep/modelos/data/maestros/Noticia.java
+++ Sigarep.webapp/src/sigarep/modelos/data/maestros/Noticia.java
@@ -8,6 +8,7 @@ import sigarep.herramientas.Archivo;
 
 import java.util.Date;
 /**Noticia
+ * Registra y modifica una noticia. Utilizada en el portal web.
  * UCLA DCYT Sistemas de Informacion.
  * @author Equipo : Builder-Sigarep Lapso 2013-2
  * @version 1.0
@@ -20,37 +21,11 @@ import java.util.Date;
  */
 @Entity
 @Access(AccessType.FIELD)
+//anotación indica que el JavaBean es una entidad persistente
 @Table(name="noticia")
 public class Noticia implements Serializable {
 	private static final long serialVersionUID = 1L;
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="id_noticia", unique=true, nullable=false)
-	private Integer idNoticia;
-	@Column(nullable=false, length=255)
-	private String contenido;
-	@Column(name="enlace_noticia", length=255)
-	private String enlaceNoticia;
-	@Column(nullable=false)
-	private Boolean estatus;
-	@Column(name="fecha_registro")
-	@Temporal(TemporalType.DATE)
-	private Date fechaRegistro;
-
-	private Archivo fotoNoticia = new Archivo();
-	@Column(nullable=false, length=60)
-	private String titulo;
-	@Column(name="fecha_vencimiento")
-	@Temporal(TemporalType.DATE)
-	private Date vencimiento;
-
-	public Noticia() {
-	}
-
-	/** Constructor Noticia
-	 * @param idNoticia, contenido, enlaceNoticia, estatus, fechaRegistro, fotoNoticia, titulo, vencimiento
-	 * @return Constructor lleno.
-	 */
+	
 	public Noticia(Integer idNoticia, String contenido, String enlaceNoticia,
 			Boolean estatus, Date fechaRegistro, Archivo fotoNoticia, String titulo,
 			Date vencimiento) {
@@ -63,6 +38,39 @@ public class Noticia implements Serializable {
 		this.fotoNoticia= fotoNoticia;
 		this.titulo = titulo;
 		this.vencimiento = vencimiento;
+	}
+	
+	// Atributos de la clase
+	@Id
+	// Clave principal de la clase
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	// Genera el ID de la Noticia
+	@Column(name="id_noticia", unique=true, nullable=false)
+	private Integer idNoticia;
+	
+	@Column(nullable=false, length=255)
+	private String contenido;
+	
+	@Column(name="enlace_noticia", length=255)
+	private String enlaceNoticia;
+	
+	@Column(nullable=false)
+	private Boolean estatus;
+	
+	@Column(name="fecha_registro")
+	@Temporal(TemporalType.DATE)
+	private Date fechaRegistro;
+
+	private Archivo fotoNoticia = new Archivo();
+	
+	@Column(nullable=false, length=60)
+	private String titulo;
+	
+	@Column(name="fecha_vencimiento")
+	@Temporal(TemporalType.DATE)
+	private Date vencimiento;
+
+	public Noticia() {
 	}
 
 	public Integer getIdNoticia() {
@@ -104,7 +112,6 @@ public class Noticia implements Serializable {
 	public void setFechaRegistro(Date fechaRegistro) {
 		this.fechaRegistro = fechaRegistro;
 	}
-
 
 	public String getTitulo() {
 		return this.titulo;
