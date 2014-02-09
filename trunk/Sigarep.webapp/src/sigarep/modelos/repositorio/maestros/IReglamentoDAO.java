24d18deada8886df01ed23fb0fbe297738c098b0
javier.kllao@gmail.com <javier.kllao@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-02-09 00:31:58 +0000

    Guía paso a paso del portal Listo! Modificado RegistrarActividad.zul en cuanto a el orden de las cabeceras de la lista de actividades. Pequeños cambios en la plantilla del portal interno.

--- trunk/Sigarep.webapp/src/sigarep/modelos/repositorio/maestros/IReglamentoDAO.java
+++ trunk/Sigarep.webapp/src/sigarep/modelos/repositorio/maestros/IReglamentoDAO.java
@@ -1,27 +1,29 @@
 package sigarep.modelos.repositorio.maestros;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
-import sigarep.modelos.data.maestros.EnlaceInteres;
 import sigarep.modelos.data.maestros.Reglamento;
 
 public interface IReglamentoDAO extends JpaRepository<Reglamento, Integer> {
-	
-	//permite la busqueda de los registros por estatus, para mostrar los registros en true, luego de la eliminación lógica.
-		//se utiliza en ServicioReglamento public List<Reglamento> listaReglamento()
-			@Query("select  r from Reglamento r where r.estatus='TRUE'")
-			public List<Reglamento> listaReglamentoLogico();
-			
-			@Query("select  r from Reglamento r where r.estatus='TRUE' and r.categoria='reglamento'")
-			public List<Reglamento> buscarReglamento();
-			
-			@Query("select  r from Reglamento r where r.estatus='TRUE' and r.categoria='recaudo'")
-			public List<Reglamento> buscarRecaudos();
-			
-			@Query("select  r from Reglamento r where r.estatus='TRUE' and r.categoria='formato'")
-			public List<Reglamento> buscarFormato();
+
+	// permite la busqueda de los registros por estatus, para mostrar los
+	// registros en true, luego de la eliminación lógica.
+	// se utiliza en ServicioReglamento public List<Reglamento>
+	// listaReglamento()
+	@Query("select  r from Reglamento r where r.estatus='TRUE'")
+	public List<Reglamento> listaReglamentoLogico();
+
+	@Query("select  r from Reglamento r where r.estatus='TRUE' and r.categoria='reglamento'")
+	public List<Reglamento> buscarReglamento();
+
+	@Query("select  r from Reglamento r where r.estatus='TRUE' and r.categoria='recaudo'")
+	public List<Reglamento> buscarRecaudos();
+
+	@Query("select  r from Reglamento r where r.estatus='TRUE' and r.categoria='formato'")
+	public List<Reglamento> buscarFormato();
+
+	@Query("SELECT  r FROM Reglamento r WHERE r.estatus='TRUE' and r.categoria='guia'")
+	public Reglamento buscarGuiaPasoAPaso();
 
 }
