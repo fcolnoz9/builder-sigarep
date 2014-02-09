af28a8c192e8be6afba0642a69aebb6f1c1c2359
javier.kllao@gmail.com <javier.kllao@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-02-09 03:44:58 +0000

    Terminado descargas del portal

--- Sigarep.webapp/src/sigarep/viewmodels/transacciones/VMDescargas.java
+++ Sigarep.webapp/src/sigarep/viewmodels/transacciones/VMDescargas.java
@@ -5,7 +5,13 @@ import java.util.HashMap;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.select.annotation.VariableResolver;
+import org.zkoss.zk.ui.select.annotation.WireVariable;
+import org.zkoss.zul.Filedownload;
 import org.zkoss.zul.Window;
+
+import sigarep.herramientas.MensajesAlUsuario;
+import sigarep.modelos.data.maestros.Reglamento;
+import sigarep.modelos.servicio.maestros.ServicioReglamento;
 
 /**
  * Cronograma UCLA DCYT Sistemas de Informacion.
@@ -18,10 +24,14 @@ import org.zkoss.zul.Window;
 @VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
 public class VMDescargas {
 
+	@WireVariable
+	private ServicioReglamento servicioreglamento;
+	private MensajesAlUsuario msj = new MensajesAlUsuario();
+
 	@Command
 	public void modalReglamento() {
 		final HashMap<String, Object> map = new HashMap<String, Object>();
-	 	map.put("categoria", "REGLAMENTO");
+		map.put("categoria", "REGLAMENTO");
 		final Window window = (Window) Executions
 				.createComponents(
 						"WEB-INF/sigarep/vistas/portal/externo/modales/descargarArchivo.zul",
@@ -39,16 +49,30 @@ public class VMDescargas {
 		window.setMaximizable(true);
 		window.doModal();
 	}
-	
-	
+
 	@Command
 	public void modalFormato() {
 		final HashMap<String, Object> map = new HashMap<String, Object>();
-	 	map.put("categoria", "FORMATO");
-		final Window window = (Window) Executions.createComponents(
-				"WEB-INF/sigarep/vistas/portal/externo/modales/descargarArchivo.zul",
-				 null, map);
+		map.put("categoria", "FORMATO");
+		final Window window = (Window) Executions
+				.createComponents(
+						"WEB-INF/sigarep/vistas/portal/externo/modales/descargarArchivo.zul",
+						null, map);
 		window.setMaximizable(true);
 		window.doModal();
 	}
+
+	@Command
+	public void descargarCalendario() {
+		Reglamento calendario = servicioreglamento.buscarCalendario();
+		if (calendario != null) {
+			Filedownload.save(
+					calendario.getDocumento().getContenidoDocumento(),
+					calendario.getDocumento().getTipoDocumento(), calendario
+							.getDocumento().getNombreDocumento());
+		} else {
+			msj.advertenciaCargarDocumento();
+		}
+	}
+
 }
