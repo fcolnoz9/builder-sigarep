feffc8dcdb83779d919b86b112054cee8e463454
palencia77@gmail.com <palencia77@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-02-10 00:03:20 +0000

    ***IMPORTANTE*** Reestructuracion de la BD Cambiaron los tipos de datos que estaban incorrectos. Si el proyecto no les corre, regresar a la version 986

--- Sigarep.webapp/src/sigarep/modelos/data/seguridad/Usuario.java
+++ Sigarep.webapp/src/sigarep/modelos/data/seguridad/Usuario.java
@@ -3,6 +3,7 @@ package sigarep.modelos.data.seguridad;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -13,6 +14,7 @@ import javax.persistence.*;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
+import sigarep.herramientas.Archivo;
 import sigarep.modelos.data.maestros.Estudiante;
 import sigarep.modelos.data.maestros.Persona;
 import sigarep.modelos.data.transacciones.UsuarioGrupo;
@@ -27,7 +29,7 @@ public class Usuario implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@Column(name="nombreusuario", unique = true ,length=20, nullable=false)
+	@Column(name="nombre_usuario", unique = true ,length=35, nullable=false)
 	private String nombreUsuario;
 	
 	@Column(name = "clave")
@@ -36,11 +38,22 @@ public class Usuario implements Serializable {
 	@Column(name = "correo")
     private String correo;
 	
-	@Column(name = "nombreCompleto")
+	@Column(name = "nombre_completo")
     private String nombreCompleto;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name="fecha_creacion", nullable=true)
+	private Date fechaCreacion;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name="ultimo_acceso", nullable=true)
+	private Date ultimoAcceso;
 	
 	@Column(name = "estado")
     private Boolean estatus;
+	
+	@Embedded()
+	private Archivo foto;
 
 	//bi-directional many-to-one association to MiembroGrupo
 	@OneToMany(fetch = FetchType.EAGER, mappedBy="usuario", cascade={CascadeType.ALL})
@@ -150,5 +163,35 @@ public class Usuario implements Serializable {
 
 	public void setPersonas(List<Persona> personas) {
 		this.personas = personas;
+	}
+
+
+	public Archivo getFoto() {
+		return foto;
+	}
+
+
+	public void setFoto(Archivo foto) {
+		this.foto = foto;
+	}
+
+
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+
+	public Date getUltimoAcceso() {
+		return ultimoAcceso;
+	}
+
+
+	public void setUltimoAcceso(Date ultimoAcceso) {
+		this.ultimoAcceso = ultimoAcceso;
 	}
 }
