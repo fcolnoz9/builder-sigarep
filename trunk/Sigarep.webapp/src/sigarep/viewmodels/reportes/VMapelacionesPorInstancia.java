88eca401fb21e6961ac76f616d9d7917a56467fb
ariana.mendoza1@gmail.com <ariana.mendoza1@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-02-19 18:57:49 +0000

    Agregando el método cerrar en los vm de reportes y .zul

--- trunk/Sigarep.webapp/src/sigarep/viewmodels/reportes/VMapelacionesPorInstancia.java
+++ trunk/Sigarep.webapp/src/sigarep/viewmodels/reportes/VMapelacionesPorInstancia.java
@@ -4,12 +4,25 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
+import org.zkoss.bind.Binder;
+import org.zkoss.bind.annotation.AfterCompose;
 import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.ContextParam;
+import org.zkoss.bind.annotation.ContextType;
 import org.zkoss.bind.annotation.Init;
 import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zk.ui.select.Selectors;
 import org.zkoss.zk.ui.select.annotation.VariableResolver;
+import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zk.ui.select.annotation.WireVariable;
 import org.zkoss.zul.ListModelList;
+import org.zkoss.zul.Messagebox;
+import org.zkoss.zul.Window;
+import org.zkoss.zul.Messagebox.ClickEvent;
+
 import sigarep.modelos.data.maestros.LapsoAcademico;
 import sigarep.modelos.data.maestros.ProgramaAcademico;
 import sigarep.modelos.data.maestros.SancionMaestro;
@@ -61,6 +74,13 @@ public class VMapelacionesPorInstancia {
 	private String nombre_sancion;
 	private String codigo_lapso;
 	private String programa_academico;
+	
+	@Wire("#winApelacionesPorInstancia")//para conectarse a la ventana con el ID
+	Window ventana;
+	 @AfterCompose //para poder conectarse con los componentes en la vista, es necesario si no da null Pointer
+    public void afterCompose(@ContextParam(ContextType.VIEW) Component view){
+        Selectors.wireComponents(view, this, false);
+    }
 
 	// *************************INSTANCIANDO LAS CLASES NECESARIAS PARA EL
 	// REPORTE***************************
@@ -82,11 +102,7 @@ public class VMapelacionesPorInstancia {
 	public void buscarApelacionesR() {
 		System.out.println(objSancion.getNombreSancion());
 		System.out.println(objLapso.getCodigoLapso());
-		// String radio = objRadio.getName();
-		// System.out.println(radio);
-
-		//if (!selected.equals("")) {
-			//if (getSelected().equals("resultado")) {
+		
 				if (objSancion.getNombreSancion() == "Todos") {
 					apelacionesPrograma = servicioapelacionespormotivo
 							.buscarPorInstanciaResultado_Programa(
@@ -99,24 +115,13 @@ public class VMapelacionesPorInstancia {
 									objSancion.getIdSancion(),
 									objPrograma.getIdPrograma());
 
-				// model = ChartDataTipoDeSexo.getModel(lista);
-			//} else {
-			//	Messagebox.show("Debe Seleccionar una Opción", "Advertencia",
-			//			Messagebox.OK, Messagebox.EXCLAMATION);
-			//}
-		//} else {
-		//	Messagebox.show("Debe Seleccionar una Opción", "Advertencia",
-		//			Messagebox.OK, Messagebox.EXCLAMATION);
-		//}
 	}
 
 	@Command
 	@NotifyChange({ "listaPrograma" })
 	public void buscarPrograma() {
 		listaPrograma = servicioprogramaacademico.buscarPrograma(nombrePrograma);
-		/*ProgramaAcademico prog = new ProgramaAcademico(null, "Todos", null);
-
-		listaPrograma.add(0, prog);*/
+		
 	}
 
 	@Command
@@ -145,8 +150,7 @@ public class VMapelacionesPorInstancia {
 	@NotifyChange({ "listaLapso" })
 	public void buscarLapso() {
 		listaLapso = serviciolapsoacademico.buscarTodosLosLapsos();
-		/*LapsoAcademico lap = new LapsoAcademico("asd", null, null, null);
-		listaLapso.add(0, lap);*/
+		
 	}
 
 	@Command
@@ -293,6 +297,43 @@ public class VMapelacionesPorInstancia {
 
 	// #####################FIN DEL METODO##########################
 
+	@SuppressWarnings("unchecked")
+	@Command
+	@NotifyChange({ "objLapso", "programa_academico", "nombre_sancion",
+	 })
+	public void cerrarVentana(@ContextParam(ContextType.BINDER) final Binder binder){
+			
+		if (objLapso != null)
+		{
+			Messagebox.show("¿Realemente desea cerrar la ventana sin guardar los cambios?","Confirmar",new Messagebox.Button[] { Messagebox.Button.YES,Messagebox.Button.NO },
+					Messagebox.QUESTION,new EventListener<ClickEvent>() {
+				@SuppressWarnings("incomplete-switch")
+				public void onEvent(ClickEvent e) throws Exception {
+					switch (e.getButton()) {
+						case YES:
+								ventana.detach();
+					
+					}
+				}
+			});		
+		}
+		else{
+		Messagebox.show("¿Realmente desea cerrar la ventana?","Confirmar",new Messagebox.Button[] { Messagebox.Button.YES,Messagebox.Button.NO },
+					Messagebox.QUESTION,new EventListener<ClickEvent>() {
+				@SuppressWarnings("incomplete-switch")
+				public void onEvent(ClickEvent e) throws Exception {
+					switch (e.getButton()) {
+						case YES:
+								ventana.detach();
+					
+					
+					}
+				}
+			});		
+		}
+	}
+	
+	
 
 	public String getNombre_sancion() {
 		return nombre_sancion;
