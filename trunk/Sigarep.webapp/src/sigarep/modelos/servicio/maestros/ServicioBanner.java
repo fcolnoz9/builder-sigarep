dbdb7aaf4b96c11cddd65df16435af4305b039cc
palencia77@gmail.com <palencia77@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-02-20 15:25:00 +0000

    Quitando dependencias no usadas

--- trunk/Sigarep.webapp/src/sigarep/modelos/servicio/maestros/ServicioBanner.java
+++ trunk/Sigarep.webapp/src/sigarep/modelos/servicio/maestros/ServicioBanner.java
@@ -1,7 +1,6 @@
 package sigarep.modelos.servicio.maestros;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
