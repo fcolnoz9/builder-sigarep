1ccd11dee4ddd2ef5e9dc4fd73f8f9d23affd8a7
palencia77@gmail.com <palencia77@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-02-20 17:20:11 +0000

    Actualizado repositorio de LapsoAcademico para trabajar con las findBy

--- Sigarep.webapp/src/sigarep/viewmodels/reportes/VMEstudianteSancionado.java
+++ Sigarep.webapp/src/sigarep/viewmodels/reportes/VMEstudianteSancionado.java
@@ -460,7 +460,7 @@ public class VMEstudianteSancionado {
 	@Command
 	@NotifyChange({ "listaLapsoAcademico" })
 	public void buscarActivoLapso() {
-		listaLapsoAcademico = serviciolapsoacademico.listadoLapsoAcademico();
+		listaLapsoAcademico = serviciolapsoacademico.buscarTodosLosLapsos();
 		LapsoAcademico lap = new LapsoAcademico("Todos", null, null, null);
 		listaLapsoAcademico.add(listaLapsoAcademico.size(),lap);
 	}
