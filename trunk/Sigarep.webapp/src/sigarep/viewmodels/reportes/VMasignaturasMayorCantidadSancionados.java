30b2f8d1c0c9e086e6725bcbd88ef3b088e25b43
six1409@gmail.com <six1409@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-02-20 15:41:40 +0000

    De nuevo modificados los viewmodels de mis 2 reportes

--- trunk/Sigarep.webapp/src/sigarep/viewmodels/reportes/VMasignaturasMayorCantidadSancionados.java
+++ trunk/Sigarep.webapp/src/sigarep/viewmodels/reportes/VMasignaturasMayorCantidadSancionados.java
@@ -268,7 +268,7 @@ public class VMasignaturasMayorCantidadSancionados {
 	@Command
 	@NotifyChange({ "listaLapsoAcademico" })
 	public void buscarLapso() {
-		setListaLapsoAcademico(serviciolapsoacademico.listadoLapsoAcademico());
+		setListaLapsoAcademico(serviciolapsoacademico.buscarTodosLosLapsos());
 	}
 	
 	/** Objeto Combo Lapso.
