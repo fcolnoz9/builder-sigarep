70caa90999695f9b463c8968b52f6534ae1fc06c
palencia77@gmail.com <palencia77@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-02-21 02:56:51 +0000

    Actualizado repositorio TipoMotivo para trabajar con los findBy

--- Sigarep.webapp/src/sigarep/modelos/repositorio/maestros/ITipoMotivoDAO.java
+++ Sigarep.webapp/src/sigarep/modelos/repositorio/maestros/ITipoMotivoDAO.java
@@ -4,19 +4,27 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import sigarep.modelos.data.maestros.InstanciaApelada;
 import sigarep.modelos.data.maestros.TipoMotivo;
 
 public interface ITipoMotivoDAO extends JpaRepository<TipoMotivo, Integer> {
 
-	@Query("Select tipo FROM TipoMotivo AS tipo WHERE estatus = 'TRUE'")
-	public List<TipoMotivo> buscarTipoMotivoActivas();
+	/**
+	 * Busca los Tipos de Motivos activos, es decir, que poseen estatus true
+	 * @return List<TipoMotivo> Lista de Tipos de Motivos con estatus true
+	 */
+	public List<TipoMotivo> findByEstatusTrue();
 	
-	@Query("SELECT tipo from TipoMotivo AS tipo WHERE tipo.protegido = 'FAlSE'")
-	public List<TipoMotivo> buscarTodas();
+	/**
+	 * Busca los Tipos de Motivos activos que no son protegidos por el sistema
+	 * @return List<TipoMotivo> Lista de Tipos de Motivos activos no protegidos
+	 */
+	public List<TipoMotivo> findByProtegidoFalseAndEstatusTrue();
 	
+	/**
+	 * Busca el ultimo id insertado en la tabla TipoMotivo
+	 * @return Ultimo id insertado en la tabla TipoMotivo
+	 */
 	@Query("SELECT COALESCE(MAX(tp.idTipoMotivo),0) FROM TipoMotivo AS tp")
 	public int buscarUltimoID();
 }
