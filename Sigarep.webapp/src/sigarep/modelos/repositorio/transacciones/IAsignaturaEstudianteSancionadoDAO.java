c5a86a6be7dcb91a4c4ebff36d0c85045ed60e7d
palencia77@gmail.com <palencia77@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-02-22 21:44:09 +0000

    Actualizado el repositorio

--- Sigarep.webapp/src/sigarep/modelos/repositorio/transacciones/IAsignaturaEstudianteSancionadoDAO.java
+++ Sigarep.webapp/src/sigarep/modelos/repositorio/transacciones/IAsignaturaEstudianteSancionadoDAO.java
@@ -14,6 +14,12 @@ public interface IAsignaturaEstudianteSancionadoDAO
 		extends
 		JpaRepository<AsignaturaEstudianteSancionado, AsignaturaEstudianteSancionadoPK> {
 
-		@Query("Select ae FROM AsignaturaEstudianteSancionado AS ae WHERE ae.id.cedulaEstudiante = :cedulaEstudiante AND ae.id.codigoLapso = :codigoLapso")
-		public List<AsignaturaEstudianteSancionado> buscarAsignaturaDeSancionRR(@Param("cedulaEstudiante") String cedulaEstudiante, @Param("codigoLapso") String codigoLapso);
+	/**
+	 * Busca Busca las asignaturas por las que fue sancionado un estudiante en un lapso academico especifico
+	 * @param cedulaEstudiante cedula del estudiante al que se le buscara las asignaturas
+	 * @param codigoLapso lapso academico en el que fue sancionado el estudiante
+	 * @return List<AsignaturaEstudianteSancionado>
+	 */
+	@Query("Select ae FROM AsignaturaEstudianteSancionado AS ae WHERE ae.id.cedulaEstudiante = :cedulaEstudiante AND ae.id.codigoLapso = :codigoLapso")
+	public List<AsignaturaEstudianteSancionado> buscarAsignaturaDeSancionRR(@Param("cedulaEstudiante") String cedulaEstudiante, @Param("codigoLapso") String codigoLapso);
 }
