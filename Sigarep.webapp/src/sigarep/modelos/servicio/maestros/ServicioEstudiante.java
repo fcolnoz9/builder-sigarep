754229b1c9b6dfe3c05649f33662d087c9d24903
vanedunob@gmail.com <vanedunob@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-03-09 06:32:04 +0000

    Validando historial estudiante, y haciendo varios cambios

--- Sigarep.webapp/src/sigarep/modelos/servicio/maestros/ServicioEstudiante.java
+++ Sigarep.webapp/src/sigarep/modelos/servicio/maestros/ServicioEstudiante.java
@@ -1,12 +1,17 @@
 package sigarep.modelos.servicio.maestros;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import sigarep.modelos.data.maestros.Estudiante;
+import sigarep.modelos.data.transacciones.EstudianteSancionado;
+
 import sigarep.modelos.repositorio.maestros.IEstudianteDAO;
+
+
 
 
 // El servicio interactua con la base de datos
@@ -74,4 +79,40 @@ public class ServicioEstudiante {
 		}
 		return result;
 	}
+	/** Lista de Estudiante
+	 * @return Lista de Estudiantes registrados
+	 * @parameters vacio
+	 * @throws No dispara ninguna excepcion.
+	   */
+	public List<Estudiante> buscarEstudiante() {
+		return est.buscarEstudiante();
+	}
+	
+	
+	/** Filtra Estudiante para ListaGenerica 
+	 * @return Lista de EstudianteS
+	 * @parameters String cedula, String programa, String nombre, String apellido
+	 * @throws No dispara ninguna excepcion.
+	   */
+	public List<Estudiante> filtrarEstudiantesHistorial(
+			String programa, String cedula, String nombre, String apellido) {
+		
+		List<Estudiante> result = new ArrayList<Estudiante>();
+        if(programa==null || cedula==null || nombre==null || apellido==null ){
+        	result= buscarEstudiante();
+        }
+        else{
+			for (Estudiante sa : buscarEstudiante())
+			{
+				if (sa.getProgramaAcademico().getNombrePrograma() .toLowerCase().contains(programa.toLowerCase())&&
+						sa.getCedulaEstudiante().toLowerCase().contains(cedula.toLowerCase())&&
+						sa.getPrimerNombre().toLowerCase().contains(nombre.toLowerCase())&&
+						sa.getPrimerApellido().toLowerCase().contains(apellido.toLowerCase())){
+					result.add(sa);
+				}
+			}
+        }
+		return result;
+	}
+	
 }
