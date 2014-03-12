0efe6a021d47255274f9d2eb430a3e04a261b8c4
blumxp@gmail.com <blumxp@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-03-12 13:45:37 +0000

    se eliminó métodos que no se utilizaban

--- trunk/Sigarep.webapp/src/sigarep/controlador/maestros/ModalControler2.java
+++ trunk/Sigarep.webapp/src/sigarep/controlador/maestros/ModalControler2.java
@@ -9,36 +9,6 @@ import org.zkoss.zul.Window;
 
 public class ModalControler2 extends SelectorComposer<Component> {
 	private static final long serialVersionUID = 1L;
-	
-	@Listen("onClick = #lblRegla")
-	public void showModal(Event e) {
-		//create a window programmatically and use it as a modal dialog.
-		Window window = (Window)Executions.createComponents(
-				"/Modal/reglamentos.zul", null, null);
-		window.doModal();
-	}
-	@Listen("onClick = #orderBtn2")
-	public void showModal2(Event e) {
-		//create a window programmatically and use it as a modal dialog.
-		Window window = (Window)Executions.createComponents(
-				"/Modal/RegistrarEstudiante.zul", null, null);
-		window.doModal();
-	}
-	
-	@Listen("onClick = #buscar")
-	public void showModal3(Event e) {
-		//create a window programmatically and use it as a modal dialog.
-		Window window = (Window)Executions.createComponents(
-				"/Modal/HistorialEst.zul", null, null);
-		window.doModal();
-	}
-	@Listen("onClick = #rowcrog")
-	public void showModal4(Event e) {
-		//create a window programmatically and use it as a modal dialog.
-		Window window = (Window)Executions.createComponents(
-				"/Modal/DescripcionCrog.zul", null, null);
-		window.doModal();
-	}
 	@Listen("onClick = #btnlogin")
 	public void showModal43(Event e) {
 		Executions.sendRedirect("index.zul");
