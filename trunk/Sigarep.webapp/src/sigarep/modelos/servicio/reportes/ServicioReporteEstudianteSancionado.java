c5f70f6209ae0e5de01c997292b39698d1ee9d30
blumxp@gmail.com <blumxp@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-03-12 03:26:55 +0000

    Eliminando código sin uso.

--- trunk/Sigarep.webapp/src/sigarep/modelos/servicio/reportes/ServicioReporteEstudianteSancionado.java
+++ trunk/Sigarep.webapp/src/sigarep/modelos/servicio/reportes/ServicioReporteEstudianteSancionado.java
@@ -18,29 +18,13 @@ public class ServicioReporteEstudianteSancionado {
 	 * @throws No dispara ninguna excepcion.
 	 */
 	public List<EstudianteSancionado> buscarTodosSancionado(String lapsoAcademico,String tiposancion,String instanciaApelada,String tipoMotivo, String programaAcademico,String sexo,String veredicto,String edoApelacion,String asignatura) {
-		if(tiposancion.equals("'1'") || tiposancion.equals("esa.id_sancion")) {
+		if(tiposancion.equals("'2'") || tiposancion.equals("esa.id_sancion")) {
 			tiraSql="and asig.codigo_asignatura="+""+asignatura+"";
 		}
 		else{
 			tiraSql="";
 		}
-		System.out.println("PASE");
 		String queryStatement = 
-//		"SELECT Distinct es.primer_nombre, es.primer_apellido, es.sexo,prog.nombre_programa,san.nombre_sancion, tm.nombre_tipo_motivo,iap.instancia_apelada,lapso.codigo_lapso,sap.veredicto,edo_ape.nombre_estado,asig.nombre_asignatura " +
-//		"FROM asignatura asig, estado_apelacion edo_ape, estudiante es  " +
-//		"INNER JOIN estudiante_sancionado as esa ON esa.cedula_estudiante=es.cedula_estudiante " +
-//	    "INNER JOIN solicitud_apelacion as sap ON sap.cedula_estudiante=esa.cedula_estudiante " +
-//	    "INNER JOIN lapso_academico as lapso ON lapso.codigo_lapso=sap.codigo_lapso " +
-//	    "INNER JOIN motivo as mot ON mot.cedula_estudiante=sap.cedula_estudiante " +
-//	    "INNER JOIN tipo_motivo as tm ON tm.id_tipo_motivo=mot.id_tipo_motivo " +
-//	    "INNER JOIN instancia_apelada as iap ON iap.id_instancia_apelada=sap.id_instancia_apelada " +
-//	    "INNER JOIN programa_academico as prog ON prog.id_programa=es.id_programa " +
-//	    "INNER JOIN sancion_maestro as san ON san.id_sancion=esa.id_sancion " +
-//	    "LEFT  JOIN asignatura_estudiante_sancionado as asig_san ON asig_san.cedula_estudiante=esa.cedula_estudiante " +
-//	    "INNER JOIN apelacion_estado_apelacion as ap_edo ON ap_edo.cedula_estudiante =sap.cedula_estudiante " +
-//	    "where es.id_programa="+""+programaAcademico+" and es.sexo="+""+sexo+" and sap.id_instancia_apelada="+""+instanciaApelada+" " +
-//	    "and mot.id_tipo_motivo="+""+tipoMotivo+" and sap.codigo_lapso="+""+lapsoAcademico+" and esa.id_sancion="+""+tiposancion+" " +
-//	    "and sap.veredicto="+""+veredicto+" and edo_ape.id_estado_apelacion="+""+edoApelacion+"  and asig.codigo_asignatura="+""+asignatura+" order by es.primer_nombre " 
 		"SELECT Distinct es.primer_nombre, es.primer_apellido, es.sexo,prog.nombre_programa,san.nombre_sancion, " + 
 		"tm.nombre_tipo_motivo,iap.instancia_apelada,lapso.codigo_lapso,sap.veredicto,edo_ape.nombre_estado,asig.nombre_asignatura " +
 		"FROM estudiante es " + 
@@ -57,13 +41,11 @@ public class ServicioReporteEstudianteSancionado {
 		"LEFT JOIN asignatura as asig ON asig.codigo_asignatura=asig_esan.codigo_asignatura " +
 		"LEFT JOIN apelacion_estado_apelacion as ape_edo_ape ON ape_edo_ape.cedula_estudiante=sap.cedula_estudiante " +
 		"LEFT JOIN estado_apelacion as edo_ape ON edo_ape.id_estado_apelacion=ape_edo_ape.id_estado_apelacion " +
-		"  " +
-	
 		"where sap.codigo_lapso=esa.codigo_lapso and esa.id_sancion=san.id_sancion " +
 		"and mot.codigo_lapso=sap.codigo_lapso and mot.id_instancia_apelada=sap.id_instancia_apelada and mot.cedula_estudiante=sap.cedula_estudiante " +
 		"and mot.id_tipo_motivo="+""+tipoMotivo+" and mot.id_tipo_motivo <> 1 and mot.id_tipo_motivo <> 2 and mot.id_tipo_motivo <> 3 and  " +
 		"es.sexo="+""+sexo+" "+
-		"and  ape_edo_ape.id_estado_apelacion="+""+edoApelacion+"  " +
+		"and  ape_edo_ape.id_estado_apelacion="+""+edoApelacion+" and sap.id_instancia_apelada="+""+instanciaApelada+" " +
 		"and san.id_sancion="+""+tiposancion+" " +
 		"and es.id_programa="+""+programaAcademico+"  and sap.veredicto="+""+veredicto+" and sap.codigo_lapso="+""+lapsoAcademico+" "+""+tiraSql+" order by es.primer_nombre  ";
 		System.out.println(queryStatement);
@@ -85,7 +67,6 @@ public class ServicioReporteEstudianteSancionado {
 					(String) resultRow[9],
 					(String) resultRow[10]));
 		}
-		System.out.println("TIRA"+tiraSql);
-		return results;
+		return results;//retorna el resultado de la Query
 	}
 }
