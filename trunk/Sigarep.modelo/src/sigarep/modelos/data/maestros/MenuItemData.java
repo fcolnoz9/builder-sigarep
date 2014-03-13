8daf9e34317d00a84680a35ca56f4b75c70134bf
eliecerbarreto@gmail.com <eliecerbarreto@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-03-13 18:43:58 +0000

    comentarios de MenuItem

--- trunk/Sigarep.modelo/src/sigarep/modelos/data/maestros/MenuItemData.java
+++ trunk/Sigarep.modelo/src/sigarep/modelos/data/maestros/MenuItemData.java
@@ -5,11 +5,18 @@ import java.util.List;
 
 public class MenuItemData {
 
+	
 	private static List<MenuItem> menus = new ArrayList<MenuItem>();
 	static {
 		
 	}
 
+	/**
+	 * getAllMenus
+	 * 
+	 * @param 
+	 * @return el arreglo de menu de enlace de interes
+	 */
 	public static List<MenuItem> getAllMenus() {
 		return new ArrayList<MenuItem>(menus);
 	}
