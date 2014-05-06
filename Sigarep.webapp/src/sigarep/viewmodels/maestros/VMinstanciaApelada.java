714cce9de2a427494449d49846252fd3e4cf0fe5
ariana.mendoza1@gmail.com <ariana.mendoza1@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-05-06 17:54:10 +0000

    validacion para instancia apelada.

--- Sigarep.webapp/src/sigarep/viewmodels/maestros/VMinstanciaApelada.java
+++ Sigarep.webapp/src/sigarep/viewmodels/maestros/VMinstanciaApelada.java
@@ -1,15 +1,23 @@
 package sigarep.viewmodels.maestros;
 
 import java.util.List;
+
+import org.zkoss.bind.Binder;
 import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.ContextParam;
+import org.zkoss.bind.annotation.ContextType;
 import org.zkoss.bind.annotation.Init;
 import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zk.ui.event.EventListener;
 import org.zkoss.zk.ui.select.annotation.VariableResolver;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zk.ui.select.annotation.WireVariable;
+import org.zkoss.zul.Messagebox;
 import org.zkoss.zul.Textbox;
 import org.zkoss.zul.Window;
+import org.zkoss.zul.Messagebox.ClickEvent;
+
 import sigarep.herramientas.MensajesAlUsuario;
 import sigarep.modelos.data.maestros.InstanciaApelada;
 import sigarep.modelos.servicio.maestros.ServicioInstanciaApelada;
@@ -165,6 +173,48 @@ public class VMinstanciaApelada {
 		}
 	}
 
+	/**
+	 * eliminarInstancia
+	 * 
+	 * @param Binder
+	 *            binder
+	 * @return No devuelve ningún valor
+	 * @throws Debe
+	 *             seleccionar un registro para poder eliminarlo
+	 */
+	@Command
+	@NotifyChange({"listaInstanciaApelada","idInstanciaApelada","instanciaApelada","nombreRecursoApelacion", "descripcion"})//el notifychange le  avisa a que parametros en la pantalla se van a cambiar, en este caso es se va a colocar en blanco al guardar!!
+	public void eliminarInstancia(
+			@ContextParam(ContextType.BINDER) final Binder binder) {
+		if (instanciaApelada == null || nombreRecursoApelacion == null || descripcion == null) {
+			mensajeAlUsuario.advertenciaSeleccionarParaEliminar();
+		} else {
+			//System.out.println(id_sancion);
+			if ( idInstanciaApelada == 1 || idInstanciaApelada == 2 || idInstanciaApelada == 3) {
+				Messagebox.show("¡No puede eliminar este registro!",
+						"Advertencia", Messagebox.OK, Messagebox.EXCLAMATION);
+			} else {
+				Messagebox.show("¿Desea eliminar el registro realmente?",
+						"Confirmar", new Messagebox.Button[] {
+								Messagebox.Button.YES, Messagebox.Button.NO },
+						Messagebox.QUESTION, new EventListener<ClickEvent>() {
+							@SuppressWarnings("incomplete-switch")
+							public void onEvent(ClickEvent e) throws Exception {
+								switch (e.getButton()) {
+								case YES:
+									servicioInstanciaApelada
+											.eliminarInstancia(idInstanciaApelada);
+									mensajeAlUsuario
+											.informacionEliminarCorrecto();
+									binder.postCommand("limpiar", null);
+								case NO:
+									binder.postCommand("limpiar", null);
+								}
+							}
+						});
+			}
+		}
+	}
 	/** Metodo que indica la InstanciaApelada seleccionada del Listbox 
 	 * @return nada
 	 * @parameters vacío
