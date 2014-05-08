c789d92018ecef8a6465efc5c6f8923657d4994d
jorgechaviel@gmail.com <jorgechaviel@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-05-08 21:01:47 +0000

    Correción en Querys de buscarRecaudosEntregadosAnalizarValidezI, II y III porque faltaba comparar con el Lapso Activo.

--- trunk/Sigarep.modelo/src/sigarep/modelos/repositorio/transacciones/IRecaudoEntregadoDAO.java
+++ trunk/Sigarep.modelo/src/sigarep/modelos/repositorio/transacciones/IRecaudoEntregadoDAO.java
@@ -53,6 +53,7 @@ public interface IRecaudoEntregadoDAO extends
 	
 	@Query("SELECT re FROM RecaudoEntregado AS re, LapsoAcademico AS la " +
 			"WHERE re.id.codigoLapso = la.codigoLapso " +
+			"AND la.estatus = 'TRUE' " +
 			"AND re.id.cedulaEstudiante = :cedula " +
 			"AND re.id.idInstanciaApelada = '1'")
 	public List<RecaudoEntregado> buscarRecaudosEntregadosAnalizarValidezI(@Param("cedula") String cedula);
@@ -74,6 +75,7 @@ public interface IRecaudoEntregadoDAO extends
 	
 	@Query("SELECT re FROM RecaudoEntregado AS re, LapsoAcademico AS la " +
 			"WHERE re.id.codigoLapso = la.codigoLapso " +
+			"AND la.estatus = 'TRUE' " +
 			"AND re.id.cedulaEstudiante = :cedula " +
 			"AND (re.id.idInstanciaApelada = '1' " +
 			"OR re.id.idInstanciaApelada = '2')")
@@ -81,6 +83,7 @@ public interface IRecaudoEntregadoDAO extends
 
 	@Query("SELECT re FROM RecaudoEntregado AS re, LapsoAcademico AS la " +
 			"WHERE re.id.codigoLapso = la.codigoLapso " +
+			"AND la.estatus = 'TRUE' " +
 			"AND re.id.cedulaEstudiante = :cedula " +
 			"AND (re.id.idInstanciaApelada = '2' " +
 			"OR re.id.idInstanciaApelada = '3')")
