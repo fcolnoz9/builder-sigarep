d03cef2515acf7614c6dc5a2d4c560ec7628d3eb
TJhonRaul@gmail.com <TJhonRaul@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-05-09 19:06:16 +0000

    Organizacion de codigo

--- trunk/Sigarep.modelo/src/sigarep/modelos/repositorio/transacciones/IApelacionEstadoApelacionDAO.java
+++ trunk/Sigarep.modelo/src/sigarep/modelos/repositorio/transacciones/IApelacionEstadoApelacionDAO.java
@@ -1,5 +1,4 @@
 package sigarep.modelos.repositorio.transacciones;
-
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,18 +11,22 @@ import sigarep.modelos.data.maestros.*;
 import sigarep.modelos.data.transacciones.EstudianteSancionadoPK;
 import sigarep.modelos.data.transacciones.EstudianteSancionado;
 
+/**
+ * Repositorio IApelacionEstadoApelacionDAO
+ * @author Equipo Builder
+ * @version 1.0
+ * @since 20/12/2013
+ * @last 09/05/2014
+ */
 public interface IApelacionEstadoApelacionDAO extends
 		JpaRepository<ApelacionEstadoApelacion, ApelacionEstadoApelacionPK> {
-	@Query("select es.cedulaEstudiante, es.primerNombre, es.primerApellido, "
-			+ "sm.nombreSancion, ea.idEstadoApelacion, ia.idInstanciaApelada from Estudiante es, EstudianteSancionado esa, SolicitudApelacion sa, "
-			+ "ApelacionEstadoApelacion ap, EstadoApelacion ea, SancionMaestro sm, InstanciaApelada ia  where  es.cedulaEstudiante = "
-			+ "cedula_estudiante and cedula_estudiante = "
-			+ "sa.estudianteSancionado and sa.estudianteSancionado = "
-			+ "cedula_estudiante and id_estado_apelacion = ea.idEstadoApelacion and "
-			+ "ea.nombreEstado = 'veredicto' and id_sancion = sm.idSancion and "
-			+ "id_instancia_apelada = ia.idInstanciaApelada and codigo_lapso = codigo_lapso")
-	public List<ApelacionEstadoApelacion> buscarApelaciones();
-
+	/**
+	 * Busca las apelaciones en lapsos anteriores
+	 * @param cedula
+	 * @param codigoLapso
+	 * @param instancia
+	 * @return List<ApelacionEstadoApelacion> buscarApelacionHistorial lista con las apelaciones
+	 */
 	@Query("select sa from ApelacionEstadoApelacion sa, SolicitudApelacion sap where sa.id.cedulaEstudiante = :cedula "
 			+ " and sap.id.codigoLapso = sa.id.codigoLapso and sap.id.cedulaEstudiante = sa.id.cedulaEstudiante and sa.id.codigoLapso = :codigoLapso "
 			+ "and sap.id.idInstanciaApelada = sa.id.idInstanciaApelada and sa.id.idInstanciaApelada =:instancia order by sa.id.idEstadoApelacion asc ")
@@ -32,10 +35,17 @@ public interface IApelacionEstadoApelacionDAO extends
 			@Param("codigoLapso") String codigoLapso,
 			@Param("instancia") Integer instancia);
 
+	/**
+	 * Busca la sugerncia echa a la apelacion de un estudiante
+	 * @param cedula
+	 * @param codigoLapso
+	 * @param instancia
+	 * @param estado
+	 * @return List<ApelacionEstadoApelacion> buscarSugerencia lista con las sugerencias
+	 */
 	@Query("select a from ApelacionEstadoApelacion a where a.id.cedulaEstudiante = :cedula "
 			+ " and a.id.codigoLapso = :codigoLapso and a.id.idInstanciaApelada = :instancia and " +
-			" a.id.idEstadoApelacion = :estado")
-			
+			" a.id.idEstadoApelacion = :estado")	
 	public List<ApelacionEstadoApelacion> buscarSugerencia(
 			@Param("cedula") String cedula,
 			@Param("codigoLapso") String codigoLapso,
