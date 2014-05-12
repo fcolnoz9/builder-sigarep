51331d7403d53c08cd26b156761748a26df8d5c3
six1409@gmail.com <six1409@gmail.com@0371dfd4-b9da-1655-26ef-83519a26328c>
2014-05-12 14:58:47 +0000

    Modificando las listas de procedentes

--- trunk/Sigarep.webapp/src/sigarep/modelos/servicio/reportes/ServicioEstudiantesProcedentes.java
+++ trunk/Sigarep.webapp/src/sigarep/modelos/servicio/reportes/ServicioEstudiantesProcedentes.java
@@ -23,7 +23,7 @@ public class ServicioEstudiantesProcedentes {
 	 * @throws No dispara ninguna excepcion.
 	 */
 	public List<ListaEstudiantesProcedentes> buscarEstudiantesProcedentesPrimeraApelacion(Integer programaAcademico) {
-		String queryStatement = "SELECT sa.cedula_estudiante, e.primer_nombre,e.primer_apellido,p.nombre_programa, sa.veredicto, ia.instancia_apelada, es.semestre FROM sigarep.programa_academico as p, sigarep.estudiante as e, sigarep.lapso_academico as la, sigarep.solicitud_apelacion AS sa " +
+		String queryStatement = "SELECT sa.cedula_estudiante, e.primer_nombre,e.primer_apellido,p.nombre_programa, sa.veredicto, ia.nombre_recurso_apelacion, es.semestre FROM sigarep.programa_academico as p, sigarep.estudiante as e, sigarep.lapso_academico as la, sigarep.solicitud_apelacion AS sa " +
 				"LEFT JOIN sigarep.ESTUDIANTE_SANCIONADO AS es ON (es.cedula_estudiante=sa.cedula_estudiante AND es.codigo_lapso=sa.codigo_lapso) " +
 				"LEFT JOIN sigarep.instancia_apelada as ia ON (ia.id_instancia_apelada=sa.id_instancia_apelada) WHERE sa.codigo_lapso= la.codigo_lapso and la.estatus= 'TRUE' " +
 				"and p.id_programa=e.id_programa AND sa.id_instancia_apelada= 1 AND sa.cedula_estudiante=e.cedula_estudiante AND sa.veredicto= 'PROCEDENTE' AND e.id_programa= "+""+programaAcademico+" ORDER BY sa.cedula_estudiante,e.primer_nombre desc " ;
@@ -52,7 +52,7 @@ public class ServicioEstudiantesProcedentes {
 	 * @throws No dispara ninguna excepcion.
 	 */
 	public List<ListaEstudiantesProcedentes> buscarEstudiantesProcedentesRecursoReconsideracion(Integer programaAcademico) {
-		String queryStatement = "SELECT sa.cedula_estudiante, e.primer_nombre,e.primer_apellido,p.nombre_programa, sa.veredicto, ia.instancia_apelada, es.semestre FROM sigarep.programa_academico as p,sigarep.estudiante as e,sigarep.lapso_academico as la, sigarep.solicitud_apelacion AS sa " +
+		String queryStatement = "SELECT sa.cedula_estudiante, e.primer_nombre,e.primer_apellido,p.nombre_programa, sa.veredicto, ia.nombre_recurso_apelacion, es.semestre FROM sigarep.programa_academico as p,sigarep.estudiante as e,sigarep.lapso_academico as la, sigarep.solicitud_apelacion AS sa " +
 				"LEFT JOIN sigarep.ESTUDIANTE_SANCIONADO AS es ON (es.cedula_estudiante=sa.cedula_estudiante AND es.codigo_lapso=sa.codigo_lapso) " +
 				"LEFT JOIN sigarep.instancia_apelada as ia ON (ia.id_instancia_apelada=sa.id_instancia_apelada) WHERE sa.codigo_lapso= la.codigo_lapso and la.estatus= 'TRUE' " +
 				"and p.id_programa=e.id_programa AND sa.id_instancia_apelada= 2 AND sa.cedula_estudiante=e.cedula_estudiante AND sa.veredicto= 'PROCEDENTE' AND e.id_programa= "+""+programaAcademico+" ORDER BY sa.cedula_estudiante,e.primer_nombre desc " ;
@@ -80,7 +80,7 @@ public class ServicioEstudiantesProcedentes {
 	 * @throws No dispara ninguna excepcion.
 	 */
 	public List<ListaEstudiantesProcedentes> buscarEstudiantesProcedentesRecursoJerarquico(Integer programaAcademico) {
-		String queryStatement = "SELECT sa.cedula_estudiante, e.primer_nombre,e.primer_apellido,p.nombre_programa, sa.veredicto, ia.instancia_apelada, es.semestre FROM sigarep.programa_academico as p,sigarep.estudiante as e,sigarep.lapso_academico as la, sigarep.solicitud_apelacion AS sa " +
+		String queryStatement = "SELECT sa.cedula_estudiante, e.primer_nombre,e.primer_apellido,p.nombre_programa, sa.veredicto, ia.nombre_recurso_apelacion, es.semestre FROM sigarep.programa_academico as p,sigarep.estudiante as e,sigarep.lapso_academico as la, sigarep.solicitud_apelacion AS sa " +
 				"LEFT JOIN sigarep.ESTUDIANTE_SANCIONADO AS es ON (es.cedula_estudiante=sa.cedula_estudiante AND es.codigo_lapso=sa.codigo_lapso) " +
 				"LEFT JOIN sigarep.instancia_apelada as ia ON (ia.id_instancia_apelada=sa.id_instancia_apelada) WHERE sa.codigo_lapso= la.codigo_lapso and la.estatus= 'TRUE' " +
 				"and p.id_programa=e.id_programa AND sa.id_instancia_apelada= 3 AND sa.cedula_estudiante=e.cedula_estudiante AND sa.veredicto= 'PROCEDENTE' AND e.id_programa= "+""+programaAcademico+" ORDER BY sa.cedula_estudiante,e.primer_nombre desc " ;
